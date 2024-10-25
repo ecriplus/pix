688d06ca111ac8c5f75697553a175a29d03ecb20
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2024-10-25 22:03:29 +0200

    feat(api): add createdAt and updatedAt to buildResetPasswordDemand

--- api/db/database-builder/factory/build-reset-password-demand.js
+++ api/db/database-builder/factory/build-reset-password-demand.js
@@ -5,12 +5,16 @@ const buildResetPasswordDemand = function ({
   email = 'example@example.net',
   temporaryKey = 'ABCD12345',
   used = false,
+  createdAt,
+  updatedAt,
 } = {}) {
   const values = {
     id,
     email,
     temporaryKey,
     used,
+    createdAt,
+    updatedAt,
   };
   return databaseBuffer.pushInsertable({
     tableName: 'reset-password-demands',
