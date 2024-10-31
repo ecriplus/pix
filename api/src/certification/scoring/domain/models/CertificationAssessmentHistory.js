882002ea1d28de3139909fae24eb67ad3ff5778b
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2024-10-31 11:00:44 +0100

    refactor: improve scoring v3 test
    Co-authored-by: Stephen Mehaut <stephen.mehaut@pix.fr>
    Co-authored-by: Andreia Pena Ferreira <andreia.pena@pix.fr>

--- api/src/certification/scoring/domain/models/CertificationAssessmentHistory.js
+++ api/src/certification/scoring/domain/models/CertificationAssessmentHistory.js
@@ -4,6 +4,7 @@ export class CertificationAssessmentHistory {
   constructor({ capacityHistory }) {
     this.capacityHistory = capacityHistory;
   }
+  // WARN: challenges are not Array<Challenge> but Array<CertificationChallengeForScoring>
   static fromChallengesAndAnswers({ algorithm, challenges, allAnswers }) {
     const capacityAndErrorRateHistory = algorithm.getCapacityAndErrorRateHistory({
       allAnswers,
