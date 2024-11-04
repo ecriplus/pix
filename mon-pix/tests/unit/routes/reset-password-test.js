61445b7791c9dac73c4bf2412014777d6f0e2f21
Eric Lim <eric.lim@zenika.com>
2024-11-04 11:56:45 +0100

    refactor(mon-pix): add unit test for reset password route
    for handling a server error case

--- mon-pix/tests/unit/routes/reset-password-test.js
+++ mon-pix/tests/unit/routes/reset-password-test.js
@@ -1,10 +1,14 @@
 import Service from '@ember/service';
+import { t } from 'ember-intl/test-support';
 import { setupTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 import sinon from 'sinon';
 
+import setupIntl from '../../helpers/setup-intl';
+
 module('Unit | Route | reset-password', function (hooks) {
   setupTest(hooks);
+  setupIntl(hooks);
 
   module('Route behavior', function (hooks) {
     let storeStub;
@@ -76,6 +80,32 @@ module('Unit | Route | reset-password', function (hooks) {
         // then
         assert.deepEqual(result.user, expectedUser);
         assert.deepEqual(result.temporaryKey, params.temporary_key);
+      });
+    });
+    module('when password reset demand is not valid', function () {
+      module('when error status is equal to 401', function () {
+        test('it adds an error in errors service and replace current route with "reset-password-demand"', async function (assert) {
+          // given
+          const errors = [{ status: 401 }];
+          const errorsService = this.owner.lookup('service:errors');
+          const route = this.owner.lookup('route:reset-password');
+          const replaceWithStub = sinon.stub();
+          const routerStub = Service.create({
+            replaceWith: replaceWithStub,
+          });
+
+          queryRecordStub.rejects({ errors });
+          route.set('store', storeStub);
+          route.set('router', routerStub);
+
+          // when
+          await route.model(params);
+
+          // then
+          sinon.assert.calledOnceWithExactly(replaceWithStub, 'password-reset-demand');
+          assert.strictEqual(errorsService.errors.length, 1);
+          assert.strictEqual(errorsService.errors[0], t('pages.reset-password.error.expired-demand'));
+        });
       });
     });
   });
