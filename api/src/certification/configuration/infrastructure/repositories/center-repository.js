46d945b72844cdad610de30e03c1bd4abd50728a
AndreiaPena <andreia.pena@pix.fr>
2024-11-15 10:59:56 +0100

    🔥 api: delete findSCOV2Centers method from center repository
    Used in findAndTriggerV2CenterToConvertInV3 usecase

--- api/src/certification/configuration/infrastructure/repositories/center-repository.js
+++ api/src/certification/configuration/infrastructure/repositories/center-repository.js
@@ -1,36 +1,5 @@
 import { DomainTransaction } from '../../../../shared/domain/DomainTransaction.js';
-import { DEFAULT_PAGINATION } from '../../../../shared/infrastructure/utils/knex-utils.js';
-import { logger } from '../../../../shared/infrastructure/utils/logger.js';
-import { Center } from '../../domain/models/Center.js';
 import { CenterTypes } from '../../domain/models/CenterTypes.js';
-
-/**
- * @param {Object} params
- * @param {number} params.[cursorId] - identifies the primary identifier above with the results will be taken (cursor pagination). If empty, will start from beginning
- * @param {number} params.[size] - number of centers to fetch
- * @returns {Array<Center>} centers primary identifiers found ordered by ascending identifiers
- */
-export const findSCOV2Centers = async function ({ cursorId, size = DEFAULT_PAGINATION.PAGE_SIZE } = {}) {
-  logger.debug('cursorId:[%o]', cursorId);
-  const knexConn = DomainTransaction.getConnection();
-  const query = knexConn
-    .from('certification-centers')
-    .select('certification-centers.id', 'certification-centers.externalId')
-    .where({ isV3Pilot: false, type: CenterTypes.SCO })
-    .orderBy('certification-centers.id', 'ASC')
-    .limit(size);
-
-  if (cursorId) {
-    query.andWhere('certification-centers.id', '>', cursorId);
-  }
-
-  const results = await query;
-  return results.map(({ id, externalId }) => _toDomain({ id, externalId, type: CenterTypes.SCO }));
-};
-
-const _toDomain = ({ id, externalId, type }) => {
-  return new Center({ id, externalId, type });
-};
 
 /**
  * @param {Object} params
