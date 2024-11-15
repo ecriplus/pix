520e0726769bdea9a20edbfb5ac71701ede11808
AndreiaPena <andreia.pena@pix.fr>
2024-11-15 10:58:20 +0100

    🔥 api: delete convertCenterToV3Job repository
    Used in findAndTriggerv2CenterToConvert usecase

--- api/src/certification/configuration/domain/usecases/index.js
+++ api/src/certification/configuration/domain/usecases/index.js
@@ -12,7 +12,6 @@ import * as centerRepository from '../../infrastructure/repositories/center-repo
  * @typedef {import('../../infrastructure/repositories/index.js').SessionsRepository} SessionsRepository
  */
 import { configurationRepositories } from '../../infrastructure/repositories/index.js';
-import { convertCenterToV3JobRepository } from '../../infrastructure/repositories/jobs/convert-center-to-v3-job-repository.js';
 
 /**
  *
@@ -22,7 +21,6 @@ import { convertCenterToV3JobRepository } from '../../infrastructure/repositorie
  * @typedef {attachableTargetProfileRepository} AttachableTargetProfileRepository
  * @typedef {centerPilotFeaturesRepository} CenterPilotFeaturesRepository
  * @typedef {centerRepository} CentersRepository
- * @typedef {convertCenterToV3JobRepository} ConvertCenterToV3JobRepository
  * @typedef {sessionsRepository} SessionsRepository
  * @typedef {candidateRepository} CandidateRepository
  **/
@@ -31,7 +29,6 @@ const dependencies = {
   complementaryCertificationRepository,
   centerPilotFeaturesRepository,
   centerRepository,
-  convertCenterToV3JobRepository,
   sessionsRepository: configurationRepositories.sessionsRepository,
   candidateRepository,
 };
