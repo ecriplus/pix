7aa1a4f57d963a3d28a28ecdbb308476875c57c4
Laura Bergoens <laura.bergoens@pix.fr>
2024-11-20 14:56:31 +0100

    add competences table
    Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>

--- api/db/migrations/20241120132349_create-learningcontent-schema-and-tables.js
+++ api/db/migrations/20241120132349_create-learningcontent-schema-and-tables.js
@@ -15,6 +15,16 @@ const up = async function (knex) {
     table.string('frameworkId').references('id').inTable(`${SCHEMA_NAME}.frameworks`);
     table.specificType('competenceIds', 'string[]');
   });
+  await knex.schema.withSchema(SCHEMA_NAME).createTable('competences', function (table) {
+    table.string('id').primary();
+    table.jsonb('name_i18n');
+    table.jsonb('description_i18n');
+    table.string('index');
+    table.text('origin');
+    table.string('areaId').references('id').inTable(`${SCHEMA_NAME}.areas`);
+    table.specificType('skillIds', 'string[]');
+    table.specificType('thematicIds', 'string[]');
+  });
 };
 
 const down = function (knex) {
