f4be16a12f75a0d9226705b3e51090480c4ad9d0
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2024-11-21 12:05:21 +0100

    feat(api): add job controller to validate csv

---
+++ api/src/prescription/learner-management/application/jobs/validate-csv-organization-learners-import-file-job-controller.js
@@ -0,0 +1,39 @@
+import { JobController } from '../../../../shared/application/jobs/job-controller.js';
+import { config } from '../../../../shared/config.js';
+import { DomainError } from '../../../../shared/domain/errors.js';
+import { getI18n } from '../../../../shared/infrastructure/i18n/i18n.js';
+import { logger as l } from '../../../../shared/infrastructure/utils/logger.js';
+import { ValidateCsvOrganizationImportFileJob } from '../../domain/models/ValidateCsvOrganizationImportFileJob.js';
+import { usecases } from '../../domain/usecases/index.js';
+import { SupOrganizationLearnerParser } from '../../infrastructure/serializers/csv/sup-organization-learner-parser.js';
+
+class ValidateCsvOrganizationLearnersImportFileJobController extends JobController {
+  #logger;
+
+  constructor({ logger = l } = {}) {
+    super(ValidateCsvOrganizationImportFileJob.name);
+    this.#logger = logger;
+  }
+
+  get isJobEnabled() {
+    return config.pgBoss.validationFileJobEnabled;
+  }
+
+  async handle({ data }) {
+    const { organizationImportId, locale, type } = data;
+
+    const Parser = SupOrganizationLearnerParser;
+    const i18n = getI18n(locale);
+
+    try {
+      await usecases.validateCsvFile({ organizationImportId, i18n, type, Parser });
+    } catch (err) {
+      if (!(err instanceof DomainError)) {
+        throw err;
+      }
+      this.#logger.warn(err);
+    }
+  }
+}
+
+export { ValidateCsvOrganizationLearnersImportFileJobController };
