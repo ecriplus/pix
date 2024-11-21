103cdfcd9798a76c1d934639c79d3a0f72abdde4
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2024-11-21 16:20:49 +0100

    feat(api): add repo to import sco csv fregate job

---
+++ api/src/prescription/learner-management/domain/models/ImportScoCsvOrganizationLearnersJob.js
@@ -0,0 +1,6 @@
+export class ImportScoCsvOrganizationLearnersJob {
+  constructor({ organizationImportId, locale }) {
+    this.organizationImportId = organizationImportId;
+    this.locale = locale;
+  }
+}
