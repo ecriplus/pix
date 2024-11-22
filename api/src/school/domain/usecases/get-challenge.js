e1bff1588ad4878329492bc053e4d5e99d06f433
rouxxi <gwenvael.laskar@pix.fr>
2024-11-22 12:22:22 +0100

    feat(api): change instruction to instructions

--- api/src/school/domain/usecases/get-challenge.js
+++ api/src/school/domain/usecases/get-challenge.js
@@ -1,6 +1,3 @@
-import { challengeService } from '../services/challenge.js';
-
 export async function getChallenge({ id, challengeRepository }) {
-  const challenge = await challengeRepository.get(id);
-  return challengeService.mapChallenge(challenge);
+  return challengeRepository.get(id);
 }
