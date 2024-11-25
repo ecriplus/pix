63c599dd405f1fbab704141c33d0f72895e0a862
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2024-11-25 14:33:28 +0100

    feat(api): add anonymizeUser function to Privacy bounded context users-api.js

--- api/src/privacy/application/api/users-api.js
+++ api/src/privacy/application/api/users-api.js
@@ -1,12 +1,27 @@
 import { usecases } from '../../domain/usecases/index.js';
 
 /**
+ * Anonymizes everything related to the user.
+ *
+ * @param{string} params.userId
+ * @param{string} params.anonymizedByUserId
+ * @param{string} params.anonymizedByUserRole
+ * @param{string} params.client
+ * @returns {Promise<void>}
+ */
+const anonymizeUser = async ({ userId, anonymizedByUserId, anonymizedByUserRole, client }) => {
+  return usecases.anonymizeUser({ userId, anonymizedByUserId, anonymizedByUserRole, client });
+};
+
+/**
  * Determines if a user can self-delete their account.
  *
  * @param {Object} params - The parameters for the function.
  * @param {number} params.userId - The ID of the user.
  * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating if the user can self-delete their account.
  */
-export const canSelfDeleteAccount = async ({ userId }) => {
+const canSelfDeleteAccount = async ({ userId }) => {
   return usecases.canSelfDeleteAccount({ userId });
 };
+
+export { anonymizeUser, canSelfDeleteAccount };
