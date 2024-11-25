6661601b32ec0b0d2efad6f9fbdf23e7cb68dc90
Benjamin Petetot <benjamin.petetot@pix.fr>
2024-11-25 18:24:42 +0100

    feat(api): add business rules to can-self-delete-account usecase

--- api/tests/privacy/integration/domain/usecases/can-self-delete-account.usecase.test.js
+++ api/tests/privacy/integration/domain/usecases/can-self-delete-account.usecase.test.js
@@ -2,22 +2,7 @@ import { usecases } from '../../../../../src/privacy/domain/usecases/index.js';
 import { databaseBuilder, expect } from '../../../../test-helper.js';
 
 describe('Integration | Privacy | Domain | UseCase | can-self-delete-account', function () {
-  context('Feature flag is disabled', function () {
-    it('returns false', async function () {
-      // given
-      const featureToggles = { isSelfAccountDeletionEnabled: false };
-      const user = databaseBuilder.factory.buildUser();
-      await databaseBuilder.commit();
-
-      // when
-      const result = await usecases.canSelfDeleteAccount({ userId: user.id, featureToggles });
-
-      // then
-      expect(result).to.be.false;
-    });
-  });
-
-  context('Feature flag is enabled', function () {
+  context('When user is eligible', function () {
     it('returns true', async function () {
       // given
       const featureToggles = { isSelfAccountDeletionEnabled: true };
