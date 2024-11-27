67635b94b35e37d3245d0fd221e55f7bbbbb2334
Laura Bergoens <laura.bergoens@pix.fr>
2024-11-27 14:04:52 +0100

    feat(api): adds learning content challenge repository w/ save method
    Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>

---
+++ api/src/learning-content/infrastructure/repositories/challenge-repository.js
@@ -0,0 +1,3 @@
+import { LearningContentRepository } from './learning-content-repository.js';
+
+export const challengeRepository = new LearningContentRepository({ tableName: 'learningcontent.challenges' });
