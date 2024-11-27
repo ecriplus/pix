3f14eb4efe073a5ce1e5c109367ccb46a0599bd5
tbeaumont79 <tbeaumont79@icloud.com>
2024-11-27 15:09:49 +0100

    refactor(mon-pix): remove featureToggle in JS file

--- mon-pix/app/controllers/inscription/inscription.js
+++ mon-pix/app/controllers/inscription/inscription.js
@@ -7,11 +7,6 @@ export default class InscriptionController extends Controller {
   @service intl;
   @service locale;
   @service router;
-  @service featureToggles;
-
-  get isNewAuthenticationDesignEnabled() {
-    return this.featureToggles.featureToggles.isNewAuthenticationDesignEnabled;
-  }
 
   get isInternationalDomain() {
     return !this.currentDomain.isFranceDomain;
--- mon-pix/app/routes/reset-password.js
+++ mon-pix/app/routes/reset-password.js
@@ -4,7 +4,6 @@ import get from 'lodash/get';
 
 export default class ResetPasswordRoute extends Route {
   @service errors;
-  @service featureToggles;
   @service intl;
   @service router;
   @service session;
@@ -18,11 +17,7 @@ export default class ResetPasswordRoute extends Route {
     } catch (error) {
       const status = get(error, 'errors[0].status');
       if (status && (status === 401 || (status && 404))) {
-        let error = this.intl.t('pages.reset-password.error.expired-demand');
-
-        if (this.featureToggles.featureToggles.isNewAuthenticationDesignEnabled) {
-          error = 'pages.reset-password.error.expired-demand';
-        }
+        const error = 'pages.reset-password.error.expired-demand';
         this.errors.push(error);
         this.router.replaceWith('password-reset-demand');
       }
