2f68de7edeaeab1ec9396089d5cffe147e8948f5
Laura Bergoens <laura.bergoens@pix.fr>
2024-11-29 11:26:26 +0100

    feat(api): creates new learning content datasource implementation
    Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>

---
+++ api/src/shared/infrastructure/repositories/learning-content-repository.js
@@ -0,0 +1,186 @@
+import Dataloader from 'dataloader';
+
+import { knex } from '../../../../db/knex-database-connection.js';
+import * as learningContentPubSub from '../caches/learning-content-pubsub.js';
+import { child } from '../utils/logger.js';
+
+const logger = child('learningcontent:repository', { event: 'learningcontent' });
+
+/**
+ * @typedef {(knex: import('knex').QueryBuilder) => Promise<string[]|number[]>} QueryBuilderCallback
+ */
+
+/**
+ * Datasource for learning content repositories.
+ * This datasource uses a {@link Dataloader} to load and cache entities.
+ */
+export class LearningContentRepository {
+  #tableName;
+  #idType;
+  #dataloader;
+  #findCache;
+  #findCacheMiss;
+
+  /**
+   * @param {{
+   *   tableName: string
+   *   idType?: 'text' | 'integer'
+   * }} config
+   */
+  constructor({ tableName, idType = 'text' }) {
+    this.#tableName = tableName;
+    this.#idType = idType;
+
+    this.#dataloader = new Dataloader((ids) => this.#batchLoad(ids), {
+      cacheMap: new LearningContentCache({ name: `${tableName}:entities` }),
+    });
+
+    this.#findCache = new LearningContentCache({ name: `${tableName}:results` });
+
+    this.#findCacheMiss = new Map();
+  }
+
+  /**
+   * Finds several entities using a request and caches results.
+   * The request is built using a knex query builder given to {@link callback}.
+   * {@link cacheKey} must vary according to params given to the query builder.
+   * @param {string} cacheKey
+   * @param {QueryBuilderCallback} callback
+   * @returns {Promise<object[]>}
+   */
+  async find(cacheKey, callback) {
+    let dtos = this.#findCache.get(cacheKey);
+    if (dtos) return dtos;
+
+    dtos = this.#findCacheMiss.get(cacheKey);
+    if (dtos) return dtos;
+
+    dtos = this.#loadDtos(callback, cacheKey).finally(() => {
+      this.#findCacheMiss.delete(cacheKey);
+    });
+    this.#findCacheMiss.set(cacheKey, dtos);
+
+    return dtos;
+  }
+
+  /**
+   * Loads one entity by ID.
+   * @param {string|number} id
+   * @returns {Promise<object>}
+   */
+  async load(id) {
+    if (!id) return null;
+    return this.#dataloader.load(id);
+  }
+
+  /**
+   * Loads several entities by ID.
+   * @param {string[]|number[]} ids
+   * @returns {Promise<object[]>}
+   */
+  async loadMany(ids) {
+    const notNullIds = ids.filter((id) => id);
+    return this.#dataloader.loadMany(notNullIds);
+  }
+
+  /**
+   * Loads entities from database using a request and writes result to cache.
+   * @param {string} cacheKey
+   * @param {QueryBuilderCallback} callback
+   * @returns {Promise<object[]>}
+   */
+  async #loadDtos(callback, cacheKey) {
+    const ids = await callback(knex.pluck(`${this.#tableName}.id`).from(this.#tableName));
+    const dtos = await this.#dataloader.loadMany(ids);
+
+    logger.debug({ tableName: this.#tableName, cacheKey }, 'caching find result');
+    this.#findCache.set(cacheKey, dtos);
+
+    return dtos;
+  }
+
+  /**
+   * Loads a batch of entities from database by ID.
+   * Entities are returned in the same order as {@link ids}.
+   * If an ID is not found, it is null in results.
+   * @param {string[]|number[]} ids
+   * @returns {Promise<(object|null)[]>}
+   */
+  async #batchLoad(ids) {
+    logger.debug({ tableName: this.#tableName, count: ids.length }, 'loading from PG');
+    const dtos = await knex
+      .select(`${this.#tableName}.*`)
+      .from(knex.raw(`unnest(?::${this.#idType}[]) with ordinality as ids(id, idx)`, [ids])) // eslint-disable-line knex/avoid-injections
+      .leftJoin(this.#tableName, `${this.#tableName}.id`, 'ids.id')
+      .orderBy('ids.idx');
+    return dtos.map((dto) => (dto.id ? dto : null));
+  }
+
+  /**
+   * Clears repository’s cache.
+   * If {@link id} is undefined, all cache is cleared.
+   * If {@link id} is given, cache is partially cleared.
+   * @param {string|number|undefined} id
+   */
+  clearCache(id) {
+    logger.debug({ tableName: this.#tableName, id }, 'trigerring cache clear');
+
+    if (id) {
+      this.#dataloader.clear(id);
+    } else {
+      this.#dataloader.clearAll();
+    }
+    this.#findCache.clear();
+  }
+}
+
+class LearningContentCache {
+  #map;
+  #pubSub;
+  #name;
+
+  /**
+   * @param {{
+   *   name: string
+   *   pubSub: import('../caches/learning-content-pubsub.js').LearningContentPubSub
+   *   map: Map
+   * }} config
+   * @returns
+   */
+  constructor({ name, pubSub = learningContentPubSub.getPubSub(), map = new Map() }) {
+    this.#name = name;
+    this.#pubSub = pubSub;
+    this.#map = map;
+
+    this.#subscribe();
+  }
+
+  get(key) {
+    return this.#map.get(key);
+  }
+
+  set(key, value) {
+    return this.#map.set(key, value);
+  }
+
+  delete(key) {
+    return this.#pubSub.publish(this.#name, { type: 'delete', key });
+  }
+
+  clear() {
+    return this.#pubSub.publish(this.#name, { type: 'clear' });
+  }
+
+  async #subscribe() {
+    for await (const message of this.#pubSub.subscribe(this.#name)) {
+      if (message.type === 'clear') {
+        logger.debug({ name: this.#name }, 'clearing cache');
+        this.#map.clear();
+      }
+      if (message.type === 'delete') {
+        logger.debug({ name: this.#name, key: message.key }, 'deleting cache key');
+        this.#map.delete(message.key);
+      }
+    }
+  }
+}
