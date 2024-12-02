86069fd9105f032aa9962a2736db767bc5645ba8
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2024-12-02 18:29:49 +0100

    refactor(mon-pix): rename class in user-oidc-authentication-request.js into UserOidcAuthenticationRequest
    for consistency.

--- mon-pix/app/adapters/user-oidc-authentication-request.js
+++ mon-pix/app/adapters/user-oidc-authentication-request.js
@@ -1,6 +1,6 @@
 import ApplicationAdapter from './application';
 
-export default class ExternalUserAuthenticationRequest extends ApplicationAdapter {
+export default class UserOidcAuthenticationRequest extends ApplicationAdapter {
   buildURL() {
     return `${this.host}/${this.namespace}/oidc/user/`;
   }
