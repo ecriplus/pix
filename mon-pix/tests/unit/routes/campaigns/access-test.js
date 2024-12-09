ee873b0dfc11f0992d9821d735a9e6e0fd5cff74
Benjamin Petetot <benjamin.petetot@pix.fr>
2024-12-09 18:10:21 +0100

    refactor(mon-pix): use session helper in campaings access

--- mon-pix/tests/unit/routes/campaigns/access-test.js
+++ mon-pix/tests/unit/routes/campaigns/access-test.js
@@ -3,27 +3,23 @@ import { setupTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 import sinon from 'sinon';
 
+import { stubSessionService } from '../../../helpers/service-stubs.js';
+
 module('Unit | Route | Access', function (hooks) {
   setupTest(hooks);
 
-  let route, campaign, sessionStub;
+  let route, campaign;
 
   hooks.beforeEach(function () {
     campaign = {
       code: 'NEW_CODE',
       isRestrictedByIdentityProvider: sinon.stub(),
     };
-    sessionStub = {
-      requireAuthenticationAndApprovedTermsOfService: sinon.stub(),
-      setAttemptedTransition: sinon.stub(),
-      data: { externalUser: null, authenticated: {} },
-    };
+
     route = this.owner.lookup('route:campaigns.access');
     route.modelFor = sinon.stub().returns(campaign);
     route.campaignStorage = { get: sinon.stub() };
-
     route.router = { replaceWith: sinon.stub(), transitionTo: sinon.stub() };
-    route.session = sessionStub;
     class OidcIdentityProvidersStub extends Service {
       list = [];
     }
@@ -31,102 +27,109 @@ module('Unit | Route | Access', function (hooks) {
   });
 
   module('#beforeModel', function () {
-    test('should redirect to entry point when /access is directly set in the url', async function (assert) {
-      //when
-      await route.beforeModel({ from: null });
+    module('when user is authenticated from Pix', function (hooks) {
+      let sessionStub;
 
-      //then
-      sinon.assert.calledWith(route.router.replaceWith, 'campaigns.entry-point');
-      assert.ok(true);
-    });
-
-    test('should continue on access route when from is set', async function (assert) {
-      //when
-      await route.beforeModel({ from: 'campaigns.campaign-landing-page' });
-
-      //then
-      sinon.assert.notCalled(route.router.replaceWith);
-      assert.ok(true);
-    });
-
-    test('should override authentication route', async function (assert) {
-      // when
-      await route.beforeModel({ from: 'campaigns.campaign-landing-page' });
-
-      // then
-
-      assert.strictEqual(route.authenticationRoute, 'inscription');
-    });
-
-    test("should call parent's beforeModel and transition to authenticationRoute", async function (assert) {
-      // when
-      await route.beforeModel({ from: 'campaigns.campaign-landing-page' });
-
-      // then
-      sinon.assert.calledWith(
-        sessionStub.requireAuthenticationAndApprovedTermsOfService,
-        { from: 'campaigns.campaign-landing-page' },
-        'inscription',
-      );
-      assert.ok(true);
-    });
-
-    module('when campaign belongs to an oidc provider', function (hooks) {
       hooks.beforeEach(function () {
-        const oidcPartner = {
-          id: 'oidc-partner',
-          code: 'OIDC_PARTNER',
-        };
-        class OidcIdentityProvidersStub extends Service {
-          'oidc-partner' = oidcPartner;
-          list = [oidcPartner];
-        }
-        this.owner.register('service:oidcIdentityProviders', OidcIdentityProvidersStub);
+        sessionStub = stubSessionService(this.owner, { isAuthenticated: true });
+        route.session = sessionStub;
       });
 
-      module('and user is not connected with that provider', function () {
-        test('should use provider route', async function (assert) {
-          // given
-          route.session.data.externalUser = 'some external user';
-          campaign.isRestrictedByIdentityProvider.withArgs('OIDC_PARTNER').returns(true);
+      test('should redirect to entry point when /access is directly set in the url', async function (assert) {
+        //when
+        await route.beforeModel({ from: null });
 
-          // when
-          await route.beforeModel({ from: 'campaigns.campaign-landing-page' });
+        //then
+        sinon.assert.calledWith(route.router.replaceWith, 'campaigns.entry-point');
+        assert.ok(true);
+      });
 
-          // then
-          sinon.assert.calledWith(route.router.replaceWith, 'authentication.login-oidc', 'oidc-partner');
-          assert.ok(true);
+      test('should continue on access route when from is set', async function (assert) {
+        //when
+        await route.beforeModel({ from: 'campaigns.campaign-landing-page' });
+
+        //then
+        sinon.assert.notCalled(route.router.replaceWith);
+        assert.ok(true);
+      });
+
+      test('should override authentication route', async function (assert) {
+        // when
+        await route.beforeModel({ from: 'campaigns.campaign-landing-page' });
+
+        // then
+
+        assert.strictEqual(route.authenticationRoute, 'inscription');
+      });
+
+      test("should call parent's beforeModel and transition to authenticationRoute", async function (assert) {
+        // when
+        await route.beforeModel({ from: 'campaigns.campaign-landing-page' });
+
+        // then
+        sinon.assert.calledWith(
+          sessionStub.requireAuthenticationAndApprovedTermsOfService,
+          { from: 'campaigns.campaign-landing-page' },
+          'inscription',
+        );
+        assert.ok(true);
+      });
+
+      module('when campaign belongs to an oidc provider', function (hooks) {
+        hooks.beforeEach(function () {
+          const oidcProviderService = this.owner.lookup('service:oidcIdentityProviders');
+          const oidcPartner = { id: 'oidc-partner', code: 'OIDC_PARTNER' };
+          oidcProviderService['oidc-partner'] = oidcPartner;
+          oidcProviderService.list = [oidcPartner];
         });
-      });
 
-      module('and user is connected with that provider', function () {
-        test('should not use provider route', async function (assert) {
-          // given
-          route.session.data.externalUser = 'some external user';
-          const OIDC_PARTNER = 'OIDC_PARTNER';
-          route.session.data.authenticated.identityProviderCode = OIDC_PARTNER;
-          campaign.isRestrictedByIdentityProvider.withArgs(OIDC_PARTNER).returns(true);
+        module('and user is not connected with that provider', function () {
+          test('should use provider route', async function (assert) {
+            // given
+            campaign.isRestrictedByIdentityProvider.withArgs('OIDC_PARTNER').returns(true);
 
-          // when
-          await route.beforeModel({ from: 'campaigns.campaign-landing-page' });
+            // when
+            await route.beforeModel({ from: 'campaigns.campaign-landing-page' });
 
-          // then
-          sinon.assert.neverCalledWith(route.router.replaceWith, 'authentication.login-oidc', 'oidc-partner');
-          assert.ok(true);
+            // then
+            sinon.assert.calledWith(route.router.replaceWith, 'authentication.login-oidc', 'oidc-partner');
+            assert.ok(true);
+          });
+        });
+
+        module('and user is connected with that provider', function () {
+          test('should not use provider route', async function (assert) {
+            // given
+            const OIDC_PARTNER = 'OIDC_PARTNER';
+            route.session.data.authenticated.identityProviderCode = OIDC_PARTNER;
+            campaign.isRestrictedByIdentityProvider.withArgs(OIDC_PARTNER).returns(true);
+
+            // when
+            await route.beforeModel({ from: 'campaigns.campaign-landing-page' });
+
+            // then
+            sinon.assert.neverCalledWith(route.router.replaceWith, 'authentication.login-oidc', 'oidc-partner');
+            assert.ok(true);
+          });
         });
       });
     });
 
     module(
       'when campaign is SCO restricted and user is neither authenticated from Pix nor a user from an external platform',
-      function () {
+      function (hooks) {
+        let sessionStub;
+
+        hooks.beforeEach(function () {
+          sessionStub = stubSessionService(this.owner, { isAuthenticated: false });
+          route.session = sessionStub;
+          campaign.isRestricted = true;
+          campaign.organizationType = 'SCO';
+        });
+
         test('should override authentication route with student-sco', async function (assert) {
           // given
-          route.session.isAuthenticated = false;
-          campaign.isRestricted = true;
           campaign.isReconciliationRequired = false;
-          campaign.organizationType = 'SCO';
-          route.session.data.externalUser = undefined;
 
           // when
           await route.beforeModel({ from: 'campaigns.campaign-landing-page' });
@@ -137,11 +140,7 @@ module('Unit | Route | Access', function (hooks) {
 
         test('should not override authentication route when campaign reconciliationRequired', async function (assert) {
           // given
-          route.session.isAuthenticated = false;
-          campaign.isRestricted = true;
           campaign.isReconciliationRequired = true;
-          campaign.organizationType = 'SCO';
-          route.session.data.externalUser = undefined;
 
           // when
           await route.beforeModel({ from: 'campaigns.campaign-landing-page' });
@@ -155,10 +154,10 @@ module('Unit | Route | Access', function (hooks) {
     module('when campaign is SCO restricted and user has been disconnected from sco form', function () {
       test('should override authentication route with student-sco', async function (assert) {
         // given
-        route.session.isAuthenticated = false;
+        const sessionStub = stubSessionService(this.owner, { isAuthenticated: false, isAuthenticatedByGar: true });
+        route.session = sessionStub;
         campaign.isRestricted = true;
         campaign.organizationType = 'SCO';
-        route.session.data.externalUser = 'some external user';
         route.campaignStorage.get.withArgs(campaign.code, 'hasUserSeenJoinPage').returns(true);
 
         // when
@@ -172,8 +171,9 @@ module('Unit | Route | Access', function (hooks) {
     module('when campaign is restricted and user is from an external platform', function () {
       test('should override authentication route with sco-mediacentre', async function (assert) {
         // given
+        const sessionStub = stubSessionService(this.owner, { isAuthenticated: false, isAuthenticatedByGar: true });
+        route.session = sessionStub;
         campaign.isRestricted = true;
-        route.session.data.externalUser = 'some external user';
 
         // when
         await route.beforeModel({ from: 'campaigns.campaign-landing-page' });
@@ -186,6 +186,8 @@ module('Unit | Route | Access', function (hooks) {
     module('when campaign is simplified access and user is not authenticated', function () {
       test('should override authentication route with anonymous', async function (assert) {
         // given
+        const sessionStub = stubSessionService(this.owner, { isAuthenticated: false });
+        route.session = sessionStub;
         campaign.isSimplifiedAccess = true;
         route.session.isAuthenticated = false;
 
