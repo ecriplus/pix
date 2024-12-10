a10b409fd26c15dbe946d2a515b3f52971f33b1f
Laura Bergoens <laura.bergoens@pix.fr>
2024-12-10 06:45:19 +0100

    fix(api): skillRepository.find*ByRecordIds methods do not return duplicates

--- api/src/shared/infrastructure/repositories/skill-repository.js
+++ api/src/shared/infrastructure/repositories/skill-repository.js
@@ -1,3 +1,4 @@
+import { knex } from '../../../../db/knex-database-connection.js';
 import { config } from '../../config.js';
 import { NotFoundError } from '../../domain/errors.js';
 import { Skill } from '../../domain/models/Skill.js';
@@ -64,17 +65,19 @@ export async function findOperativeByCompetenceId(competenceId) {
 export async function findOperativeByCompetenceIds(competenceIds) {
   if (!config.featureToggles.useNewLearningContent)
     return oldSkillRepository.findOperativeByCompetenceIds(competenceIds);
-  const skills = [];
-  for (const competenceId of competenceIds) {
-    const skillsForCompetence = await findOperativeByCompetenceId(competenceId);
-    skills.push(...skillsForCompetence);
-  }
-  return skills.sort(byId);
+  const ids = await knex
+    .pluck('id')
+    .from(TABLE_NAME)
+    .whereIn('competenceId', competenceIds)
+    .whereIn('status', OPERATIVE_STATUSES)
+    .orderBy('id');
+  const skillDtos = await getInstance().loadMany(ids);
+  return skillDtos.map(toDomain);
 }
 
 export async function findOperativeByIds(ids) {
   if (!config.featureToggles.useNewLearningContent) return oldSkillRepository.findOperativeByIds(ids);
-  const skillDtos = await getInstance().loadMany(ids);
+  const skillDtos = await getInstance().getMany(ids);
   return skillDtos
     .filter((skillDto) => skillDto && OPERATIVE_STATUSES.includes(skillDto.status))
     .sort(byId)
@@ -83,7 +86,7 @@ export async function findOperativeByIds(ids) {
 
 export async function findByRecordIds(ids) {
   if (!config.featureToggles.useNewLearningContent) return oldSkillRepository.findByRecordIds(ids);
-  const skillDtos = await getInstance().loadMany(ids);
+  const skillDtos = await getInstance().getMany(ids);
   return skillDtos
     .filter((skillDto) => skillDto)
     .sort(byId)
@@ -92,7 +95,7 @@ export async function findByRecordIds(ids) {
 
 export async function findActiveByRecordIds(ids) {
   if (!config.featureToggles.useNewLearningContent) return oldSkillRepository.findActiveByRecordIds(ids);
-  const skillDtos = await getInstance().loadMany(ids);
+  const skillDtos = await getInstance().getMany(ids);
   return skillDtos
     .filter((skillDto) => skillDto && skillDto.status === ACTIVE_STATUS)
     .sort(byId)
