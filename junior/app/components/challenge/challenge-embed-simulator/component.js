ca78e461ed28bd8ef64c408e13c4bc862f4814c6
rouxxi <gwenvael.laskar@pix.fr>
2024-12-11 15:38:12 +0100

    feat(junior): add reload on embed-simulator

--- junior/app/components/challenge/challenge-embed-simulator/component.js
+++ junior/app/components/challenge/challenge-embed-simulator/component.js
@@ -1,3 +1,4 @@
+import { action } from '@ember/object';
 import { htmlSafe } from '@ember/template';
 import Component from '@glimmer/component';
 
@@ -7,5 +8,31 @@ export default class ChallengeEmbedSimulator extends Component {
     const itemMedia = document.getElementsByClassName('challenge-item__media ')[0];
     const height = this.args.isMediaWithForm ? (baseHeight * itemMedia.offsetWidth) / 710 : baseHeight;
     return htmlSafe(`height: ${height}px; max-height: ${baseHeight}px`);
+  }
+
+  @action
+  setIframeHtmlElement(htmlElement) {
+    this.iframe = htmlElement;
+  }
+
+  @action
+  rebootSimulator() {
+    const iframe = this.iframe;
+    const tmpSrc = iframe.src;
+
+    const loadListener = () => {
+      if (iframe.src === 'about:blank') {
+        // First onload: when we reset the iframe
+        iframe.src = tmpSrc;
+      } else {
+        // Second onload: when we re-assign the iframe's src to its original value
+        iframe.focus();
+        iframe.removeEventListener('load', loadListener);
+      }
+    };
+
+    iframe.addEventListener('load', loadListener);
+
+    iframe.src = 'about:blank';
   }
 }
