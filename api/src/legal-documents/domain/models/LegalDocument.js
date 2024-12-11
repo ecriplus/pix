31c8475e9650d9d302e95903eac59a823a87d09b
Benjamin Petetot <benjamin.petetot@pix.fr>
2024-12-11 16:20:02 +0100

    feat(api): add LegalDocumentStatus in models

--- api/src/legal-documents/domain/models/LegalDocument.js
+++ api/src/legal-documents/domain/models/LegalDocument.js
@@ -1,8 +1,17 @@
+import dayjs from 'dayjs';
+
 export class LegalDocument {
   constructor({ id, type, service, versionAt }) {
     this.id = id;
     this.type = type;
     this.service = service;
     this.versionAt = versionAt;
+  }
+
+  buildDocumentPath() {
+    const service = this.service.toLowerCase();
+    const type = this.type.toLowerCase();
+    const versionAt = dayjs(this.versionAt).format('YYYY-MM-DD');
+    return `${service}-${type}-${versionAt}`;
   }
 }
