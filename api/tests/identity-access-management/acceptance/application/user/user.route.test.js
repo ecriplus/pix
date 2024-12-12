d08fa5858f363fa36be8cabd9da4dc28dcc14984
Quentin Lebouc <quentin.lebouc@pix.fr>
2024-12-12 14:39:58 +0100

    tech(api): move acceptance tests to identity access management context

--- api/tests/identity-access-management/acceptance/application/user/user.route.test.js
+++ api/tests/identity-access-management/acceptance/application/user/user.route.test.js
@@ -662,6 +662,88 @@ describe('Acceptance | Identity Access Management | Application | Route | User',
     });
   });
 
+  describe('PATCH /api/users/{id}/has-seen-challenge-tooltip/{challengeType}', function () {
+    let challengeType, user, options;
+
+    describe('Error cases', function () {
+      beforeEach(function () {
+        challengeType = 'focused';
+        user = databaseBuilder.factory.buildUser({ hasSeenFocusedChallengeTooltip: false });
+
+        options = {
+          method: 'PATCH',
+          url: `/api/users/${user.id}/has-seen-challenge-tooltip/${challengeType}`,
+          headers: { authorization: generateValidRequestAuthorizationHeader(user.id) },
+        };
+
+        return databaseBuilder.commit();
+      });
+
+      it('should respond with a 401 - unauthorized access - if user is not authenticated', async function () {
+        // given
+        options.headers.authorization = 'invalid.access.token';
+
+        // when
+        const response = await server.inject(options);
+
+        // then
+        expect(response.statusCode).to.equal(401);
+      });
+
+      it('should respond with a 403 - forbidden access - if requested user is not the same as authenticated user', async function () {
+        // given
+        const otherUserId = 9999;
+        options.headers.authorization = generateValidRequestAuthorizationHeader(otherUserId);
+
+        // when
+        const response = await server.inject(options);
+
+        // then
+        expect(response.statusCode).to.equal(403);
+      });
+    });
+
+    describe('Success cases', function () {
+      it('should return the user with has seen challenge tooltip', async function () {
+        // given
+        challengeType = 'focused';
+        user = databaseBuilder.factory.buildUser({ hasSeenFocusedChallengeTooltip: false });
+
+        options = {
+          method: 'PATCH',
+          url: `/api/users/${user.id}/has-seen-challenge-tooltip/${challengeType}`,
+          headers: { authorization: generateValidRequestAuthorizationHeader(user.id) },
+        };
+
+        await databaseBuilder.commit();
+        // when
+        const response = await server.inject(options);
+
+        // then
+        expect(response.result.data.attributes['has-seen-focused-challenge-tooltip']).to.be.true;
+      });
+
+      it('should return the user with has seen other challenges tooltip', async function () {
+        // given
+        challengeType = 'other';
+        user = databaseBuilder.factory.buildUser({ hasSeenFocusedChallengeTooltip: false });
+
+        options = {
+          method: 'PATCH',
+          url: `/api/users/${user.id}/has-seen-challenge-tooltip/${challengeType}`,
+          headers: { authorization: generateValidRequestAuthorizationHeader(user.id) },
+        };
+
+        await databaseBuilder.commit();
+        // when
+        const response = await server.inject(options);
+
+        // then
+        expect(response.result.data.attributes['has-seen-other-challenges-tooltip']).to.be.true;
+      });
+    });
+  });
+
   describe('GET /api/users/validate-email', function () {
     let user, token;
 
