b38415a9bff671081d0b377303c6e1182f399b10
LionelB <lionel@lumographe.fr>
2024-12-20 09:12:30 +0100

    feat(api): show both profiles collection and assessment participation for user

--- api/src/prescription/campaign-participation/infrastructure/repositories/participations-for-user-management-repository.js
+++ api/src/prescription/campaign-participation/infrastructure/repositories/participations-for-user-management-repository.js
@@ -1,17 +1,17 @@
 import { knex } from '../../../../../db/knex-database-connection.js';
 import { Assessment } from '../../../../shared/domain/models/Assessment.js';
+import { CampaignTypes } from '../../../shared/domain/constants.js';
 import { CampaignParticipationForUserManagement } from '../../domain/models/CampaignParticipationForUserManagement.js';
 
 const findByUserId = async function (userId) {
   const campaignParticipations = await knex('assessments')
     .select({
-      id: 'assessments.id',
       campaignParticipationId: 'campaign-participations.id',
       participantExternalId: 'campaign-participations.participantExternalId',
       status: 'campaign-participations.status',
       campaignId: 'campaigns.id',
       campaignCode: 'campaigns.code',
-      createdAt: 'assessments.createdAt',
+      createdAt: knex.raw('COALESCE("campaign-participations"."createdAt", assessments."createdAt")'),
       sharedAt: 'campaign-participations.sharedAt',
       deletedAt: 'campaign-participations.deletedAt',
       updatedAt: 'assessments.updatedAt',
@@ -27,9 +27,32 @@ const findByUserId = async function (userId) {
     )
     .where('assessments.userId', userId)
     .where('assessments.type', Assessment.types.CAMPAIGN)
-    .orderBy('campaign-participations.createdAt', 'desc')
+    .union(function () {
+      this.select({
+        campaignParticipationId: 'campaign-participations.id',
+        participantExternalId: 'campaign-participations.participantExternalId',
+        status: 'campaign-participations.status',
+        campaignId: 'campaigns.id',
+        campaignCode: 'campaigns.code',
+        createdAt: 'campaign-participations.createdAt',
+        sharedAt: 'campaign-participations.sharedAt',
+        deletedAt: 'campaign-participations.deletedAt',
+        updatedAt: 'campaign-participations.deletedAt',
+        organizationLearnerFirstName: 'view-active-organization-learners.firstName',
+        organizationLearnerLastName: 'view-active-organization-learners.lastName',
+      })
+        .from('campaign-participations')
+        .leftJoin('campaigns', 'campaigns.id', 'campaign-participations.campaignId')
+        .leftJoin(
+          'view-active-organization-learners',
+          'view-active-organization-learners.id',
+          'campaign-participations.organizationLearnerId',
+        )
+        .where({ 'campaign-participations.userId': userId, type: CampaignTypes.PROFILES_COLLECTION });
+    })
+    .orderBy('createdAt', 'desc')
     .orderBy('campaignCode', 'asc')
-    .orderBy('campaign-participations.sharedAt', 'desc');
+    .orderBy('sharedAt', 'desc');
 
   return campaignParticipations.map((attributes) => new CampaignParticipationForUserManagement(attributes));
 };
