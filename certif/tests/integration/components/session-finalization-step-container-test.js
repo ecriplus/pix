806896d9da13b057ebd4bd0daf00bc3d578cacce
Alexandre COIN <alexandre.pc.coin@gmail.com>
2024-12-31 09:38:30 +0100

    tests(certif): add parameter to component to remove warning

--- certif/tests/integration/components/session-finalization-step-container-test.js
+++ certif/tests/integration/components/session-finalization-step-container-test.js
@@ -9,7 +9,7 @@ module('Integration | Component | session-finalization-step-container', function
 
   test('it renders', async function (assert) {
     // when
-    const screen = await render(hbs`<SessionFinalizationStepContainer @title='Étape 1 : title'>
+    const screen = await render(hbs`<SessionFinalizationStepContainer @title='Étape 1 : title' @iconName='info'>
   template block text
 </SessionFinalizationStepContainer>`);
 
