fc8371427fb8de0abbf27471acb87d94474ca8a2
Alexandre COIN <alexandre.pc.coin@gmail.com>
2025-01-06 11:40:11 +0100

    chore(certif): upgrade ember-cli to 5.12

--- certif/config/environment.js
+++ certif/config/environment.js
@@ -29,6 +29,7 @@ module.exports = function (environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
+        // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
       },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
