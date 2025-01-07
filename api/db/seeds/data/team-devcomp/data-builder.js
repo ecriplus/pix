f104b21eea1524b1a22023dfcf636e31bf74dda5
Eric Lim <eric.lim@zenika.com>
2025-01-07 14:31:48 +0100

    feat(api): use organization learner new seed in data builder
    Co-authored-by: Clément Latzarus <clement.latzarus@pix.fr>
    Co-authored-by: Diane Cordier <diane.cordier@pix.fr>

--- api/db/seeds/data/team-devcomp/data-builder.js
+++ api/db/seeds/data/team-devcomp/data-builder.js
@@ -1,13 +1,17 @@
 import { buildCampaigns } from './build-campaigns.js';
 import { createDevcompOrganization } from './build-organization.js';
+import { createDevcompOrganizationLearners } from './build-organization-learners.js';
 import { buildTargetProfiles } from './build-target-profiles.js';
 import { buildTrainings } from './build-trainings.js';
 
 async function teamDevcompDataBuilder({ databaseBuilder }) {
-  await buildTargetProfiles(databaseBuilder);
   await createDevcompOrganization(databaseBuilder);
+  const learnersCount = await createDevcompOrganizationLearners(databaseBuilder);
+  await databaseBuilder.commit();
+
+  await buildTargetProfiles(databaseBuilder);
   const trainingsIds = await buildTrainings(databaseBuilder);
-  await buildCampaigns(databaseBuilder, trainingsIds);
+  await buildCampaigns(databaseBuilder, trainingsIds, learnersCount);
 }
 
 export { teamDevcompDataBuilder };
