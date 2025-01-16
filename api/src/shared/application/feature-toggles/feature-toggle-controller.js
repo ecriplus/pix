29d04e425c7d14a85999e0e0f0ee7840a56451d6
Benjamin Petetot <benjamin.petetot@pix.fr>
2025-01-16 17:12:29 +0100

    feat(api): add new feature toggles in /api/feature-toggles

--- api/src/shared/application/feature-toggles/feature-toggle-controller.js
+++ api/src/shared/application/feature-toggles/feature-toggle-controller.js
@@ -1,9 +1,14 @@
 import { config } from '../../../../src/shared/config.js';
 import * as network from '../../../identity-access-management/infrastructure/utils/network.js';
+import { featureToggles } from '../../infrastructure/feature-toggles/index.js';
 import * as serializer from '../../infrastructure/serializers/jsonapi/feature-toggle-serializer.js';
 
-const getActiveFeatures = function () {
-  return serializer.serialize(config.featureToggles);
+const getActiveFeatures = async function () {
+  const legacyFeatureToggles = config.featureToggles;
+
+  const newFeatureToggles = await featureToggles.withTag('frontend');
+
+  return serializer.serialize({ ...newFeatureToggles, ...legacyFeatureToggles });
 };
 
 // TODO: Test route to be removed soon
