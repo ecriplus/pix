0711550df273ac33d9825d21f1469b451b2c7e05
mcampourcy <mireille.campourcy@pix.fr>
2025-01-17 15:48:37 +0100

    chore(junior): Update pix-ui 54

--- junior/config/icons.js
+++
@@ -1,8 +0,0 @@
-const getCommonIcons = require('@1024pix/pix-ui/config/icons.js');
-const commonIcons = getCommonIcons();
-
-module.exports = function () {
-  return {
-    'free-solid-svg-icons': [...commonIcons['free-solid-svg-icons'], 'arrows-rotate', 'image', 'video'],
-  };
-};
