dadef4740b22f8d3f4b21cbf884b7f78358ff8f8
Yannick François <yannick.francois@pix.fr>
2025-01-21 10:58:44 +0100

    :truck: chore(api): move CertificationCourseUnrejected event

--- api/src/certification/session-management/domain/usecases/unreject-certification-course.js
+++ api/src/certification/session-management/domain/usecases/unreject-certification-course.js
@@ -1,4 +1,4 @@
-import { CertificationCourseUnrejected } from '../../../../../lib/domain/events/CertificationCourseUnrejected.js';
+import { CertificationCourseUnrejected } from '../../../../shared/domain/events/CertificationCourseUnrejected.js';
 
 export const unrejectCertificationCourse = async ({ certificationCourseId, juryId, certificationCourseRepository }) => {
   const certificationCourse = await certificationCourseRepository.get({ id: certificationCourseId });
