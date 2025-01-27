96c87804bbfa6e5ca447b92b33236d54a886c4b6
AndreiaPena <andreia.pena@pix.fr>
2025-01-27 10:29:51 +0100

    ✨ api: allow invigilator to access kit resource

---
+++ api/tests/certification/session-management/integration/application/pre-handlers/authorization_test.js
@@ -0,0 +1,174 @@
+import { authorization } from '../../../../../../src/certification/session-management/application/pre-handlers/authorization.js';
+import {
+  databaseBuilder,
+  expect,
+  generateValidRequestAuthorizationHeader,
+  HttpTestServer,
+} from '../../../../../test-helper.js';
+
+describe('Certification | Session-Management | Integration | Application | Pre-Handlers | Authorization', function () {
+  describe('#checkUserHaveCertificationCenterMembershipForSession', function () {
+    let httpServerTest;
+
+    beforeEach(async function () {
+      const moduleUnderTest = {
+        name: 'security-test',
+        register: async function (server) {
+          server.route([
+            {
+              method: 'GET',
+              path: '/api/test/sessions/{sessionId}/supervisor-kit',
+              handler: (r, h) => h.response().code(200),
+              config: {
+                pre: [
+                  {
+                    method: authorization.checkUserHaveCertificationCenterMembershipForSession,
+                  },
+                ],
+              },
+            },
+          ]);
+        },
+      };
+      httpServerTest = new HttpTestServer();
+      await httpServerTest.register(moduleUnderTest);
+      httpServerTest.setupAuthentication();
+    });
+
+    context(
+      'when the user is authenticated and has certification center membership for the given session',
+      function () {
+        it('should return 200', async function () {
+          // given
+          const { id: userId } = databaseBuilder.factory.buildUser();
+          const { id: certificationCenterId } = databaseBuilder.factory.buildCertificationCenter();
+          const { id: sessionId } = databaseBuilder.factory.buildSession({
+            certificationCenterId,
+          });
+          databaseBuilder.factory.buildCertificationCenterMembership({
+            certificationCenterId,
+            userId,
+          });
+          await databaseBuilder.commit();
+
+          const options = {
+            method: 'GET',
+            url: `/api/test/sessions/${sessionId}/supervisor-kit`,
+            headers: { authorization: generateValidRequestAuthorizationHeader(userId) },
+          };
+
+          // when
+          const response = await httpServerTest.requestObject(options);
+
+          // then
+          expect(response.statusCode).to.equal(200);
+        });
+      },
+    );
+
+    context(
+      'when the user is authenticated and has no certification center membership for the given session',
+      function () {
+        it('should return 403', async function () {
+          // given
+          const { id: userId } = databaseBuilder.factory.buildUser();
+          const { id: certificationCenterId } = databaseBuilder.factory.buildCertificationCenter();
+          const { id: sessionId } = databaseBuilder.factory.buildSession({
+            certificationCenterId,
+          });
+          await databaseBuilder.commit();
+
+          const options = {
+            method: 'GET',
+            url: `/api/test/sessions/${sessionId}/supervisor-kit`,
+            headers: { authorization: generateValidRequestAuthorizationHeader(userId) },
+          };
+
+          // when
+          const response = await httpServerTest.requestObject(options);
+
+          // then
+          expect(response.statusCode).to.equal(403);
+        });
+      },
+    );
+  });
+
+  describe('#checkUserHaveInvigilatorAccessForSession', function () {
+    let httpServerTest;
+
+    beforeEach(async function () {
+      const moduleUnderTest = {
+        name: 'security-test',
+        register: async function (server) {
+          server.route([
+            {
+              method: 'GET',
+              path: '/api/test/sessions/{sessionId}/supervisor-kit',
+              handler: (r, h) => h.response().code(200),
+              config: {
+                pre: [
+                  {
+                    method: authorization.checkUserHaveInvigilatorAccessForSession,
+                  },
+                ],
+              },
+            },
+          ]);
+        },
+      };
+      httpServerTest = new HttpTestServer();
+      await httpServerTest.register(moduleUnderTest);
+      httpServerTest.setupAuthentication();
+    });
+
+    context('when the user is authenticated and has supervisor access to the given session', function () {
+      it('should return 200', async function () {
+        // given
+        const { id: userId } = databaseBuilder.factory.buildUser();
+        const { id: certificationCenterId } = databaseBuilder.factory.buildCertificationCenter();
+        const { id: sessionId } = databaseBuilder.factory.buildSession({
+          certificationCenterId,
+        });
+        databaseBuilder.factory.buildSupervisorAccess({ userId, sessionId });
+        await databaseBuilder.commit();
+
+        const options = {
+          method: 'GET',
+          url: `/api/test/sessions/${sessionId}/supervisor-kit`,
+          headers: { authorization: generateValidRequestAuthorizationHeader(userId) },
+        };
+
+        // when
+        const response = await httpServerTest.requestObject(options);
+
+        // then
+        expect(response.statusCode).to.equal(200);
+      });
+    });
+
+    context('when the user is authenticated and has no supervisor access to the given session', function () {
+      it('should return 403', async function () {
+        // given
+        const { id: userId } = databaseBuilder.factory.buildUser();
+        const { id: certificationCenterId } = databaseBuilder.factory.buildCertificationCenter();
+        const { id: sessionId } = databaseBuilder.factory.buildSession({
+          certificationCenterId,
+        });
+        await databaseBuilder.commit();
+
+        const options = {
+          method: 'GET',
+          url: `/api/test/sessions/${sessionId}/supervisor-kit`,
+          headers: { authorization: generateValidRequestAuthorizationHeader(userId) },
+        };
+
+        // when
+        const response = await httpServerTest.requestObject(options);
+
+        // then
+        expect(response.statusCode).to.equal(403);
+      });
+    });
+  });
+});
