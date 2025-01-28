169020436c5b0e73ffe02d000257e6a09c666e56
Yvonnick Frin <frin.yvonnick@gmail.com>
2025-01-28 16:22:47 +0100

    refactor(orga): move components from app/components/certificability to GJS

--- orga/app/components/certificability/cell.js
+++
@@ -1,7 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class Cell extends Component {
-  get displayCertificabilityDate() {
-    return !this.args.hideCertifiableDate && this.args.isCertifiable !== null;
-  }
-}
