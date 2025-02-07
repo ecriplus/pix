e73e662cb93d7bc5adef16da6a7b86990a9002b8
Eric Lim <eric.lim@zenika.com>
2025-02-07 11:18:07 +0100

    feat(admin): update training-summary model
    Co-authored-by: Diane Cordier <diane.cordier@pix.fr>

--- admin/app/models/training-summary.js
+++ admin/app/models/training-summary.js
@@ -2,6 +2,7 @@ import Model, { attr } from '@ember-data/model';
 
 export default class TrainingSummary extends Model {
   @attr() title;
+  @attr() internalTitle;
   @attr('number') targetProfilesCount;
   @attr('number') prerequisiteThreshold;
   @attr('number') goalThreshold;
