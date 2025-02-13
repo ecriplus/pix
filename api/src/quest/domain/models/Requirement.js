56b1c66341cf5c94569710cd3269457deffb720e
Laura Bergoens <laura.bergoens@pix.fr>
2025-02-14 00:14:08 +0100

    tech(api): introduce new requirement SkillProfile

--- api/src/quest/domain/models/Requirement.js
+++ api/src/quest/domain/models/Requirement.js
@@ -3,6 +3,7 @@ import { TYPES } from './Eligibility.js';
 import { COMPARISON } from './Quest.js';
 
 export const COMPOSE_TYPE = 'compose';
+export const SKILL_PROFILE_TYPE = 'skillProfile';
 
 function getComparisonFunction(comparison) {
   return comparison === COMPARISON.ONE_OF ? 'some' : 'every';
@@ -66,6 +67,9 @@ export class ComposedRequirement extends BaseRequirement {
     return this.#subRequirements[comparisonFunction]((subRequirement) => subRequirement.isFulfilled(dataInput));
   }
 
+  /**
+   * @returns {Object}
+   */
   toDTO() {
     return {
       ...super.toDTO(),
@@ -110,6 +114,49 @@ export class ObjectRequirement extends BaseRequirement {
   }
 }
 
+export class SkillProfileRequirement extends BaseRequirement {
+  #skillIds;
+  #threshold;
+
+  constructor({ data }) {
+    super({ requirement_type: SKILL_PROFILE_TYPE, comparison: null });
+    this.#skillIds = data.skillIds;
+    this.#threshold = data.threshold;
+  }
+
+  /**
+   * @returns {Object}
+   */
+  get data() {
+    return {
+      skillIds: Object.freeze(this.#skillIds),
+      threshold: this.#threshold,
+    };
+  }
+
+  /**
+   * @param {Eligibility|Success} dataInput
+   * @returns {Boolean}
+   */
+  isFulfilled(dataInput) {
+    const masteryPercentage = dataInput.getMasteryPercentageForSkills(this.#skillIds);
+    return masteryPercentage >= this.#threshold;
+    // la comparaison ici pourrait être celle du requirement ? pour pouvoir avoir de la flexi sur =<>=
+  }
+
+  /**
+   * @returns {Object}
+   */
+  toDTO() {
+    const superDto = super.toDTO();
+    delete superDto.comparison;
+    return {
+      ...superDto,
+      data: this.data,
+    };
+  }
+}
+
 /**
  * @param {Object} params
  * @param {string} params.requirement_type
@@ -123,6 +170,8 @@ export function buildRequirement({ requirement_type, data, comparison }) {
     return new ComposedRequirement({ data, comparison });
   } else if (objectTypes.includes(requirement_type)) {
     return new ObjectRequirement({ requirement_type, data, comparison });
+  } else if (requirement_type === SKILL_PROFILE_TYPE) {
+    return new SkillProfileRequirement({ data });
   }
   throw new Error(`Unknown requirement_type "${requirement_type}"`);
 }
--- api/tests/quest/unit/domain/models/Requirement_test.js
+++ api/tests/quest/unit/domain/models/Requirement_test.js
@@ -7,7 +7,11 @@ import {
   COMPOSE_TYPE,
   ComposedRequirement,
   ObjectRequirement,
+  SKILL_PROFILE_TYPE,
+  SkillProfileRequirement,
 } from '../../../../../src/quest/domain/models/Requirement.js';
+import { Success } from '../../../../../src/quest/domain/models/Success.js';
+import { KnowledgeElement } from '../../../../../src/shared/domain/models/index.js';
 import { expect } from '../../../../test-helper.js';
 
 describe('Quest | Unit | Domain | Models | Requirement ', function () {
@@ -77,6 +81,23 @@ describe('Quest | Unit | Domain | Models | Requirement ', function () {
 
         // then
         expect(requirement instanceof ObjectRequirement).to.be.true;
+      });
+    });
+
+    context('when requirement_type is "skillProfile"', function () {
+      it('should build an SkillProfileRequirement', function () {
+        // given
+        const buildData = {
+          requirement_type: SKILL_PROFILE_TYPE,
+          comparison: 'irrelevant',
+          data: {},
+        };
+
+        // when
+        const requirement = buildRequirement(buildData);
+
+        // then
+        expect(requirement instanceof SkillProfileRequirement).to.be.true;
       });
     });
 
@@ -502,6 +523,97 @@ describe('Quest | Unit | Domain | Models | Requirement ', function () {
             },
           },
           comparison: COMPARISON.ALL,
+        });
+      });
+    });
+  });
+
+  describe('SkillProfileRequirement', function () {
+    describe('isFulfilled', function () {
+      context("when dataInput's masteryPercentage is below threshold", function () {
+        it('should return false', function () {
+          const requirement = new SkillProfileRequirement({
+            data: {
+              skillIds: ['skillB', 'skillA', 'skillC', 'skillE'],
+              threshold: 51,
+            },
+          });
+          const successWith50MasteryPercentage = new Success({
+            knowledgeElements: [
+              { status: KnowledgeElement.StatusType.VALIDATED, skillId: 'skillA' },
+              { status: KnowledgeElement.StatusType.VALIDATED, skillId: 'skillB' },
+              { status: KnowledgeElement.StatusType.INVALIDATED, skillId: 'skillC' },
+              { status: KnowledgeElement.StatusType.INVALIDATED, skillId: 'skillD' },
+            ],
+          });
+
+          expect(requirement.isFulfilled(successWith50MasteryPercentage)).to.be.false;
+        });
+      });
+
+      context("when dataInput's masteryPercentage is equal to threshold", function () {
+        it('should return true', function () {
+          const requirement = new SkillProfileRequirement({
+            data: {
+              skillIds: ['skillB', 'skillA', 'skillC', 'skillE'],
+              threshold: 50,
+            },
+          });
+          const successWith50MasteryPercentage = new Success({
+            knowledgeElements: [
+              { status: KnowledgeElement.StatusType.VALIDATED, skillId: 'skillA' },
+              { status: KnowledgeElement.StatusType.VALIDATED, skillId: 'skillB' },
+              { status: KnowledgeElement.StatusType.INVALIDATED, skillId: 'skillC' },
+              { status: KnowledgeElement.StatusType.INVALIDATED, skillId: 'skillD' },
+            ],
+          });
+
+          expect(requirement.isFulfilled(successWith50MasteryPercentage)).to.be.true;
+        });
+      });
+
+      context("when dataInput's masteryPercentage is above threshold", function () {
+        it('should return true', function () {
+          const requirement = new SkillProfileRequirement({
+            data: {
+              skillIds: ['skillB', 'skillA', 'skillC', 'skillE'],
+              threshold: 49,
+            },
+          });
+          const successWith50MasteryPercentage = new Success({
+            knowledgeElements: [
+              { status: KnowledgeElement.StatusType.VALIDATED, skillId: 'skillA' },
+              { status: KnowledgeElement.StatusType.VALIDATED, skillId: 'skillB' },
+              { status: KnowledgeElement.StatusType.INVALIDATED, skillId: 'skillC' },
+              { status: KnowledgeElement.StatusType.INVALIDATED, skillId: 'skillD' },
+            ],
+          });
+
+          expect(requirement.isFulfilled(successWith50MasteryPercentage)).to.be.true;
+        });
+      });
+    });
+
+    describe('toDTO', function () {
+      it('should transform into a DTO', function () {
+        // given
+        const requirement = new SkillProfileRequirement({
+          data: {
+            skillIds: ['id1', 'id2'],
+            threshold: 70,
+          },
+        });
+
+        // when
+        const DTO = requirement.toDTO();
+
+        // then
+        expect(DTO).to.deep.equal({
+          requirement_type: SKILL_PROFILE_TYPE,
+          data: {
+            skillIds: ['id1', 'id2'],
+            threshold: 70,
+          },
         });
       });
     });
