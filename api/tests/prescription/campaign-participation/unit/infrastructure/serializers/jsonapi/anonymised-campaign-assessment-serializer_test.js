2cf429861ba57346d426919fe5751573a902cc89
LionelB <lionel@lumographe.fr>
2025-02-24 19:08:35 +0100

    feat(api): add AnonymisedCampaignAssessment serializer

---
+++ api/tests/prescription/campaign-participation/unit/infrastructure/serializers/jsonapi/anonymised-campaign-assessment-serializer_test.js
@@ -0,0 +1,34 @@
+import * as serializer from '../../../../../../../src/prescription/campaign-participation/infrastructure/serializers/jsonapi/anonymised-campaign-assessment-serializer.js';
+import { Assessment } from '../../../../../../../src/shared/domain/models/Assessment.js';
+import { domainBuilder, expect } from '../../../../../../test-helper.js';
+
+describe('Unit | Serializer | JSONAPI | anonymised-campaign-assessment-serializer', function () {
+  describe('#serialize()', function () {
+    it('should convert a DetachAssessment model object into JSON API data', function () {
+      // given
+      const assessment = domainBuilder.buildAssessment({
+        id: 123,
+        updatedAt: new Date('2020-10-10'),
+        state: Assessment.states.STARTED,
+        type: Assessment.types.CAMPAIGN,
+      });
+
+      // when
+      const json = serializer.serialize([assessment]);
+
+      // then
+      expect(json).to.deep.equal({
+        data: [
+          {
+            type: 'anonymised-campaign-assessment',
+            id: assessment.id.toString(),
+            attributes: {
+              'updated-at': assessment.updatedAt,
+              state: assessment.state,
+            },
+          },
+        ],
+      });
+    });
+  });
+});
