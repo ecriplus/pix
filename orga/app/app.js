709f29bebb0023817e2cfad9f4dc435c881262ed
Guillaume <guillaume.olejnicak@pix.fr>
2025-02-24 14:42:26 +0100

    tech(orga): expose ember internals in window

--- orga/app/app.js
+++ orga/app/app.js
@@ -1,8 +1,23 @@
+import { RSVP } from '@ember/-internals/runtime';
 import Application from '@ember/application';
+import * as runtime from '@glimmer/runtime';
+import * as tracking from '@glimmer/tracking';
+import * as validator from '@glimmer/validator';
+import Ember from 'ember';
 import loadInitializers from 'ember-load-initializers';
 import Resolver from 'ember-resolver';
 
 import config from './config/environment';
+
+// This is a temporary solution, see https://github.com/emberjs/ember-inspector/issues/2612
+window.define('@glimmer/tracking', () => tracking);
+window.define('@glimmer/runtime', () => runtime);
+window.define('@glimmer/validator', () => validator);
+window.define('rsvp', () => RSVP);
+window.define('ember', () => ({ default: Ember }));
+window.define('<my-app>/config/environment', () => ({
+  default: config,
+}));
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
