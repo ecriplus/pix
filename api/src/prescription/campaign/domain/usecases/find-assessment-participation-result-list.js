09b3493777201655fa34aab409c155528e2e6bb9
Guillaume <guillaume.olejnicak@pix.fr>
2025-02-24 11:50:19 +0100

    tech(api): remove error when filter on the same badge for unacquired and acquired filter

--- api/src/prescription/campaign/domain/usecases/find-assessment-participation-result-list.js
+++ api/src/prescription/campaign/domain/usecases/find-assessment-participation-result-list.js
@@ -1,15 +1,8 @@
-import { AssessmentParticipationResultFilterError } from '../errors.js';
-
-async function findAssessmentParticipationResultList({
+const findAssessmentParticipationResultList = async ({
   campaignId,
   filters,
   page,
   campaignAssessmentParticipationResultListRepository,
-}) {
-  if (filters?.badges?.some((id) => filters?.unacquiredBadges?.includes(id))) {
-    throw new AssessmentParticipationResultFilterError();
-  }
-  return campaignAssessmentParticipationResultListRepository.findPaginatedByCampaignId({ campaignId, filters, page });
-}
+}) => campaignAssessmentParticipationResultListRepository.findPaginatedByCampaignId({ campaignId, filters, page });
 
 export { findAssessmentParticipationResultList };
