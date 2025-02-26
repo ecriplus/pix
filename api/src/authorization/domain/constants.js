a35dfc32ad1202df88647fcda67e0f7056a910e1
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2025-02-26 15:28:54 +0100

    refactor(api): remove now unused constants

--- api/src/authorization/domain/constants.js
+++ api/src/authorization/domain/constants.js
@@ -6,18 +6,11 @@ const PIX_ADMIN = {
     SUPER_ADMIN: 'SUPER_ADMIN',
     SUPPORT: 'SUPPORT',
   },
-  SCOPE: 'pix-admin',
-  TARGET: 'admin',
 };
 
 const PIX_ORGA = {
   NOT_LINKED_ORGANIZATION_MSG:
     "L'accès à Pix Orga est limité aux membres invités. Chaque espace est géré par un administrateur Pix Orga propre à l'organisation qui l'utilise. Contactez-le pour qu'il vous y invite.",
-  SCOPE: 'pix-orga',
 };
 
-const PIX_CERTIF = {
-  SCOPE: 'pix-certif',
-};
-
-export { PIX_ADMIN, PIX_CERTIF, PIX_ORGA };
+export { PIX_ADMIN, PIX_ORGA };
