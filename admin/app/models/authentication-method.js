b838d91313190891f0f2f72bbb7b098c4b3b5fe6
Theotime2005 <theotime.berthod@pix.fr>
2025-02-28 17:39:37 +0100

    feat(admin): Add lastLoggedAt to model
    t

--- admin/app/models/authentication-method.js
+++ admin/app/models/authentication-method.js
@@ -3,4 +3,5 @@ import Model, { attr } from '@ember-data/model';
 export default class AuthenticationMethod extends Model {
   @attr() identityProvider;
   @attr() authenticationComplement;
+  @attr() lastLoggedAt;
 }
