1152646a3429718f9c753a6053afa9b2042211eb
Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>
2025-03-03 21:13:47 +0100

    refactor(api): move unit test from /lib to /src/team

--- api/tests/team/unit/application/membership/membership.route.test.js
+++ api/tests/team/unit/application/membership/membership.route.test.js
@@ -41,4 +41,42 @@ describe('Unit | Team | Application | Route | Membership', function () {
       expect(membershipController.update).to.have.not.been.called;
     });
   });
+
+  describe('POST /api/memberships/{id}/disable', function () {
+    it('should return 204 if user is admin in organization', async function () {
+      // given
+      sinon.stub(securityPreHandlers, 'checkUserIsAdminInOrganization').callsFake((request, h) => h.response(true));
+      sinon.stub(membershipController, 'disable').callsFake((request, h) => h.response().code(204));
+
+      const httpTestServer = new HttpTestServer();
+      await httpTestServer.register(teamRoutes);
+      const membershipId = 123;
+
+      // when
+      const response = await httpTestServer.request('POST', `/api/memberships/${membershipId}/disable`);
+
+      // then
+      expect(response.statusCode).to.equal(204);
+      expect(membershipController.disable).to.have.been.called;
+    });
+
+    it('should return 403 if user is not admin in organization', async function () {
+      // given
+      sinon
+        .stub(securityPreHandlers, 'checkUserIsAdminInOrganization')
+        .callsFake((request, h) => h.response().code(403).takeover());
+      sinon.stub(membershipController, 'disable');
+
+      const httpTestServer = new HttpTestServer();
+      await httpTestServer.register(teamRoutes);
+      const membershipId = 123;
+
+      // when
+      const response = await httpTestServer.request('POST', `/api/memberships/${membershipId}/disable`);
+
+      // then
+      expect(response.statusCode).to.equal(403);
+      expect(membershipController.disable).to.have.not.been.called;
+    });
+  });
 });
