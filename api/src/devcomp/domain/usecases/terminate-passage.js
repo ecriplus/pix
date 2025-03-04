43e0c8f7efb961a80fe428889a0920a6b095a2ab
dianeCdrPix <diane.cordier@pix.fr>
2025-03-04 15:32:27 +0100

    feat(api): record passage-terminated event
    Co-authored-by: Clément Latzarus <clement.latzarus@pix.fr>
    Co-authored-by: Eric Lim <eric.lim@pix.fr>

--- api/src/devcomp/domain/usecases/terminate-passage.js
+++ api/src/devcomp/domain/usecases/terminate-passage.js
@@ -1,13 +1,20 @@
 import { NotFoundError } from '../../../shared/domain/errors.js';
 import { PassageDoesNotExistError, PassageTerminatedError } from '../errors.js';
+import { PassageTerminatedEvent } from '../models/passage-events/passage-events.js';
 
-async function terminatePassage({ passageId, passageRepository }) {
+async function terminatePassage({ passageId, passageRepository, passageEventRepository }) {
   const passage = await _getPassage({ passageId, passageRepository });
   if (passage.terminatedAt) {
     throw new PassageTerminatedError();
   }
   passage.terminate();
-  return passageRepository.update({ passage });
+  const terminatedPassage = await passageRepository.update({ passage });
+  const event = new PassageTerminatedEvent({
+    passageId: terminatedPassage.id,
+    occurredAt: terminatedPassage.terminatedAt,
+  });
+  await passageEventRepository.record(event);
+  return terminatedPassage;
 }
 
 async function _getPassage({ passageId, passageRepository }) {
--- api/tests/devcomp/unit/domain/usecases/terminate-passage_test.js
+++ api/tests/devcomp/unit/domain/usecases/terminate-passage_test.js
@@ -1,4 +1,5 @@
 import { PassageDoesNotExistError, PassageTerminatedError } from '../../../../../src/devcomp/domain/errors.js';
+import { PassageTerminatedEvent } from '../../../../../src/devcomp/domain/models/passage-events/passage-events.js';
 import { terminatePassage } from '../../../../../src/devcomp/domain/usecases/terminate-passage.js';
 import { NotFoundError } from '../../../../../src/shared/domain/errors.js';
 import { catchErr, expect, sinon } from '../../../../test-helper.js';
@@ -45,7 +46,7 @@ describe('Unit | Devcomp | Domain | UseCases | terminate-passage', function () {
         expect(error).to.be.instanceof(PassageTerminatedError);
       });
 
-      it('should call terminate method and update passage and return it', async function () {
+      it('should call terminate method and update passage and return it, then record an event', async function () {
         // given
         const passageId = Symbol('passageId');
 
@@ -53,24 +54,38 @@ describe('Unit | Devcomp | Domain | UseCases | terminate-passage', function () {
           get: sinon.stub(),
           update: sinon.stub(),
         };
+        const passageEventRepository = {
+          record: sinon.stub(),
+        };
+
         const passage = {
           terminatedAt: null,
           terminate: sinon.stub(),
         };
         passageRepository.get.withArgs({ passageId }).resolves(passage);
 
-        const updatedPassage = Symbol();
+        const updatedPassage = {
+          terminatedAt: new Date('2025-03-04'),
+          id: passageId,
+        };
         passageRepository.update.withArgs({ passage }).resolves(updatedPassage);
+
+        const event = new PassageTerminatedEvent({
+          passageId: updatedPassage.id,
+          occurredAt: updatedPassage.terminatedAt,
+        });
 
         // when
         const returnedPassage = await terminatePassage({
           passageId,
           passageRepository,
+          passageEventRepository,
         });
 
         // then
         expect(passage.terminate).to.have.been.calledOnce;
         expect(returnedPassage).to.equal(updatedPassage);
+        expect(passageEventRepository.record).to.have.been.calledOnceWithExactly(event);
       });
     });
   });
