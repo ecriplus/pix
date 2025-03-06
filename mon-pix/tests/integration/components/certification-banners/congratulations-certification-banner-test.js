91001bb91aff6e20c23621705d114342d99b3080
AndreiaPena <andreia.pena@pix.fr>
2025-03-06 18:26:42 +0100

    💄 mon-pix: apply design modification on certifiable page

--- mon-pix/tests/integration/components/certification-banners/congratulations-certification-banner-test.js
+++ mon-pix/tests/integration/components/certification-banners/congratulations-certification-banner-test.js
@@ -1,8 +1,6 @@
 import { render } from '@1024pix/ember-testing-library';
-import { click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { module, test } from 'qunit';
-import sinon from 'sinon';
 
 import setupIntlRenderingTest from '../../../helpers/setup-intl-rendering';
 
@@ -13,43 +11,17 @@ module('Integration | Component | Certification Banners | Congratulations Certif
     // given
     const store = this.owner.lookup('service:store');
     this.set('fullName', 'Fifi Brindacier');
-    this.set('closeBanner', () => {});
     this.set('certificationEligibility', store.createRecord('is-certifiable', {}));
 
     // when
     const screen = await render(
       hbs`<CertificationBanners::CongratulationsCertificationBanner
   @fullName={{this.fullName}}
-  @closeBanner={{this.closeBanner}}
   @certificationEligibility={{this.certificationEligibility}}
 />`,
     );
 
     // then
     assert.ok(screen.getByText('Bravo Fifi Brindacier, votre profil Pix est certifiable.'));
-  });
-
-  test('calls the closeBanner method when closing the banner', async function (assert) {
-    // given
-    const store = this.owner.lookup('service:store');
-    const closeBannerStub = sinon.stub();
-    this.set('closeBanner', closeBannerStub);
-    this.set('fullName', 'Fifi Brindacier');
-    this.set('certificationEligibility', store.createRecord('is-certifiable', {}));
-
-    const screen = await render(
-      hbs`<CertificationBanners::CongratulationsCertificationBanner
-  @fullName={{this.fullName}}
-  @closeBanner={{this.closeBanner}}
-  @certificationEligibility={{this.certificationEligibility}}
-/>`,
-    );
-
-    // when
-    await click(screen.getByRole('button', { name: 'Fermer' }));
-
-    // then
-    sinon.assert.calledOnce(closeBannerStub);
-    assert.ok(true);
   });
 });
