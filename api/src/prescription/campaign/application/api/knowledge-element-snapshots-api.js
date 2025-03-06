ad399043fc0820422b5dc0339ad8a49034478e29
Laura Bergoens <laura.bergoens@pix.fr>
2025-03-06 14:02:45 +0100

    feat(api): expose an API to interact with KnowledgeElementSnapshots

---
+++ api/src/prescription/campaign/application/api/knowledge-element-snapshots-api.js
@@ -0,0 +1,46 @@
+import { KnowledgeElementCollection } from '../../../shared/domain/models/KnowledgeElementCollection.js';
+import { usecases } from '../../domain/usecases/index.js';
+import { KnowledgeElementSnapshot } from './models/KnowledgeElementSnapshot.js';
+/**
+ * @module KnowledgeElementSnapshotAPI
+ */
+
+/**
+ * @typedef KnowledgeElementSnapshotPayload
+ * @type {object}
+ * @property {number} userId
+ * @property {number} campaignParticipationId
+ * @property {Array<KnowledgeElement>} knowledgeElements
+ */
+
+/**
+ * @function
+ * @name save
+ *
+ * @param {KnowledgeElementSnapshotPayload} knowledgeElementSnapshotPayload
+ * @returns {Promise<Boolean>}
+ */
+export async function save(knowledgeElementSnapshotPayload) {
+  await usecases.saveKnowledgeElementSnapshotForParticipation({
+    userId: knowledgeElementSnapshotPayload.userId,
+    snappedAt: new Date(),
+    KnowledgeElementCollection: new KnowledgeElementCollection(knowledgeElementSnapshotPayload.knowledgeElements),
+    campaignParticipationId: knowledgeElementSnapshotPayload.campaignParticipationId,
+  });
+
+  return true;
+}
+
+/**
+ * @function
+ * @name getByParticipation
+ *
+ * @param {number} campaignParticipationId
+ * @returns {Promise<KnowledgeElementSnapshot|null>}
+ */
+export async function getByParticipation(campaignParticipationId) {
+  const knowledgeElementSnapshot = await usecases.getKnowledgeElementSnapshotForParticipation({
+    campaignParticipationId,
+  });
+  return new KnowledgeElementSnapshot(knowledgeElementSnapshot);
+}
