3785ceee4d5b4b0ec7fbf8afc7330c15a8aab5a3
Alexandre COIN <alexandre.pc.coin@gmail.com>
2025-03-10 11:06:33 +0100

    refactor(api): revert FT_V3_CERTIFICATION_ATTESTATION_ENABLED initialization

--- api/tests/shared/acceptance/application/feature-toggles/feature-toggle-controller_test.js
+++ api/tests/shared/acceptance/application/feature-toggles/feature-toggle-controller_test.js
@@ -39,7 +39,6 @@ describe('Acceptance | Shared | Application | Controller | feature-toggle', func
             'show-experimental-missions': false,
             'show-new-campaign-presentation-page': false,
             'show-new-result-page': false,
-            'is-v3-certification-attestation-enabled': false,
           },
         },
       };
