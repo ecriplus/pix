e527da85def3f696a1d5e2db424e21a4a0efcdfa
Yannick François <yannick.francois@pix.fr>
2025-03-12 09:17:10 +0100

    :recycle: chore(api): extract SUP certif center

---
+++ api/db/seeds/data/team-certification/create-sup-certifification-center-only.js
@@ -0,0 +1,33 @@
+import { CertificationCenter } from '../../../../src/shared/domain/models/index.js';
+import * as tooling from '../common/tooling/index.js';
+import { acceptPixOrgaTermsOfService } from '../common/tooling/legal-documents.js';
+import { SUP_CERTIFICATION_CENTER_ID, SUP_EXTERNAL_ID, V3_CERTIFICATION_CENTER_USER_ID } from './constants.js';
+
+export async function supCertificationCenterOnly({ databaseBuilder }) {
+  databaseBuilder.factory.buildUser.withRawPassword({
+    id: V3_CERTIFICATION_CENTER_USER_ID,
+    firstName: 'membre certif v3',
+    lastName: 'Certification',
+    email: 'certifv3@example.net',
+    cgu: true,
+    lang: 'fr',
+    lastTermsOfServiceValidatedAt: new Date(),
+    mustValidateTermsOfService: false,
+    pixCertifTermsOfServiceAccepted: false,
+    hasSeenAssessmentInstructions: false,
+  });
+
+  acceptPixOrgaTermsOfService(databaseBuilder, V3_CERTIFICATION_CENTER_USER_ID);
+
+  await tooling.certificationCenter.createCertificationCenter({
+    databaseBuilder,
+    certificationCenterId: SUP_CERTIFICATION_CENTER_ID,
+    name: 'Centre de certification v3 pro pilote pour la séparation pix/pix+',
+    type: CertificationCenter.types.PRO,
+    externalId: SUP_EXTERNAL_ID,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    members: [{ id: V3_CERTIFICATION_CENTER_USER_ID }],
+    isV3Pilot: true,
+  });
+}
--- api/db/seeds/data/team-certification/data-builder.js
+++ api/db/seeds/data/team-certification/data-builder.js
@@ -1,76 +1,24 @@
-import { CertificationCenter } from '../../../../src/shared/domain/models/index.js';
-import { FEATURE_CAN_REGISTER_FOR_A_COMPLEMENTARY_CERTIFICATION_ALONE_ID } from '../common/constants.js';
 import * as tooling from '../common/tooling/index.js';
 import { acceptPixOrgaTermsOfService } from '../common/tooling/legal-documents.js';
-import {
-  CERTIFIABLE_SUCCESS_USER_ID,
-  complementaryCertificationIds,
-  PRO_PILOT_CERTIFICATION_CENTER_ID,
-  V3_CERTIFICATION_CENTER_ID,
-  V3_CERTIFICATION_CENTER_USER_ID,
-  V3_PRO_PILOT_EXTERNAL_ID,
-} from './constants.js';
+import { CERTIFIABLE_SUCCESS_USER_ID } from './constants.js';
 import { proOrganizationWithCertifCenter } from './create-pro-organization-with-certif-center.js';
 import { scoOrganizationManaginAgriStudentsWithFregata } from './create-sco-organization-managing-agri-student-with-fregata.js';
 import { scoOrganizationManaginStudentsWithSiecle } from './create-sco-organization-managing-student-with-siecle.js';
 import { scoOrganizationNotManagingStudents } from './create-sco-organization-not-managing-students.js';
+import { supCertificationCenterOnly } from './create-sup-certifification-center-only.js';
 import { setupConfigurations } from './setup-configuration.js';
 
 async function teamCertificationDataBuilder({ databaseBuilder }) {
   await scoOrganizationManaginAgriStudentsWithFregata({ databaseBuilder });
   await scoOrganizationManaginStudentsWithSiecle({ databaseBuilder });
   await proOrganizationWithCertifCenter({ databaseBuilder });
+  await supCertificationCenterOnly({ databaseBuilder });
   await scoOrganizationNotManagingStudents({ databaseBuilder });
-  await _createV3PilotCertificationCenter({ databaseBuilder });
   await _createSuccessCertifiableUser({ databaseBuilder });
   await setupConfigurations({ databaseBuilder });
 }
 
 export { teamCertificationDataBuilder };
-
-async function _createV3PilotCertificationCenter({ databaseBuilder }) {
-  databaseBuilder.factory.buildUser.withRawPassword({
-    id: V3_CERTIFICATION_CENTER_USER_ID,
-    firstName: 'membre certif v3',
-    lastName: 'Certification',
-    email: 'certifv3@example.net',
-    cgu: true,
-    lang: 'fr',
-    lastTermsOfServiceValidatedAt: new Date(),
-    mustValidateTermsOfService: false,
-    pixCertifTermsOfServiceAccepted: false,
-    hasSeenAssessmentInstructions: false,
-  });
-
-  acceptPixOrgaTermsOfService(databaseBuilder, V3_CERTIFICATION_CENTER_USER_ID);
-
-  await tooling.certificationCenter.createCertificationCenter({
-    databaseBuilder,
-    certificationCenterId: V3_CERTIFICATION_CENTER_ID,
-    name: 'Centre de certification v3',
-    type: CertificationCenter.types.PRO,
-    externalId: null,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    members: [{ id: V3_CERTIFICATION_CENTER_USER_ID }],
-    isV3Pilot: true,
-    complementaryCertificationIds: [],
-  });
-
-  await tooling.certificationCenter.createCertificationCenter({
-    databaseBuilder,
-    certificationCenterId: PRO_PILOT_CERTIFICATION_CENTER_ID,
-    name: 'Centre de certification v3 pro pilote pour la séparation pix/pix+',
-    type: CertificationCenter.types.PRO,
-    externalId: V3_PRO_PILOT_EXTERNAL_ID,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    members: [{ id: V3_CERTIFICATION_CENTER_USER_ID }],
-    isV3Pilot: true,
-    complementaryCertificationIds,
-    featureIds: [FEATURE_CAN_REGISTER_FOR_A_COMPLEMENTARY_CERTIFICATION_ALONE_ID],
-  });
-}
 
 async function _createSuccessCertifiableUser({ databaseBuilder }) {
   const userId = databaseBuilder.factory.buildUser.withRawPassword({
