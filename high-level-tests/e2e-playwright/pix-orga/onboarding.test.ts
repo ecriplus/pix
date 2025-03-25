d06f748cfd702d0c12c5055a86df89d2280245d3
rouxxi <gwenvael.laskar@pix.fr>
2025-03-25 16:28:36 +0100

    test(e2e): add onboarding test with existing account
     Co-authored-by: Guillaume Lagorce <guillame.lagorce@pix.fr>

--- high-level-tests/e2e-playwright/pix-orga/onboarding.test.ts
+++ high-level-tests/e2e-playwright/pix-orga/onboarding.test.ts
@@ -3,18 +3,12 @@ import { expect } from '@playwright/test';
 import { databaseBuilder } from '../helpers/db.ts';
 import { test } from '../helpers/fixtures.ts';
 
-let invitation: { id: string; code: string };
-const email = 'admin@example.net';
-
-test.beforeEach(async function () {
-  invitation = databaseBuilder.factory.buildOrganizationInvitation({
-    email,
-  });
-  await databaseBuilder.commit();
-});
-
 test('Join an organization without having an account', async function ({ page }) {
+  const invitation = databaseBuilder.factory.buildOrganizationInvitation();
+  await databaseBuilder.commit();
+
   await page.goto(`/rejoindre?invitationId=${invitation.id}&code=${invitation.code}`);
+
   await expect(page.getByText('Vous êtes invité(e) à')).toBeVisible();
   await page.getByRole('textbox', { name: 'Prénom' }).fill('mini');
   await page.getByRole('textbox', { name: 'Nom', exact: true }).fill('pixou');
@@ -23,6 +17,25 @@ test('Join an organization without having an account', async function ({ page })
   await page.getByRole('checkbox', { name: "Accepter les conditions d'utilisation de Pix" }).check();
   await page.getByRole('button', { name: "Je m'inscris" }).click();
   await page.getByRole('heading', { name: "Veuillez accepter nos Conditions Générales d'Utilisation" }).waitFor();
+  await page.getByRole('button', { name: 'Accepter et continuer' }).click();
+  await expect(page.getByRole('heading', { name: 'Campagnes' })).toBeVisible();
+});
+test('Join an organization with an existing account', async function ({ page }) {
+  const invitation = databaseBuilder.factory.buildOrganizationInvitation();
+  databaseBuilder.factory.buildUser.withRawPassword({
+    email: 'random-account@example.net',
+  });
+  await databaseBuilder.commit();
+
+  await page.goto(`/rejoindre?invitationId=${invitation.id}&code=${invitation.code}`);
+
+  await page.getByRole('button', { name: 'Se connecter' }).click();
+  await page.getByRole('textbox', { name: 'Adresse e-mail' }).fill('random-account@example.net');
+  await page.getByRole('textbox', { name: 'Mot de passe' }).fill('pix123');
+  await page.getByRole('button', { name: 'Je me connecte' }).click();
+  await expect(
+    page.getByRole('heading', { name: "Veuillez accepter nos Conditions Générales d'Utilisation" }),
+  ).toBeVisible();
   await page.getByRole('button', { name: 'Accepter et continuer' }).click();
   await expect(page.getByRole('heading', { name: 'Campagnes' })).toBeVisible();
 });
