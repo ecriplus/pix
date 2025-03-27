7897f62efee15a2cdd5dd0daa898b4c81d1893fb
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2025-03-27 08:53:15 +0100

    tech?(api): add comment on user api

--- api/src/identity-access-management/application/api/users-api.js
+++ api/src/identity-access-management/application/api/users-api.js
@@ -4,10 +4,26 @@ import { UserDTO } from './models/UserDTO.js';
  * @module UserApi
  */
 
+/**
+ * @function
+ * @name markNewDashboardInfoAsSeen
+ *
+ * @param {Object} params
+ * @param {Number} params.userId
+ * @returns {Promise}
+ */
 export const markNewDashboardInfoAsSeen = async ({ userId }) => {
   return usecases.markUserHasSeenNewDashboardInfo({ userId });
 };
 
+/**
+ * @function
+ * @name markAssessmentInstructionsInfoAsSeen
+ *
+ * @param {Object} params
+ * @param {Number} params.userId
+ * @returns {Promise}
+ */
 export const markAssessmentInstructionsInfoAsSeen = async ({ userId }) => {
   return usecases.markAssessmentInstructionsInfoAsSeen({ userId });
 };
