ca44495cb60afe968c5db7ab5d8ba68630a4b678
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2025-03-28 16:09:06 +0100

    tech(mon-pix): remove hasSeenEndTestScreen infos

--- mon-pix/mirage/serializers/certification-course.js
+++ mon-pix/mirage/serializers/certification-course.js
@@ -1,15 +1,7 @@
 import ApplicationSerializer from './application';
 
 export default ApplicationSerializer.extend({
-  attrs: [
-    'nbChallenges',
-    'examinerComment',
-    'hasSeenEndTestScreen',
-    'firstName',
-    'lastName',
-    'version',
-    'isAdjustedForAccessibility',
-  ],
+  attrs: ['nbChallenges', 'examinerComment', 'firstName', 'lastName', 'version', 'isAdjustedForAccessibility'],
   links(certificationCourse) {
     return {
       assessment: {
