7eae9cb0f08c4057add0e313222f5ff302df5588
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2025-04-02 16:06:10 +0200

    tech(orga): implem banner inside layout

--- orga/tests/unit/controllers/application-test.js
+++ orga/tests/unit/controllers/application-test.js
@@ -2,12 +2,12 @@ import { setupTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 import sinon from 'sinon';
 
-module('Unit | Controller | authenticated', function (hooks) {
+module('Unit | Controller | application', function (hooks) {
   setupTest(hooks);
 
   test('should call send method', async function (assert) {
     // given
-    const controller = this.owner.lookup('controller:authenticated');
+    const controller = this.owner.lookup('controller:application');
     controller.send = sinon.stub();
 
     // when
