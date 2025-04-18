09e973b76f13b448aa9b044bf5f49cc6fb4fd13d
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2025-04-18 09:48:29 +0200

    feat(api): remove usage of userId on stageAcquisition

--- api/tests/tooling/domain-builder/factory/build-stage-acquisition.js
+++ api/tests/tooling/domain-builder/factory/build-stage-acquisition.js
@@ -1,14 +1,8 @@
 import { StageAcquisition } from '../../../../src/evaluation/domain/models/StageAcquisition.js';
 
-const buildStageAcquisition = function ({
-  id = 1,
-  userId = 3000,
-  stageId = 4000,
-  campaignParticipationId = 5000,
-} = {}) {
+const buildStageAcquisition = function ({ id = 1, stageId = 4000, campaignParticipationId = 5000 } = {}) {
   return new StageAcquisition({
     id,
-    userId,
     stageId,
     campaignParticipationId,
   });
