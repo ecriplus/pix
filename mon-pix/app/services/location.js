4c4602a30b7a21b4aaaec0cd2d97bb8d27b55204
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2025-04-19 09:12:52 +0200

    refactor(mon-pix): remove useless return and explicitly use window

--- mon-pix/app/services/location.js
+++ mon-pix/app/services/location.js
@@ -2,6 +2,6 @@ import Service from '@ember/service';
 
 export default class LocationService extends Service {
   replace(url) {
-    return location.replace(url);
+    window.location.replace(url);
   }
 }
