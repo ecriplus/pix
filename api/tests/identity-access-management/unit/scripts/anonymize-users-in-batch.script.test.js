d8c75f9bec21d94659a763a60c86599ca01cc58d
Theotime2005 <theotime.berthod@pix.fr>
2025-04-28 11:43:47 +0200

    feat(api): create script to anonymize users in batch

---
+++ api/tests/identity-access-management/unit/scripts/anonymize-users-in-batch.script.test.js
@@ -0,0 +1,90 @@
+import { AnonymizeUsersInBatchScript } from '../../../../src/identity-access-management/scripts/anonymize-users-in-batch.script.js';
+import { expect, sinon } from '../../../test-helper.js';
+
+describe('Unit | Identities Access Management | Scripts | Anonymize users in batch', function () {
+  let script;
+  let logger;
+  let anonymizeUser;
+
+  beforeEach(function () {
+    script = new AnonymizeUsersInBatchScript();
+    logger = {
+      info: sinon.spy(),
+      error: sinon.spy(),
+    };
+    anonymizeUser = sinon.stub();
+  });
+
+  describe('#handle', function () {
+    context('dryRun mode', function () {
+      it('displays the dryRun message', async function () {
+        // given
+        const file = [{ userId: 1 }, { userId: 2 }, { userId: 3 }];
+
+        // when
+        await script.handle({
+          options: {
+            file,
+            dryRun: true,
+            anonymizerId: 1234,
+          },
+          logger,
+          anonymizeUser,
+        });
+
+        // then
+        expect(logger.info).to.have.been.calledWith(`DryRun mode, 3 user accounts to be processed.`);
+      });
+    });
+
+    context('without dryRun mode', function () {
+      it('returns anonymizes successfully', async function () {
+        // given
+        const file = [{ userId: 1 }, { userId: 2 }, { userId: 3 }];
+
+        // when
+        await script.handle({
+          options: {
+            file,
+            dryRun: false,
+            anonymizerId: 1234,
+          },
+          logger,
+          anonymizeUser,
+        });
+
+        // then
+        expect(anonymizeUser.callCount).to.equal(3);
+        expect(logger.info).to.have.been.calledWith('3 user accounts processed successfully.');
+      });
+
+      context('when there are errors', function () {
+        it('continues the process and reports the errors', async function () {
+          // given
+          const file = [{ userId: 1 }, { userId: 2 }, { userId: 3 }];
+          const error = new Error('An error occurred');
+          anonymizeUser.onCall(1).rejects(error);
+
+          // when // then
+          await expect(
+            script.handle({
+              options: {
+                file,
+                dryRun: false,
+                anonymizerId: 1234,
+              },
+              logger,
+              anonymizeUser,
+            }),
+          ).to.be.rejectedWith('There was some errors during the process.');
+
+          // then
+          expect(anonymizeUser.callCount).to.equal(file.length);
+          expect(logger.info).to.have.been.calledWith(`2 user accounts processed successfully.`);
+          expect(logger.error).to.have.been.calledWith(`Error on line 2. ${error}`);
+          expect(script.successLines).to.equal(2);
+        });
+      });
+    });
+  });
+});
