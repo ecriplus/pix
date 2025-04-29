7b7739faae9a6d41689b001d1fadaa1e9af4b343
dianeCdrPix <diane.cordier@pix.fr>
2025-04-29 11:49:14 +0200

    feat(mon-pix): add custom createRecord method
    in passage adapter
    
    Co-authored-by: Diane Cordier <diane.cordier@pix.fr>

--- mon-pix/app/adapters/passage.js
+++ mon-pix/app/adapters/passage.js
@@ -1,6 +1,20 @@
 import ApplicationAdapter from './application';
 
 export default class Passage extends ApplicationAdapter {
+  createRecord(store, type, snapshot) {
+    const passageData = this.serialize(snapshot);
+    const { moduleVersion, occurredAt, sequenceNumber } = snapshot.adapterOptions;
+    passageData.data.attributes = {
+      ...passageData.data.attributes,
+      'module-version': moduleVersion,
+      'occurred-at': occurredAt,
+      'sequence-number': sequenceNumber,
+    };
+
+    const url = this.buildURL(type.modelName, null, snapshot, 'createRecord');
+    return this.ajax(url, 'POST', { data: passageData });
+  }
+
   async terminate({ passageId }) {
     const url = `${this.host}/${this.namespace}/passages/${passageId}/terminate`;
     await this.ajax(url, 'POST');
--- mon-pix/tests/unit/adapters/passage-test.js
+++ mon-pix/tests/unit/adapters/passage-test.js
@@ -5,6 +5,62 @@ import sinon from 'sinon';
 module('Unit | Adapter | Module | Passage', function (hooks) {
   setupTest(hooks);
 
+  module('#createRecord', function () {
+    test('should trigger an ajax call with the right url and method', async function (assert) {
+      // given
+      const moduleId = Symbol('moduleId');
+      const sequenceNumber = 1;
+      const occurredAt = Symbol('timestamp');
+      const moduleVersion = Symbol('moduleVersion');
+      const adapterOptions = {
+        moduleId,
+        moduleVersion,
+        occurredAt,
+        sequenceNumber,
+      };
+      const snapshot = {
+        adapterOptions,
+        serialize: function () {
+          return {
+            data: {
+              attributes: {
+                'module-id': moduleId,
+              },
+              type: 'passages',
+            },
+          };
+        },
+      };
+
+      const store = this.owner.lookup('service:store');
+      const adapter = this.owner.lookup('adapter:passage');
+      const type = { modelName: 'passage' };
+      sinon.stub(adapter, 'ajax').resolves();
+      const expectedUrl = `http://localhost:3000/api/passages`;
+
+      // when
+      await adapter.createRecord(store, type, snapshot);
+
+      // then
+      const expectedPayload = {
+        data: {
+          data: {
+            attributes: {
+              'module-id': moduleId,
+              'module-version': moduleVersion,
+              'sequence-number': sequenceNumber,
+              'occurred-at': occurredAt,
+            },
+            type: 'passages',
+          },
+        },
+      };
+
+      sinon.assert.calledWith(adapter.ajax, expectedUrl, 'POST', expectedPayload);
+      assert.ok(true);
+    });
+  });
+
   module('#terminate', function () {
     test('should trigger an ajax call with the right url and method', async function (assert) {
       // given
