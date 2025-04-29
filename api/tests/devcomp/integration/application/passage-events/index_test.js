bda5c73fc5232e22b7f51589debf5a3b5105cfa0
Eric Lim <eric.lim@pix.fr>
2025-04-29 17:58:01 +0200

    feat(api): add joi validation
    in passage-events route

---
+++ api/tests/devcomp/integration/application/passage-events/index_test.js
@@ -0,0 +1,42 @@
+import * as moduleUnderTest from '../../../../../src/devcomp/application/passage-events/index.js';
+import { expect } from '../../../../test-helper.js';
+import { HttpTestServer } from '../../../../tooling/server/http-test-server.js';
+
+describe('Integration | Devcomp | Application | Module | Router | passage-events-router', function () {
+  describe('POST /api/passage-events', function () {
+    context('when payload is empty', function () {
+      it('should return a 400 HTTP code error', async function () {
+        // given
+        const httpTestServer = new HttpTestServer();
+        await httpTestServer.register(moduleUnderTest);
+        const invalidPayload = {};
+
+        // when
+        const response = await httpTestServer.request('POST', '/api/passage-events', invalidPayload);
+
+        // then
+        expect(response.statusCode).to.equal(400);
+      });
+    });
+    context('when payload does not pass validation', function () {
+      it('should return a 400 HTTP code error', async function () {
+        // given
+        const httpTestServer = new HttpTestServer();
+        await httpTestServer.register(moduleUnderTest);
+        const invalidPayload = {
+          data: {
+            attributes: {
+              'passage-events': [],
+            },
+          },
+        };
+
+        // when
+        const response = await httpTestServer.request('POST', '/api/passage-events', invalidPayload);
+
+        // then
+        expect(response.statusCode).to.equal(400);
+      });
+    });
+  });
+});
