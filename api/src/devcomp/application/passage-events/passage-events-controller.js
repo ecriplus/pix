95b16471b7e7f8fc32d90c1983b1b15139673e5f
Eric Lim <eric.lim@pix.fr>
2025-04-30 15:04:42 +0200

    feat(api): passing userId to recordPassageEvents usecase
    Co-authored-by: Diane Cordier <diane.cordier@pix.fr>
    Co-authored-by: Alice Goarnisson <alice.goarnisson@pix.fr>

--- api/src/devcomp/application/passage-events/passage-events-controller.js
+++ api/src/devcomp/application/passage-events/passage-events-controller.js
@@ -1,10 +1,14 @@
 import { BadRequestError } from '../../../shared/application/http-errors.js';
 import { DomainError } from '../../../shared/domain/errors.js';
+import { requestResponseUtils } from '../../../shared/infrastructure/utils/request-response-utils.js';
 
 const create = async function (request, h, { usecases, passageEventSerializer }) {
   try {
     const passageEvents = await passageEventSerializer.deserialize(request.payload);
-    await usecases.recordPassageEvents({ events: passageEvents });
+    await usecases.recordPassageEvents({
+      events: passageEvents,
+      userId: requestResponseUtils.extractUserIdFromRequest(request),
+    });
 
     return h.response().created();
   } catch (error) {
