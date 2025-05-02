5f999f4adcea8d87705bf4075c61e50154b3f2e8
alicegoarnisson <alice.goarnisson@pix.fr>
2025-05-02 16:05:37 +0200

    refactor(api): removed _buildPassageEvent from recordPassageEvent usecase

--- api/src/devcomp/domain/usecases/record-passage-events.js
+++ api/src/devcomp/domain/usecases/record-passage-events.js
@@ -1,42 +1,14 @@
 import { DomainError } from '../../../shared/domain/errors.js';
 import { PromiseUtils } from '../../../shared/infrastructure/utils/promise-utils.js';
-import {
-  FlashcardsCardAutoAssessedEvent,
-  FlashcardsRectoReviewedEvent,
-  FlashcardsRetriedEvent,
-  FlashcardsStartedEvent,
-  FlashcardsVersoSeenEvent,
-} from '../models/passage-events/flashcard-events.js';
-import { PassageStartedEvent, PassageTerminatedEvent } from '../models/passage-events/passage-events.js';
+import { PassageEventFactory } from '../factories/passage-event-factory.js';
 
 const recordPassageEvents = async function ({ events, userId, passageRepository, passageEventRepository }) {
   await PromiseUtils.mapSeries(events, async (event) => {
-    const passageEvent = _buildPassageEvent(event);
+    const passageEvent = PassageEventFactory.build(event);
     await _validatePassage({ event, userId, passageRepository });
     await passageEventRepository.record(passageEvent);
   });
 };
-
-function _buildPassageEvent(event) {
-  switch (event.type) {
-    case 'PASSAGE_STARTED':
-      return new PassageStartedEvent(event);
-    case 'PASSAGE_TERMINATED':
-      return new PassageTerminatedEvent(event);
-    case 'FLASHCARDS_STARTED':
-      return new FlashcardsStartedEvent(event);
-    case 'FLASHCARDS_VERSO_SEEN':
-      return new FlashcardsVersoSeenEvent(event);
-    case 'FLASHCARDS_CARD_AUTO_ASSESSED':
-      return new FlashcardsCardAutoAssessedEvent(event);
-    case 'FLASHCARDS_RECTO_REVIEWED':
-      return new FlashcardsRectoReviewedEvent(event);
-    case 'FLASHCARDS_RETRIED':
-      return new FlashcardsRetriedEvent(event);
-    default:
-      throw new DomainError(`Passage event with type ${event.type} does not exist`);
-  }
-}
 
 async function _validatePassage({ event, userId, passageRepository }) {
   const passage = await passageRepository.get({ passageId: event.passageId });
--- api/tests/devcomp/unit/domain/usecases/record-passage-events_test.js
+++ api/tests/devcomp/unit/domain/usecases/record-passage-events_test.js
@@ -1,15 +1,5 @@
 import { Passage } from '../../../../../src/devcomp/domain/models/Passage.js';
-import {
-  FlashcardsCardAutoAssessedEvent,
-  FlashcardsRectoReviewedEvent,
-  FlashcardsRetriedEvent,
-  FlashcardsStartedEvent,
-  FlashcardsVersoSeenEvent,
-} from '../../../../../src/devcomp/domain/models/passage-events/flashcard-events.js';
-import {
-  PassageStartedEvent,
-  PassageTerminatedEvent,
-} from '../../../../../src/devcomp/domain/models/passage-events/passage-events.js';
+import { FlashcardsVersoSeenEvent } from '../../../../../src/devcomp/domain/models/passage-events/flashcard-events.js';
 import { recordPassageEvents } from '../../../../../src/devcomp/domain/usecases/record-passage-events.js';
 import { DomainError, NotFoundError } from '../../../../../src/shared/domain/errors.js';
 import { catchErr, domainBuilder, expect, sinon } from '../../../../test-helper.js';
@@ -26,65 +16,7 @@ describe('Unit | Devcomp | Domain | UseCases | record-passage-events', function 
       type: 'FLASHCARDS_VERSO_SEEN',
     };
 
-    const flashcardsStartedEvent = {
-      occurredAt: new Date(),
-      passageId: 2,
-      sequenceNumber: 1,
-      elementId: '5ad40bc9-8b5c-47ee-b893-f8ab1a1b8095',
-      type: 'FLASHCARDS_STARTED',
-    };
-
-    const flashcardsCardAutoAssessedEvent = {
-      occurredAt: new Date(),
-      passageId: 2,
-      sequenceNumber: 1,
-      elementId: '5ad40bc9-8b5c-47ee-b893-f8ab1a1b8095',
-      cardId: 'c4675f66-97f1-4202-8aeb-0388edf102d5',
-      type: 'FLASHCARDS_CARD_AUTO_ASSESSED',
-      autoAssessment: 'yes',
-    };
-
-    const flashcardsRectoReviewedEvent = {
-      occurredAt: new Date(),
-      passageId: 2,
-      sequenceNumber: 1,
-      elementId: '5ad40bc9-8b5c-47ee-b893-f8ab1a1b8095',
-      cardId: 'c4675f66-97f1-4202-8aeb-0388edf102d5',
-      type: 'FLASHCARDS_RECTO_REVIEWED',
-    };
-
-    const flashcardsRetriedEvent = {
-      occurredAt: new Date(),
-      passageId: 2,
-      sequenceNumber: 1,
-      elementId: '5ad40bc9-8b5c-47ee-b893-f8ab1a1b8095',
-      type: 'FLASHCARDS_RETRIED',
-    };
-
-    const passageTerminatedEvent = {
-      occurredAt: new Date(),
-      passageId: 2,
-      sequenceNumber: 1,
-      type: 'PASSAGE_TERMINATED',
-    };
-
-    const passageStartedEvent = {
-      occurredAt: new Date(),
-      passageId: 2,
-      sequenceNumber: 1,
-      contentHash: 'module-version',
-      type: 'PASSAGE_STARTED',
-    };
-
-    const events = [
-      flashcardsVersoSeenEvent,
-      flashcardsStartedEvent,
-      flashcardsCardAutoAssessedEvent,
-      flashcardsRectoReviewedEvent,
-      flashcardsRetriedEvent,
-      passageTerminatedEvent,
-      passageStartedEvent,
-    ];
+    const events = [flashcardsVersoSeenEvent];
 
     const passage = domainBuilder.devcomp.buildPassage({ id: 2 });
     const passageEventRepositoryStub = {
@@ -104,57 +36,10 @@ describe('Unit | Devcomp | Domain | UseCases | record-passage-events', function 
 
     // then
     const flashcardsVersoSeenPassageEvent = new FlashcardsVersoSeenEvent(flashcardsVersoSeenEvent);
-    const flashcardsStartedPassageEvent = new FlashcardsStartedEvent(flashcardsStartedEvent);
-    const flashcardsCardAutoAssessedPassageEvent = new FlashcardsCardAutoAssessedEvent(flashcardsCardAutoAssessedEvent);
-    const flashcardsRectoReviewedEventPassageEvent = new FlashcardsRectoReviewedEvent(flashcardsRectoReviewedEvent);
-    const flashcardsRetriedEventPassageEvent = new FlashcardsRetriedEvent(flashcardsRetriedEvent);
-    const passageTerminatedPassageEvent = new PassageTerminatedEvent(passageTerminatedEvent);
-    const passageStartedPassageEvent = new PassageStartedEvent(passageStartedEvent);
 
     expect(passageEventRepositoryStub.record.getCall(0)).to.have.been.calledWithExactly(
       flashcardsVersoSeenPassageEvent,
     );
-    expect(passageEventRepositoryStub.record.getCall(1)).to.have.been.calledWithExactly(flashcardsStartedPassageEvent);
-    expect(passageEventRepositoryStub.record.getCall(2)).to.have.been.calledWithExactly(
-      flashcardsCardAutoAssessedPassageEvent,
-    );
-    expect(passageEventRepositoryStub.record.getCall(3)).to.have.been.calledWithExactly(
-      flashcardsRectoReviewedEventPassageEvent,
-    );
-    expect(passageEventRepositoryStub.record.getCall(4)).to.have.been.calledWithExactly(
-      flashcardsRetriedEventPassageEvent,
-    );
-    expect(passageEventRepositoryStub.record.getCall(5)).to.have.been.calledWithExactly(passageTerminatedPassageEvent);
-    expect(passageEventRepositoryStub.record.getCall(6)).to.have.been.calledWithExactly(passageStartedPassageEvent);
-  });
-
-  context('when type of passage event does not exist', function () {
-    it('should throw an error', async function () {
-      // given
-      const event = {
-        type: 'NON_EXISTING_TYPE',
-      };
-
-      const passageRepositoryStub = {
-        get: sinon.stub(),
-      };
-      const passageEventRepositoryStub = {
-        record: sinon.stub().resolves(),
-      };
-
-      // when
-      const error = await catchErr(recordPassageEvents)({
-        events: [event],
-        userId: null,
-        passageRepository: passageRepositoryStub,
-        passageEventRepository: passageEventRepositoryStub,
-      });
-
-      // then
-      expect(error).to.be.instanceOf(DomainError);
-      expect(error.message).to.equal(`Passage event with type ${event.type} does not exist`);
-      expect(passageEventRepositoryStub.record).to.not.have.been.called;
-    });
   });
 
   context('when there is no passage for given passage id', function () {
