f7e90705461504efd2d663e34002e32ea8a576cd
alicegoarnisson <alice.goarnisson@pix.fr>
2025-05-05 15:48:16 +0200

    feat(mon-pix): add PassageEvents service

---
+++ mon-pix/tests/unit/services/passage-events-test.js
@@ -0,0 +1,84 @@
+import { setupTest } from 'ember-qunit';
+import { module, test } from 'qunit';
+import sinon from 'sinon';
+
+module('Unit | Services | PassageEvents', function (hooks) {
+  setupTest(hooks);
+
+  let clock;
+  const now = new Date('2019-04-28T02:42:00Z');
+
+  hooks.beforeEach(function () {
+    clock = sinon.useFakeTimers({ now });
+  });
+
+  hooks.afterEach(function () {
+    clock.restore();
+  });
+
+  module('#record', function () {
+    test('it should record a passageEvent', async function (assert) {
+      // given
+      const passageEventService = this.owner.lookup('service:passageEvents');
+
+      const store = this.owner.lookup('service:store');
+      sinon.stub(store, 'createRecord');
+      const saveStub = sinon.stub().resolves({});
+      const passageEventCollection = { save: saveStub };
+      store.createRecord.returns(passageEventCollection);
+
+      // when
+      await passageEventService.record({
+        type: 'FlashcardsStartedEvent',
+        passageId: 1,
+        data: {
+          elementId: '04287d5b-285e-4a67-9fb1-3adbf95deb2f',
+        },
+      });
+
+      // then
+      assert.deepEqual(passageEventCollection.events, [
+        {
+          type: 'FlashcardsStartedEvent',
+          passageId: 1,
+          occurredAt: 1556419320000,
+          elementId: '04287d5b-285e-4a67-9fb1-3adbf95deb2f',
+          sequenceNumber: 2,
+        },
+      ]);
+      assert.true(saveStub.called);
+    });
+
+    test('it should increment sequenceNumber when called multiple times', async function (assert) {
+      // given
+      const passageEventService = this.owner.lookup('service:passageEvents');
+
+      const store = this.owner.lookup('service:store');
+      const passageEventCollection1 = { save: sinon.stub() };
+      const passageEventCollection2 = { save: sinon.stub() };
+      sinon.stub(store, 'createRecord');
+      store.createRecord.onCall(0).returns(passageEventCollection1);
+      store.createRecord.onCall(1).returns(passageEventCollection2);
+
+      // when
+      await passageEventService.record({
+        type: 'FlashcardsStartedEvent',
+        passageId: 1,
+        data: {
+          elementId: '04287d5b-285e-4a67-9fb1-3adbf95deb2f',
+        },
+      });
+
+      await passageEventService.record({
+        type: 'FlashcardsRectoSeenEvent',
+        passageId: 1,
+        data: {
+          elementId: '04287d5b-285e-4a67-9fb1-3adbf95deb2f',
+        },
+      });
+
+      assert.strictEqual(passageEventCollection1.events[0].sequenceNumber, 2);
+      assert.strictEqual(passageEventCollection2.events[0].sequenceNumber, 3);
+    });
+  });
+});
