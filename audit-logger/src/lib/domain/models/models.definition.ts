d269e3115e189504d4f3d3e86e052bc890fd28f7
Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>
2025-05-07 17:48:17 +0200

    feat(audit-logger): delete non generic constraints
    Co-authored-by: LEGO Technix <109212476+lego-technix@users.noreply.github.com>

--- audit-logger/src/lib/domain/models/models.definition.ts
+++
@@ -1,8 +0,0 @@
-export const AuditLogActionTypes = ['ANONYMIZATION', 'ANONYMIZATION_GAR', 'EMAIL_CHANGED'] as const;
-export type AuditLogAction = (typeof AuditLogActionTypes)[number];
-
-export const AuditLogClientTypes = ['PIX_ADMIN', 'PIX_APP'] as const;
-export type AuditLogClient = (typeof AuditLogClientTypes)[number];
-
-export const AuditLogRoleTypes = ['SUPER_ADMIN', 'SUPPORT', 'USER'] as const;
-export type AuditLogRole = (typeof AuditLogRoleTypes)[number];
