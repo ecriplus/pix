a8a9cf4ada83559432b492dc0620c45579838dfa
Benjamin Petetot <benjamin.petetot@pix.fr>
2025-05-19 10:08:00 +0200

    refactor(certif): replace inject by service from @ember/service import

--- certif/app/services/locale.js
+++ certif/app/services/locale.js
@@ -1,4 +1,4 @@
-import Service, { inject as service } from '@ember/service';
+import Service, { service } from '@ember/service';
 import config from 'pix-certif/config/environment';
 import languages from 'pix-certif/languages';
 
