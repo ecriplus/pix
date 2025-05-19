8e08e93f6445587bcdc8a0042f63dac4045e5806
Theotime2005 <theotime.berthod@pix.fr>
2025-05-19 14:12:59 +0200

    feat(api): add createdBy column to certification-centers

---
+++ api/db/migrations/20250519100538_add-createdBy-column-to-certification-centers-table.js
@@ -0,0 +1,24 @@
+const TABLE_NAME = 'certification-centers';
+const COLUMN_NAME = 'createdBy';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  await knex.schema.alterTable(TABLE_NAME, function (table) {
+    table.bigInteger(COLUMN_NAME).references('users.id');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.dropColumn(COLUMN_NAME);
+  });
+};
+
+export { down, up };
