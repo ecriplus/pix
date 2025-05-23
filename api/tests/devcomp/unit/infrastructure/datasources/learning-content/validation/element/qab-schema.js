01890c528a1f860f9aba0bfbb6055157836c0cb7
Clément Latzarus <clement.latzarus@pix.fr>
2025-05-23 15:26:45 +0200

    feat(api): add QAB Joi schema

---
+++ api/tests/devcomp/unit/infrastructure/datasources/learning-content/validation/element/qab-schema.js
@@ -0,0 +1,26 @@
+import Joi from 'joi';
+
+import { htmlNotAllowedSchema, htmlSchema, uuidSchema } from '../utils.js';
+
+const qabElementSchema = Joi.object({
+  id: uuidSchema,
+  type: htmlNotAllowedSchema.valid('qab').required(),
+  instruction: htmlSchema.required(),
+  cards: Joi.array()
+    .items({
+      id: uuidSchema,
+      text: htmlNotAllowedSchema.allow('').required(),
+      image: {
+        url: htmlNotAllowedSchema.uri().allow('').required(),
+        altText: htmlNotAllowedSchema.uri().allow('').required(),
+      },
+      proposalA: htmlNotAllowedSchema.required(),
+      proposalB: htmlNotAllowedSchema.required(),
+      solution: htmlNotAllowedSchema.required(),
+    })
+    .min(1)
+    .max(6)
+    .required(),
+}).required();
+
+export { qabElementSchema };
