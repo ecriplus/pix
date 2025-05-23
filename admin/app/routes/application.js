60f7a80960ca8a66f801943c23b8a57c0abac65d
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2025-05-23 12:34:26 +0200

    feat(admin): set "en" as the default locale for .org but let translation fallback to "fr"

--- admin/app/routes/application.js
+++ admin/app/routes/application.js
@@ -1,17 +1,25 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
-const defaultLocale = 'fr';
+export const DEFAULT_LOCALE = ENGLISH_INTERNATIONAL_LOCALE;
+export const ENGLISH_INTERNATIONAL_LOCALE = 'en';
+export const FRENCH_INTERNATIONAL_LOCALE = 'fr';
 
 export default class ApplicationRoute extends Route {
   @service session;
   @service intl;
+  @service currentDomain;
   @service currentUser;
   @service featureToggles;
 
   async beforeModel() {
     await this.session.setup();
-    this.intl.setLocale([defaultLocale]);
+
+    if (this.currentDomain.isFranceDomain) {
+      this.intl.setLocale(FRENCH_INTERNATIONAL_LOCALE);
+    } else {
+      this.intl.setLocale(DEFAULT_LOCALE);
+    }
 
     await this.featureToggles.load();
 
