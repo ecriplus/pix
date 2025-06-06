3da91d6e11a354ab20675f531c1f8c1130ad7af0
Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>
2025-06-06 21:12:51 +0200

    refactor(api): add integration test for usecase

--- api/tests/identity-access-management/integration/domain/usecases/update-user-for-account-recovery.usecase.test.js
+++ api/tests/identity-access-management/integration/domain/usecases/update-user-for-account-recovery.usecase.test.js
@@ -1,10 +1,8 @@
-import { updateUserForAccountRecovery } from '../../../../../src/identity-access-management/domain/usecases/update-user-for-account-recovery.usecase.js';
-import { accountRecoveryDemandRepository } from '../../../../../src/identity-access-management/infrastructure/repositories/account-recovery-demand.repository.js';
-import * as authenticationMethodRepository from '../../../../../src/identity-access-management/infrastructure/repositories/authentication-method.repository.js';
-import * as userRepository from '../../../../../src/identity-access-management/infrastructure/repositories/user.repository.js';
+import { NON_OIDC_IDENTITY_PROVIDERS } from '../../../../../src/identity-access-management/domain/constants/identity-providers.js';
+import { usecases } from '../../../../../src/identity-access-management/domain/usecases/index.js';
 import { DomainTransaction } from '../../../../../src/shared/domain/DomainTransaction.js';
-import { cryptoService } from '../../../../../src/shared/domain/services/crypto-service.js';
-import { catchErr, databaseBuilder, expect, knex } from '../../../../test-helper.js';
+import { featureToggles } from '../../../../../src/shared/infrastructure/feature-toggles/index.js';
+import { catchErr, databaseBuilder, expect, knex, sinon } from '../../../../test-helper.js';
 
 describe('Integration | Identity Access Management | Domain | UseCase | update-user-for-account-recovery', function () {
   context('when domain transaction throw an error', function () {
@@ -12,25 +10,19 @@ describe('Integration | Identity Access Management | Domain | UseCase | update-u
       // given
       const password = 'pix123';
       const user = databaseBuilder.factory.buildUser();
-      await databaseBuilder.commit();
       const authenticatedMethod =
         databaseBuilder.factory.buildAuthenticationMethod.withPixAsIdentityProviderAndHashedPassword({
           userId: user.id,
         });
-      await databaseBuilder.commit();
       const accountRecovery = databaseBuilder.factory.buildAccountRecoveryDemand({ userId: user.id });
       await databaseBuilder.commit();
 
       // when
       await catchErr(async () => {
         await DomainTransaction.execute(async () => {
-          await updateUserForAccountRecovery({
+          await usecases.updateUserForAccountRecovery({
             password,
             temporaryKey: accountRecovery.temporaryKey,
-            userRepository,
-            authenticationMethodRepository,
-            accountRecoveryDemandRepository,
-            cryptoService,
           });
           throw new Error('an error occurs within the domain transaction');
         });
@@ -50,6 +42,157 @@ describe('Integration | Identity Access Management | Domain | UseCase | update-u
       expect(userUpdated[0].cgu).to.be.equal(user.cgu);
       expect(accountRecoveryDemand[0].used).to.be.false;
       expect(authenticationMethod[0].password).to.equal(authenticatedMethod.password);
+    });
+  });
+
+  context('success cases', function () {
+    let clock;
+    let now;
+
+    beforeEach(function () {
+      clock = sinon.useFakeTimers({ now: new Date(), toFake: ['Date'] });
+      now = new Date(clock.now);
+    });
+
+    afterEach(function () {
+      clock.restore();
+    });
+
+    context('when user has no Pix authentication method', function () {
+      it('adds Pix authentication method', async function () {
+        // given
+        const password = 'pix123';
+        const user = databaseBuilder.factory.buildUser.withoutPixAuthenticationMethod();
+        const demand = databaseBuilder.factory.buildAccountRecoveryDemand({ userId: user.id, oldEmail: null });
+        await databaseBuilder.commit();
+        const temporaryKey = demand.temporaryKey;
+
+        // when
+        await usecases.updateUserForAccountRecovery({
+          password,
+          temporaryKey,
+        });
+
+        // then
+        const pixAuthenticationMethod = await knex('authentication-methods')
+          .where({ identityProvider: NON_OIDC_IDENTITY_PROVIDERS.PIX.code })
+          .first();
+        expect(pixAuthenticationMethod).to.include({
+          userId: user.id,
+        });
+      });
+    });
+
+    context('when user has Pix authentication method', function () {
+      it('updates only password', async function () {
+        // given
+        const newPassword = 'pix123';
+        const user = databaseBuilder.factory.buildUser();
+        const authenticationMethod =
+          databaseBuilder.factory.buildAuthenticationMethod.withPixAsIdentityProviderAndHashedPassword({
+            userId: user.id,
+          });
+        const accountRecoveryDemand = databaseBuilder.factory.buildAccountRecoveryDemand({
+          userId: user.id,
+          oldEmail: null,
+        });
+        await databaseBuilder.commit();
+
+        // when
+        await usecases.updateUserForAccountRecovery({
+          password: newPassword,
+          temporaryKey: accountRecoveryDemand.temporaryKey,
+        });
+
+        // then
+        const updatedAuthenticationMethods = await knex('authentication-methods').where({
+          identityProvider: NON_OIDC_IDENTITY_PROVIDERS.PIX.code,
+        });
+        expect(updatedAuthenticationMethods).to.have.lengthOf(1);
+
+        const updatedAuthenticationMethod = updatedAuthenticationMethods[0];
+        expect(updatedAuthenticationMethod).to.include({ userId: user.id });
+        expect(updatedAuthenticationMethod.authenticationComplement.password).to.not.equal(
+          authenticationMethod.authenticationComplement.password,
+        );
+      });
+    });
+
+    context('when user has GAR authentication method', function () {
+      it('sets username to null and removes GAR authentication method', async function () {
+        // given
+        await featureToggles.set('isNewAccountRecoveryEnabled', true);
+
+        const password = 'pix123';
+        const user = databaseBuilder.factory.buildUser({ email: null });
+        databaseBuilder.factory.buildAuthenticationMethod.withGarAsIdentityProvider({ userId: user.id });
+        const accountRecoveryDemand = databaseBuilder.factory.buildAccountRecoveryDemand({
+          userId: user.id,
+          oldEmail: null,
+        });
+        await databaseBuilder.commit();
+
+        // when
+        await usecases.updateUserForAccountRecovery({
+          password,
+          temporaryKey: accountRecoveryDemand.temporaryKey,
+        });
+
+        // then
+        const foundUser = await knex('users').where({ id: user.id }).first();
+        const removedGarAuthenticationMethod = await knex('authentication-methods')
+          .where({ userId: user.id, identityProvider: NON_OIDC_IDENTITY_PROVIDERS.GAR })
+          .first();
+
+        expect(foundUser.username).to.be.null;
+        expect(removedGarAuthenticationMethod).to.not.exist;
+      });
+    });
+
+    it('updates user values', async function () {
+      // given
+      const password = 'pix123';
+      const user = databaseBuilder.factory.buildUser({ email: 'oldEmail@example.net' });
+      const accountRecoveryDemand = databaseBuilder.factory.buildAccountRecoveryDemand({
+        userId: user.id,
+        oldEmail: 'oldEmail@example.net',
+        newEmail: 'newEmail@example.net',
+      });
+      await databaseBuilder.commit();
+
+      // when
+      await usecases.updateUserForAccountRecovery({
+        password,
+        temporaryKey: accountRecoveryDemand.temporaryKey,
+      });
+
+      // then
+      const foundUser = await knex('users').where({ id: user.id }).first();
+      expect(foundUser.cgu).to.be.true;
+      expect(foundUser).to.include({ email: 'newemail@example.net' });
+      expect(foundUser.emailConfirmedAt).to.deep.equal(now);
+      expect(foundUser.lastTermsOfServiceValidatedAt).to.deep.equal(now);
+    });
+
+    it('marks account recovery demand as being used', async function () {
+      // given
+      const password = 'pix123';
+      const user = databaseBuilder.factory.buildUser();
+      const accountRecoveryDemand = databaseBuilder.factory.buildAccountRecoveryDemand({
+        userId: user.id,
+        oldEmail: null,
+      });
+      await databaseBuilder.commit();
+
+      // when
+      await usecases.updateUserForAccountRecovery({
+        password,
+        temporaryKey: accountRecoveryDemand.temporaryKey,
+      });
+
+      // then
+      const updatedRecoveryDemand = await knex('account-recovery-demands').where({ userId: user.id }).first();
+      expect(updatedRecoveryDemand.used).to.be.true;
     });
   });
 });
