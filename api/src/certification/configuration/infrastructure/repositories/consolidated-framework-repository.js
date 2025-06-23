75e8ff0c406793e9cc60dc7a46199a11ae9c38d5
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2025-06-23 15:38:50 +0200

    feat(api): add getCurrentFrameworkByComplementaryCertificationKey repository method
    Co-authored-by: Andreia Pena <andreia.pena@pix.fr>

--- api/src/certification/configuration/infrastructure/repositories/consolidated-framework-repository.js
+++ api/src/certification/configuration/infrastructure/repositories/consolidated-framework-repository.js
@@ -1,4 +1,8 @@
+import { LOCALE } from '../../../../shared/domain/constants.js';
 import { DomainTransaction } from '../../../../shared/domain/DomainTransaction.js';
+import * as challengeRepository from '../../../../shared/infrastructure/repositories/challenge-repository.js';
+import * as skillRepository from '../../../../shared/infrastructure/repositories/skill-repository.js';
+import { ConsolidatedFramework } from '../../domain/models/ConsolidatedFramework.js';
 
 export async function create({ complementaryCertificationKey, challenges }) {
   const knexConn = DomainTransaction.getConnection();
@@ -9,4 +13,35 @@ export async function create({ complementaryCertificationKey, challenges }) {
   }));
 
   await knexConn('certification-frameworks-challenges').insert(challengesDTO);
+}
+
+export async function getCurrentFrameworkByComplementaryCertificationKey({ complementaryCertificationKey }) {
+  const knexConn = DomainTransaction.getConnection();
+
+  const currentFrameworkChallenges = await knexConn('certification-frameworks-challenges')
+    .where({
+      createdAt: knexConn('certification-frameworks-challenges')
+        .select('createdAt')
+        .orderBy('createdAt', 'desc')
+        .first(),
+    })
+    .select('*');
+
+  const currentChallengeIds = currentFrameworkChallenges.map((challenge) => challenge.challengeId);
+  const currentChallenges = await challengeRepository.getMany(currentChallengeIds, LOCALE.FRENCH_SPOKEN);
+
+  const currentSkillIds = currentChallenges.map((challenge) => challenge.skill.id);
+  const currentSkills = await skillRepository.findByRecordIds(currentSkillIds);
+
+  const currentTubeIds = currentSkills.map((skill) => skill.tubeId);
+
+  return _toDomain({
+    complementaryCertificationKey,
+    createdAt: currentFrameworkChallenges[0].createdAt,
+    tubeIds: currentTubeIds,
+  });
+}
+
+function _toDomain({ complementaryCertificationKey, createdAt, tubeIds }) {
+  return new ConsolidatedFramework({ complementaryCertificationKey, createdAt, tubeIds });
 }
