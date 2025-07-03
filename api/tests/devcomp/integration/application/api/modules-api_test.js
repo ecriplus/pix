7a102219968ee6c17c9847643d7793acb795f16b
Eric Lim <eric.lim@pix.fr>
2025-07-03 18:19:50 +0200

    feat(api): ad getUserModuleStatuses internal api

---
+++ api/tests/devcomp/integration/application/api/modules-api_test.js
@@ -0,0 +1,103 @@
+import { ModuleStatus } from '../../../../../src/devcomp/application/api/models/ModuleStatus.js';
+import * as modulesApi from '../../../../../src/devcomp/application/api/modules-api.js';
+import { DomainError } from '../../../../../src/shared/domain/errors.js';
+import { catchErr, databaseBuilder, expect, sinon } from '../../../../test-helper.js';
+
+describe('Integration | Devcomp | Application | Api | Modules', function () {
+  let clock, now;
+
+  beforeEach(function () {
+    now = new Date('2023-10-05T18:02:00Z');
+    clock = sinon.useFakeTimers({ now, toFake: ['Date'] });
+  });
+
+  afterEach(function () {
+    clock.restore();
+  });
+
+  describe('#getModuleStatuses', function () {
+    it('should return a list of Module statuses', async function () {
+      // given
+      const { id: userId } = databaseBuilder.factory.buildUser();
+      const existingModuleIdWithoutRelatedPassage = '6282925d-4775-4bca-b513-4c3009ec5886';
+      const existingModuleId2 = 'f7b3a2e1-0d5c-4c6c-9c4d-1a3d8f7e9f5d';
+      const existingModuleId3 = '5df14039-803b-4db4-9778-67e4b84afbbd';
+      const moduleIds = [existingModuleIdWithoutRelatedPassage, existingModuleId2, existingModuleId3];
+
+      databaseBuilder.factory.buildPassage({
+        moduleId: existingModuleId2,
+        userId,
+        terminatedAt: null,
+      });
+
+      databaseBuilder.factory.buildPassage({
+        moduleId: existingModuleId3,
+        userId,
+        terminatedAt: now,
+      });
+
+      await databaseBuilder.commit();
+
+      // when
+      const result = await modulesApi.getUserModuleStatuses({ userId, moduleIds });
+
+      // then
+      const expectedResult = [
+        new ModuleStatus({
+          id: existingModuleIdWithoutRelatedPassage,
+          slug: 'bac-a-sable',
+          title: 'Bac à sable',
+          duration: 5,
+          status: 'NOT_STARTED',
+        }),
+        new ModuleStatus({
+          id: existingModuleId2,
+          slug: 'bien-ecrire-son-adresse-mail',
+          title: 'Bien écrire une adresse mail',
+          duration: 10,
+          status: 'IN_PROGRESS',
+        }),
+        new ModuleStatus({
+          id: existingModuleId3,
+          slug: 'adresse-ip-publique-et-vous',
+          title: "L'adresse IP publique : ce qu'elle révèle sur vous !",
+          duration: 10,
+          status: 'COMPLETED',
+        }),
+      ];
+
+      expect(result).to.deep.equal(expectedResult);
+    });
+
+    context('if userId passed is not defined', function () {
+      it('should throw a DomainError error', async function () {
+        // given
+        const existingModuleIdWithoutRelatedPassage = '6282925d-4775-4bca-b513-4c3009ec5886';
+        const existingModuleId2 = 'f7b3a2e1-0d5c-4c6c-9c4d-1a3d8f7e9f5d';
+        const existingModuleId3 = '5df14039-803b-4db4-9778-67e4b84afbbd';
+        const moduleIds = [existingModuleIdWithoutRelatedPassage, existingModuleId2, existingModuleId3];
+
+        // when
+        const error = await catchErr(modulesApi.getUserModuleStatuses)({ moduleIds });
+
+        // then
+        expect(error).to.be.instanceOf(DomainError);
+        expect(error.message).to.equal('The userId is required');
+      });
+    });
+
+    context('if moduleIds is empty', function () {
+      it('should return an empty array', async function () {
+        // given
+        const userId = '1';
+        const moduleIds = [];
+
+        // when
+        const result = await modulesApi.getUserModuleStatuses({ userId, moduleIds });
+
+        // then
+        expect(result).to.be.empty;
+      });
+    });
+  });
+});
