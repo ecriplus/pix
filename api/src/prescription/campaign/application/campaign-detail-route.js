f99d513b8785ffbbbbd6718f0e341a845d1e9d2b
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2025-07-04 10:38:23 +0200

    feat(api): add pre handler on csv-assessment-route when organization places exceeded

--- api/src/prescription/campaign/application/campaign-detail-route.js
+++ api/src/prescription/campaign/application/campaign-detail-route.js
@@ -140,7 +140,14 @@ const register = async function (server) {
       method: 'GET',
       path: '/api/campaigns/{campaignId}/csv-assessment-results',
       config: {
-        pre: [{ method: securityPreHandlers.checkAuthorizationToAccessCampaign }],
+        pre: [
+          {
+            method: securityPreHandlers.validateAllAccess([
+              securityPreHandlers.checkAuthorizationToAccessCampaign,
+              securityPreHandlers.checkOrganizationAccess,
+            ]),
+          },
+        ],
         validate: {
           params: Joi.object({
             campaignId: identifiersType.campaignId,
