92669accec0734c27cff0c646ec5ed3112d07b92
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2025-07-04 11:34:22 +0200

    tech(api)?: remove duplicate csv-assessment-route test

--- api/tests/prescription/campaign/integration/application/campaign-detail-route_test.js
+++ api/tests/prescription/campaign/integration/application/campaign-detail-route_test.js
@@ -28,26 +28,6 @@ describe('Integration | Application | Route | campaign detail router', function 
     });
   });
 
-  describe('GET /api/campaigns/{campaignId}/csv-assessment-results', function () {
-    it('should exist', async function () {
-      // given
-      sinon.stub(securityPreHandlers, 'checkAuthorizationToAccessCampaign').callsFake((request, h) => h.response(true));
-      sinon.stub(campaignDetailController, 'getCsvAssessmentResults').callsFake((_, h) => h.response('ok').code(200));
-      const method = 'GET';
-      const url = '/api/campaigns/1/csv-assessment-results';
-
-      httpTestServer = new HttpTestServer();
-      await httpTestServer.register(moduleUnderTest);
-
-      // when
-      const response = await httpTestServer.request(method, url);
-
-      // then
-      expect(response.statusCode).to.equal(200);
-      expect(campaignDetailController.getCsvAssessmentResults).to.have.been.calledOnce;
-    });
-  });
-
   describe('GET /api/organizations/{organizationId}/campaigns', function () {
     it('should call the organization controller to get the campaigns', async function () {
       sinon.stub(campaignDetailController, 'findPaginatedFilteredCampaigns').returns('ok');
