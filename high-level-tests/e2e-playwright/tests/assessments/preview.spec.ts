4c337e1a65c11875bce30f0e21cf18cc1e759740
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2025-07-10 16:51:20 +0200

    refactor(e2e): move test record or reference check at the very end of preview test

--- high-level-tests/e2e-playwright/tests/assessments/preview.spec.ts
+++ high-level-tests/e2e-playwright/tests/assessments/preview.spec.ts
@@ -1,23 +1,14 @@
-import path from 'node:path';
-
-import { Page } from '@playwright/test';
-import * as fs from 'fs/promises';
-
 import { knex } from '../../helpers/db.ts';
-import { expect, test } from '../../helpers/fixtures.ts';
+import { expect, expectOrRecordResults, test } from '../../helpers/fixtures.ts';
 import { ChallengePage } from '../../pages/pix-app/index.ts';
 
-const RESULT_DIR = path.resolve(import.meta.dirname, '../../snapshots');
 let PREVIEW_CHALLENGE_ID: string;
 test.beforeEach(async () => {
   const { id } = await knex('learningcontent.challenges').select('id').where('status', 'validé').first();
   PREVIEW_CHALLENGE_ID = id;
 });
 
-test('[@snapshot] user assesses on preview challenge', async ({
-  page,
-  testMode,
-}: { page: Page; testMode: string }, testInfo) => {
+test('[@snapshot] user assesses on preview challenge', async ({ page, testMode }, testInfo) => {
   testInfo.annotations.push({
     type: 'tag',
     description: `@snapshot - this test runs against a reference snapshot. Snapshot can be generated with TEST_MODE=record env.
@@ -25,31 +16,18 @@ test('[@snapshot] user assesses on preview challenge', async ({
          - Reference Release has changed`,
   });
   test.setTimeout(10_000);
-  let results;
-  const resultFilePath = path.join(RESULT_DIR, 'preview.json');
-  if (testMode === 'record') {
-    results = {
-      challengeImprints: [],
-    };
-  } else {
-    results = await fs.readFile(resultFilePath, 'utf-8');
-    results = JSON.parse(results);
-  }
+  const results = {
+    challengeImprints: [] as string[],
+  };
 
   await test.step(`PREVIEW assessment started`, async () => {
     await page.goto(process.env.PIX_APP_URL + '/challenges/' + PREVIEW_CHALLENGE_ID + '/preview');
     const challengePage = new ChallengePage(page);
     const challengeImprint = await challengePage.getChallengeImprint();
-    if (testMode === 'record') {
-      results.challengeImprints.push(challengeImprint);
-    } else {
-      expect(challengeImprint).toBe(results.challengeImprints[0]);
-    }
+    results.challengeImprints.push(challengeImprint);
     await expect(page.getByLabel('Votre progression')).toContainText('1 / 1');
     await challengePage.setRightOrWrongAnswer(true);
     await challengePage.validateAnswer();
   });
-  if (testMode === 'record') {
-    await fs.writeFile(resultFilePath, JSON.stringify(results));
-  }
+  expectOrRecordResults({ results, resultFileName: 'preview.json', testMode });
 });
