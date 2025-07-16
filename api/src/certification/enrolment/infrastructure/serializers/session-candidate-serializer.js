53536fed5aca4175b358440b13c6d23102cd2133
Steph0 <Steph0@users.noreply.github.com>
2025-07-16 18:50:36 +0200

    :sparkles: admin: session candidates list

---
+++ api/src/certification/enrolment/infrastructure/serializers/session-candidate-serializer.js
@@ -0,0 +1,14 @@
+import { Serializer } from 'jsonapi-serializer';
+
+const serialize = function (enrolledCandidates) {
+  return new Serializer('certification-candidate', {
+    attributes: ['firstName', 'lastName', 'birthdate', 'subscriptions'],
+    subscriptions: {
+      include: true,
+      ref: 'id',
+      attributes: ['complementaryCertificationId', 'type'],
+    },
+  }).serialize(enrolledCandidates);
+};
+
+export { serialize };
