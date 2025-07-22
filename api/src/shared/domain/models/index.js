33fe4a020314d97531ed9beace0d7391b58b2515
Yannick François <yannick.francois@pix.fr>
2025-07-22 16:13:34 +0200

    :wastebasket: remove unused shared models

---
+++ api/src/shared/domain/models/index.js
@@ -0,0 +1,96 @@
+import { AdminMember } from './AdminMember.js';
+import { AnswerStatus } from './AnswerStatus.js';
+import { Area } from './Area.js';
+import { Assessment } from './Assessment.js';
+import { AssessmentResult } from './AssessmentResult.js';
+import { Authentication } from './Authentication.js';
+import { AuthenticationSessionContent } from './AuthenticationSessionContent.js';
+import { BadgeCriterionForCalculation } from './BadgeCriterionForCalculation.js';
+import { BadgeDetails } from './BadgeDetails.js';
+import { BadgeForCalculation } from './BadgeForCalculation.js';
+import { CampaignLearningContent } from './CampaignLearningContent.js';
+import { CampaignParticipationResult } from './CampaignParticipationResult.js';
+import { CertifiableBadgeAcquisition } from './CertifiableBadgeAcquisition.js';
+import { CertificationCandidate } from './CertificationCandidate.js';
+import { CertificationCenter } from './CertificationCenter.js';
+import { CertificationCenterMembership } from './CertificationCenterMembership.js';
+import { CertificationChallenge } from './CertificationChallenge.js';
+import { CertificationChallengeWithType } from './CertificationChallengeWithType.js';
+import { CertificationResult } from './CertificationResult.js';
+import { Challenge } from './Challenge.js';
+import { Competence } from './Competence.js';
+import { CompetenceResult } from './CompetenceResult.js';
+import { CompetenceTree } from './CompetenceTree.js';
+import { ComplementaryCertificationHabilitation } from './ComplementaryCertificationHabilitation.js';
+import { Correction } from './Correction.js';
+import { Course } from './Course.js';
+import { Examiner } from './Examiner.js';
+import { Framework } from './Framework.js';
+import { Hint } from './Hint.js';
+import { KnowledgeElement } from './KnowledgeElement.js';
+import { LearningContent } from './LearningContent.js';
+import { AreaForAdmin, CompetenceForAdmin, ThematicForAdmin, TubeForAdmin } from './LearningContentForAdmin.js';
+import { Membership } from './Membership.js';
+import { OrganizationTag } from './OrganizationTag.js';
+import { PlacementProfile } from './PlacementProfile.js';
+import { ReproducibilityRate } from './ReproducibilityRate.js';
+import { Skill } from './Skill.js';
+import { Solution } from './Solution.js';
+import { Student } from './Student.js';
+import { TargetProfile } from './TargetProfile.js';
+import { Thematic } from './Thematic.js';
+import { Tube } from './Tube.js';
+import { UserCompetence } from './UserCompetence.js';
+import { UserOrgaSettings } from './UserOrgaSettings.js';
+import { Validation } from './Validation.js';
+
+export {
+  AdminMember,
+  AnswerStatus,
+  Area,
+  AreaForAdmin,
+  Assessment,
+  AssessmentResult,
+  Authentication,
+  AuthenticationSessionContent,
+  BadgeCriterionForCalculation,
+  BadgeDetails,
+  BadgeForCalculation,
+  CampaignLearningContent,
+  CampaignParticipationResult,
+  CertifiableBadgeAcquisition,
+  CertificationCandidate,
+  CertificationCenter,
+  CertificationCenterMembership,
+  CertificationChallenge,
+  CertificationChallengeWithType,
+  CertificationResult,
+  Challenge,
+  Competence,
+  CompetenceForAdmin,
+  CompetenceResult,
+  CompetenceTree,
+  ComplementaryCertificationHabilitation,
+  Correction,
+  Course,
+  Examiner,
+  Framework,
+  Hint,
+  KnowledgeElement,
+  LearningContent,
+  Membership,
+  OrganizationTag,
+  PlacementProfile,
+  ReproducibilityRate,
+  Skill,
+  Solution,
+  Student,
+  TargetProfile,
+  Thematic,
+  ThematicForAdmin,
+  Tube,
+  TubeForAdmin,
+  UserCompetence,
+  UserOrgaSettings,
+  Validation,
+};
