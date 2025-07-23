5c82f662fc5bbe9d2baa154c954233660c885a01
Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>
2025-07-23 14:35:10 +0200

    refactor(orga): replace custom Accept Language Header by localeService.acceptLanguageHeader
    Co-authored-by: LEGO Technix <109212476+lego-technix@users.noreply.github.com>

--- orga/app/adapters/application.js
+++ orga/app/adapters/application.js
@@ -2,13 +2,8 @@ import { service } from '@ember/service';
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import ENV from 'pix-orga/config/environment';
 
-const FRENCH_DOMAIN_EXTENSION = 'fr';
-const FRENCH_LOCALE = 'fr-fr';
-const FRENCHSPOKEN_LOCALE = 'fr';
-
 export default class ApplicationAdapter extends JSONAPIAdapter {
   @service ajaxQueue;
-  @service currentDomain;
   @service locale;
   @service session;
 
@@ -20,21 +15,13 @@ export default class ApplicationAdapter extends JSONAPIAdapter {
     if (this.session.isAuthenticated) {
       headers['Authorization'] = `Bearer ${this.session.data.authenticated.access_token}`;
     }
-    headers['Accept-Language'] = this._locale;
+    headers['Accept-Language'] = this.locale.acceptLanguageHeader;
     headers['X-App-Version'] = ENV.APP.APP_VERSION;
     return headers;
   }
 
   ajax() {
     return this.ajaxQueue.add(() => super.ajax(...arguments));
-  }
-
-  get _locale() {
-    const currentLocale = this.locale.currentLocale;
-    if (currentLocale === 'fr') {
-      return this.currentDomain.getExtension() === FRENCH_DOMAIN_EXTENSION ? FRENCH_LOCALE : FRENCHSPOKEN_LOCALE;
-    }
-    return currentLocale;
   }
 
   handleResponse(status, headers, errors) {
