ed2a1315fe6399a5fbf64a9d966f2fa301c61733
Gudsfile <gudsfile@protonmail.com>
2025-07-24 15:20:38 +0200

    refactor(api-maddo): reorganize post-deploy script

--- api/scripts/scalingo-post-ra-creation.sh
+++ api/scripts/scalingo-post-ra-creation.sh
@@ -4,14 +4,16 @@ set -ex
 install-scalingo-cli
 
 if [[ -z "$MADDO" ]]; then
+  # api deployment
   npm run postdeploy
-  scalingo -a pix-api-maddo-review-pr$PR_NUMBER env-set DATABASE_URL="$SCALINGO_POSTGRESQL_URL"
-  scalingo -a pix-api-maddo-review-pr$PR_NUMBER restart || echo
   npm run db:seed
 else
+  # api-maddo deployment
   npm run postdeploy:maddo
   npm run datamart:seed
+  API_DATABASE_URL=$(scalingo -a pix-api-review-pr$PR_NUMBER env-get SCALINGO_POSTGRESQL_URL)
+  scalingo -a pix-api-maddo-review-pr$PR_NUMBER env-set DATABASE_URL="$API_DATABASE_URL"
   scalingo -a pix-api-review-pr$PR_NUMBER env-set DATAMART_DATABASE_URL="$SCALINGO_POSTGRESQL_URL"
+  # le premier déploiement de l'api n'a pas forcément déjà eu lieu
   scalingo -a pix-api-review-pr$PR_NUMBER restart || echo
 fi
-
