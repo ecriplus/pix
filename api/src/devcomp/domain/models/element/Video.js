2c8aacbcb7ac5e6803605b64af7a14e45d272560
Eric Lim <eric.lim@pix.fr>
2025-07-25 10:33:25 +0200

    feat(api): limit hostname for video url and subtitles

--- api/src/devcomp/domain/models/element/Video.js
+++ api/src/devcomp/domain/models/element/Video.js
@@ -1,7 +1,10 @@
+import { DomainError } from '../../../../shared/domain/errors.js';
 import { assertNotNullOrUndefined } from '../../../../shared/domain/models/asserts.js';
 import { Element } from './Element.js';
 
 class Video extends Element {
+  static #VALID_HOSTNAME = 'assets.pix.org';
+
   constructor({ id, title, url, subtitles, transcription, poster }) {
     super({ id, type: 'video' });
 
@@ -10,11 +13,27 @@ class Video extends Element {
     assertNotNullOrUndefined(subtitles, 'The subtitles are required for a video');
     assertNotNullOrUndefined(transcription, 'The transcription is required for a video');
 
+    if (!URL.canParse(url)) {
+      throw new DomainError('The URL must be a valid URL for a video');
+    }
+
+    if (subtitles.length > 0 && !URL.canParse(subtitles)) {
+      throw new DomainError('The subtitles must be a valid URL for a video');
+    }
+
     this.url = url;
     this.title = title;
     this.subtitles = subtitles;
     this.transcription = transcription;
     this.poster = poster ?? null;
+
+    if (URL.parse(url).hostname !== Video.#VALID_HOSTNAME) {
+      throw new DomainError('The video URL must be from "assets.pix.org"');
+    }
+
+    if (subtitles.length > 0 && URL.parse(subtitles).hostname !== Video.#VALID_HOSTNAME) {
+      throw new DomainError('The subtitles URL must be from "assets.pix.org"');
+    }
   }
 }
 
--- api/tests/devcomp/unit/domain/models/element/Video_test.js
+++ api/tests/devcomp/unit/domain/models/element/Video_test.js
@@ -9,20 +9,20 @@ describe('Unit | Devcomp | Domain | Models | Element | Video', function () {
       const video = new Video({
         id: 'id',
         title: 'title',
-        url: 'url',
-        subtitles: 'subtitles',
+        url: 'https://assets.pix.org/modules/video.mp4',
+        subtitles: 'https://assets.pix.org/modules/video-poster.vtt',
         transcription: 'transcription',
-        poster: 'https://example.org/modulix/video-poster.jpg',
+        poster: 'https://assets.pix.org/modules/video-poster.jpg',
       });
 
       // then
       expect(video.id).to.equal('id');
       expect(video.type).to.equal('video');
       expect(video.title).to.equal('title');
-      expect(video.url).to.equal('url');
-      expect(video.subtitles).to.equal('subtitles');
+      expect(video.url).to.equal('https://assets.pix.org/modules/video.mp4');
+      expect(video.subtitles).to.equal('https://assets.pix.org/modules/video-poster.vtt');
       expect(video.transcription).to.equal('transcription');
-      expect(video.poster).to.equal('https://example.org/modulix/video-poster.jpg');
+      expect(video.poster).to.equal('https://assets.pix.org/modules/video-poster.jpg');
     });
   });
 
@@ -59,6 +59,19 @@ describe('Unit | Devcomp | Domain | Models | Element | Video', function () {
     });
   });
 
+  describe('A video without a valid url', function () {
+    it('should throw an error', function () {
+      // when
+      const error = catchErrSync(
+        () => new Video({ id: 'id', title: 'title', url: 'url', subtitles: '', transcription: '' }),
+      )();
+
+      // then
+      expect(error).to.be.instanceOf(DomainError);
+      expect(error.message).to.equal('The URL must be a valid URL for a video');
+    });
+  });
+
   describe('A video without subtitles', function () {
     it('should throw an error', function () {
       // when
@@ -70,14 +83,84 @@ describe('Unit | Devcomp | Domain | Models | Element | Video', function () {
     });
   });
 
+  describe('A video without a valid format url for subtitles', function () {
+    it('should throw an error', function () {
+      // when
+      const error = catchErrSync(
+        () =>
+          new Video({
+            id: 'id',
+            title: 'title',
+            url: 'https://assets.pix.org/modules/video.mp4',
+            transcription: '',
+            subtitles: 'subtitles',
+          }),
+      )();
+
+      // then
+      expect(error).to.be.instanceOf(DomainError);
+      expect(error.message).to.equal('The subtitles must be a valid URL for a video');
+    });
+  });
+
   describe('A video without a transcription', function () {
     it('should throw an error', function () {
       // when
-      const error = catchErrSync(() => new Video({ id: 'id', title: 'title', url: 'url', subtitles: 'subtitles' }))();
+      const error = catchErrSync(
+        () =>
+          new Video({
+            id: 'id',
+            title: 'title',
+            url: 'https://assets.pix.org/video.mp4',
+            subtitles: 'https://assets.pix.org/video.vtt',
+          }),
+      )();
 
       // then
       expect(error).to.be.instanceOf(DomainError);
       expect(error.message).to.equal('The transcription is required for a video');
+    });
+  });
+
+  describe('When video URL is not from assets.pix.org', function () {
+    it('should throw an error', function () {
+      // given & when
+      const error = catchErrSync(
+        () =>
+          new Video({
+            id: 'id',
+            title: 'title',
+            url: 'https://example.com/modules/video.mp4',
+            subtitles: 'https://assets.pix.org/modules/video-poster.vtt',
+            transcription: 'transcription',
+            poster: 'https://assets.pix.org/modules/video-poster.jpg',
+          }),
+      )();
+
+      // then
+      expect(error).to.be.instanceOf(DomainError);
+      expect(error.message).to.equal('The video URL must be from "assets.pix.org"');
+    });
+  });
+
+  describe('When video subtitle URL is not from assets.pix.org', function () {
+    it('should throw an error', function () {
+      // given & when
+      const error = catchErrSync(
+        () =>
+          new Video({
+            id: 'id',
+            title: 'title',
+            url: 'https://assets.pix.org/modules/video-poster.mp4',
+            subtitles: 'https://example.com/modules/video-poster.vtt',
+            transcription: 'transcription',
+            poster: 'https://assets.pix.org/modules/video-poster.jpg',
+          }),
+      )();
+
+      // then
+      expect(error).to.be.instanceOf(DomainError);
+      expect(error.message).to.equal('The subtitles URL must be from "assets.pix.org"');
     });
   });
 });
