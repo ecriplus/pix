bcaab175ea895b3bb801238a1c83c88ce7522ec2
Eric Lim <eric.lim@pix.fr>
2025-07-30 17:19:04 +0200

    feat(api): remove arborescence-monitoring bot

--- api/scripts/arborescence-monitoring/slack.js
+++
@@ -1,7 +0,0 @@
-export function formatMessageForSlack(charts) {
-  const blocks = charts.map(({ url: image_url, altText: alt_text }) => ({ type: 'image', image_url, alt_text }));
-  const slackBlocks = {
-    blocks,
-  };
-  return JSON.stringify(slackBlocks);
-}
