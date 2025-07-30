27798842fbd971615883cd6a4271caaf0d1ad7f1
Eric Lim <eric.lim@pix.fr>
2025-07-30 15:44:52 +0200

    feat(api): add feedback attribute
    in qab model

--- api/src/devcomp/domain/models/element/qab/QAB.js
+++ api/src/devcomp/domain/models/element/qab/QAB.js
@@ -1,12 +1,13 @@
 import { QABCard } from './QABCard.js';
 
 class QAB {
-  constructor({ id, type, instruction, cards }) {
+  constructor({ id, type, instruction, cards, feedback }) {
     this.id = id;
     this.type = type;
     this.instruction = instruction;
     this.isAnswerable = true;
     this.cards = cards.map((card) => new QABCard(card));
+    this.feedback = feedback;
   }
 }
 
