e7f30e10b5febc3559fe997185df69771cedec30
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2025-07-31 16:04:31 +0200

    feat(api): add new function localeService.getNearestSupportedLocale

--- api/tests/shared/unit/domain/services/locale-service_test.js
+++ api/tests/shared/unit/domain/services/locale-service_test.js
@@ -1,34 +1,7 @@
-import { LocaleFormatError, LocaleNotSupportedError } from '../../../../../src/shared/domain/errors.js';
 import * as localeService from '../../../../../src/shared/domain/services/locale-service.js';
-import { catchErrSync, expect } from '../../../../test-helper.js';
+import { expect } from '../../../../test-helper.js';
 
 describe('Unit | Shared | Domain | Service | Locale', function () {
-  describe('getCanonicalLocale', function () {
-    it('throws a LocaleFormatError error', function () {
-      // when
-      const error = catchErrSync(localeService.getCanonicalLocale)('anInvalidLocale');
-
-      // then
-      expect(error).to.be.instanceOf(LocaleFormatError);
-    });
-
-    it('throws a LocaleNotSupportedError error', function () {
-      // when
-      const error = catchErrSync(localeService.getCanonicalLocale)('pt-PT');
-
-      // then
-      expect(error).to.be.instanceOf(LocaleNotSupportedError);
-    });
-
-    it('canonicalizes the given locale', function () {
-      // given
-      const locale = localeService.getCanonicalLocale('fr-fr');
-
-      // then
-      expect(locale).to.equal('fr-FR');
-    });
-  });
-
   describe('getSupportedLanguages', function () {
     it('returns languages computed from the supported locales', function () {
       // when
@@ -36,6 +9,73 @@ describe('Unit | Shared | Domain | Service | Locale', function () {
 
       // then
       expect(result).to.deep.equal(['en', 'es', 'fr', 'nl']);
+    });
+  });
+
+  describe('getNearestSupportedLocale', function () {
+    context('when given a supported locale in canonical form', function () {
+      it('returns the locale', function () {
+        // given
+        const name = 'fr-FR';
+
+        // when
+        const locale = localeService.getNearestSupportedLocale(name);
+
+        // then
+        expect(locale).to.equal('fr-FR');
+      });
+    });
+
+    context('when given a supported locale but not in canonical form', function () {
+      it('returns the canonical locale form', function () {
+        // given
+        const name = 'fr-fr';
+
+        // when
+        const locale = localeService.getNearestSupportedLocale(name);
+
+        // then
+        expect(locale).to.equal('fr-FR');
+      });
+    });
+
+    context('when given an unsupported locale but the base language is supported', function () {
+      it('returns the base language locale', function () {
+        // given
+        const name = 'fr-CA';
+
+        // when
+        const locale = localeService.getNearestSupportedLocale(name);
+
+        // then
+        expect(locale).to.equal('fr');
+      });
+    });
+
+    context('when given a locale and base language are both not supported', function () {
+      it('returns the default locale', function () {
+        // given
+        const name = 'br_FR';
+
+        // when
+        const locale = localeService.getNearestSupportedLocale(name);
+
+        // then
+        expect(locale).to.equal('fr');
+      });
+    });
+
+    context('when given an invalid name', function () {
+      it('returns the default locale', function () {
+        // given
+        const name = 'anInvalidLocaleName';
+
+        // when
+        const locale = localeService.getNearestSupportedLocale(name);
+
+        // then
+        expect(locale).to.equal('fr');
+      });
     });
   });
 });
