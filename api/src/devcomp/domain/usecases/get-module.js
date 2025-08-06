f93c51d6c33b60bf7f02a770f2e708bc34cc6575
Yvonnick FRIN <frin.yvonnick@gmail.com>
2025-08-06 16:11:06 +0200

    feat(api): add decrypt redirectionHash logic to getModule usecase

--- api/src/devcomp/domain/usecases/get-module.js
+++ api/src/devcomp/domain/usecases/get-module.js
@@ -1,5 +1,21 @@
-async function getModule({ slug, moduleRepository }) {
-  return moduleRepository.getBySlug({ slug });
+import { config } from '../../../shared/config.js';
+import { cryptoService } from '../../../shared/domain/services/crypto-service.js';
+
+async function getModule({ slug, redirectionHash, moduleRepository }) {
+  const module = await moduleRepository.getBySlug({ slug });
+
+  if (redirectionHash) {
+    let redirectionUrl = null;
+    try {
+      redirectionUrl = await cryptoService.decrypt(redirectionHash, config.module.secret);
+      if (redirectionUrl) {
+        module.setRedirectionUrl(redirectionUrl);
+      }
+    } catch {
+      return module;
+    }
+  }
+  return module;
 }
 
 export { getModule };
--- api/tests/devcomp/unit/domain/usecases/get-module_test.js
+++ api/tests/devcomp/unit/domain/usecases/get-module_test.js
@@ -1,4 +1,7 @@
+import { Module } from '../../../../../src/devcomp/domain/models/module/Module.js';
 import { getModule } from '../../../../../src/devcomp/domain/usecases/get-module.js';
+import { config } from '../../../../../src/shared/config.js';
+import { cryptoService } from '../../../../../src/shared/domain/services/crypto-service.js';
 import { expect, sinon } from '../../../../test-helper.js';
 
 describe('Unit | Devcomp | Domain | UseCases | get-module', function () {
@@ -17,6 +20,31 @@ describe('Unit | Devcomp | Domain | UseCases | get-module', function () {
 
       // then
       expect(module).to.equal(expectedModule);
+    });
+
+    it('should get and return a Module with a redirection url', async function () {
+      // given
+      const id = 1;
+      const slug = 'les-adresses-email';
+      const title = 'Les adresses email';
+      const isBeta = false;
+      const grains = [Symbol('text')];
+      const details = Symbol('details');
+      const version = Symbol('version');
+
+      const expectedModule = new Module({ id, slug, title, isBeta, grains, details, version });
+      const moduleRepository = {
+        getBySlug: sinon.stub(),
+      };
+      moduleRepository.getBySlug.withArgs({ slug }).resolves(expectedModule);
+      const expectedUrl = 'https://app.pix.fr/parcours/COMBINIX1';
+      const redirectionHash = await cryptoService.encrypt(expectedUrl, config.module.secret);
+
+      // when
+      const module = await getModule({ slug, redirectionHash, moduleRepository });
+
+      // then
+      expect(module.redirectionUrl).to.equal(expectedUrl);
     });
   });
 });
