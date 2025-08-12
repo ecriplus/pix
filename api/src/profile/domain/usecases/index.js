fba9e5884fac57b969495c207e21090e630a7218
Vincent Hardouin <vincent.hardouin@pix.fr>
2025-08-12 15:25:40 +0200

    feat(api): migrate profile to static import
    Co-authored-by: Xavier Carron <33637571+xav-car@users.noreply.github.com>

--- api/src/profile/domain/usecases/index.js
+++ api/src/profile/domain/usecases/index.js
@@ -1,6 +1,3 @@
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
 import * as competenceEvaluationRepository from '../../../evaluation/infrastructure/repositories/competence-evaluation-repository.js';
 import { repositories } from '../../../profile/infrastructure/repositories/index.js';
 import * as profileRewardRepository from '../../../profile/infrastructure/repositories/profile-reward-repository.js';
@@ -8,19 +5,12 @@ import * as areaRepository from '../../../shared/infrastructure/repositories/are
 import * as competenceRepository from '../../../shared/infrastructure/repositories/competence-repository.js';
 import * as knowledgeElementRepository from '../../../shared/infrastructure/repositories/knowledge-element-repository.js';
 import { injectDependencies } from '../../../shared/infrastructure/utils/dependency-injection.js';
-import { importNamedExportsFromDirectory } from '../../../shared/infrastructure/utils/import-named-exports-from-directory.js';
 import { PromiseUtils } from '../../../shared/infrastructure/utils/promise-utils.js';
 import * as stringUtils from '../../../shared/infrastructure/utils/string-utils.js';
 import * as attestationRepository from '../../infrastructure/repositories/attestation-repository.js';
 import * as campaignParticipationRepository from '../../infrastructure/repositories/campaign-participation-repository.js';
 import * as organizationProfileRewardRepository from '../../infrastructure/repositories/organizations-profile-reward-repository.js';
 import * as rewardRepository from '../../infrastructure/repositories/reward-repository.js';
-
-const path = dirname(fileURLToPath(import.meta.url));
-
-const usecasesWithoutInjectedDependencies = {
-  ...(await importNamedExportsFromDirectory({ path: join(path, './'), ignoredFileNames: ['index.js'] })),
-};
 
 const dependencies = {
   competenceRepository,
@@ -35,6 +25,28 @@ const dependencies = {
   campaignParticipationRepository,
   stringUtils,
   PromiseUtils,
+};
+
+import { getAttestationDataForUsers } from './get-attestation-data-for-users.js';
+import { getAttestationDetails } from './get-attestation-details.js';
+import { getProfileRewardsByUserId } from './get-profile-rewards-by-user-id.js';
+import { getRewardByIdAndType } from './get-reward-by-id-and-type.js';
+import { getSharedAttestationsForOrganizationByUserIds } from './get-shared-attestations-for-organization-by-user-ids.js';
+import { getSharedAttestationsUserDetailForOrganizationByUserIds } from './get-shared-attestations-user-detail-for-organization-by-user-ids.js';
+import { getUserProfile } from './get-user-profile.js';
+import { rewardUser } from './reward-user.js';
+import { shareProfileReward } from './share-profile-reward.js';
+
+const usecasesWithoutInjectedDependencies = {
+  getAttestationDataForUsers,
+  getAttestationDetails,
+  getProfileRewardsByUserId,
+  getRewardByIdAndType,
+  getSharedAttestationsForOrganizationByUserIds,
+  getSharedAttestationsUserDetailForOrganizationByUserIds,
+  getUserProfile,
+  rewardUser,
+  shareProfileReward,
 };
 
 const usecases = injectDependencies(usecasesWithoutInjectedDependencies, dependencies);
