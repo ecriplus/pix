ee2bd052da7d18236da722bf97b34b170be08695
Vincent Hardouin <vincent.hardouin@pix.fr>
2025-08-12 15:18:13 +0200

    feat(api): migrate school to static import
    Co-authored-by: Xavier Carron <33637571+xav-car@users.noreply.github.com>

--- api/src/school/domain/usecases/index.js
+++ api/src/school/domain/usecases/index.js
@@ -1,12 +1,8 @@
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
 import * as areaRepository from '../../../shared/infrastructure/repositories/area-repository.js';
 import * as assessmentRepository from '../../../shared/infrastructure/repositories/assessment-repository.js';
 import * as challengeRepository from '../../../shared/infrastructure/repositories/challenge-repository.js';
 import * as competenceRepository from '../../../shared/infrastructure/repositories/competence-repository.js';
 import { injectDependencies } from '../../../shared/infrastructure/utils/dependency-injection.js';
-import { importNamedExportsFromDirectory } from '../../../shared/infrastructure/utils/import-named-exports-from-directory.js';
 import * as activityAnswerRepository from '../../infrastructure/repositories/activity-answer-repository.js';
 import * as activityRepository from '../../infrastructure/repositories/activity-repository.js';
 import { repositories } from '../../infrastructure/repositories/index.js';
@@ -27,10 +23,44 @@ const dependencies = {
   challengeRepository,
 };
 
-const path = dirname(fileURLToPath(import.meta.url));
+import { activateSchoolSession } from './activate-school-session.js';
+import { correctPreviewAnswer } from './correct-preview-answer.js';
+import { createPreviewAssessment } from './create-preview-assessment.js';
+import { findAllActiveMissions } from './find-all-active-missions.js';
+import {
+  filterByGlobalResult,
+  filterByStatuses,
+  findPaginatedMissionLearners,
+} from './find-paginated-mission-learners.js';
+import { getAssessmentById } from './get-assessment-by-id.js';
+import { getChallenge } from './get-challenge.js';
+import { getCurrentActivity } from './get-current-activity.js';
+import { getDivisions } from './get-divisions.js';
+import { getMission } from './get-mission.js';
+import { getNextChallenge } from './get-next-challenge.js';
+import { getOrganizationLearnerWithMissionIdsByState } from './get-organization-learner-with-completed-mission-ids.js';
+import { getSchoolByCode } from './get-school-by-code.js';
+import { handleActivityAnswer } from './handle-activity-answer.js';
+import { playMission } from './play-mission.js';
 
 const usecasesWithoutInjectedDependencies = {
-  ...(await importNamedExportsFromDirectory({ path: join(path, './'), ignoredFileNames: ['index.js'] })),
+  activateSchoolSession,
+  correctPreviewAnswer,
+  createPreviewAssessment,
+  findAllActiveMissions,
+  filterByGlobalResult,
+  filterByStatuses,
+  findPaginatedMissionLearners,
+  getAssessmentById,
+  getChallenge,
+  getCurrentActivity,
+  getDivisions,
+  getMission,
+  getNextChallenge,
+  getOrganizationLearnerWithMissionIdsByState,
+  getSchoolByCode,
+  handleActivityAnswer,
+  playMission,
 };
 
 const usecases = injectDependencies(usecasesWithoutInjectedDependencies, dependencies);
