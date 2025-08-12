3dc7a44518637634412259d8a2e23d3f3ca788a0
Vincent Hardouin <vincent.hardouin@pix.fr>
2025-08-12 15:34:58 +0200

    feat(api): migrate prescription organization-place to static import
    Co-authored-by: Xavier Carron <33637571+xav-car@users.noreply.github.com>

--- api/src/prescription/organization-place/domain/usecases/index.js
+++ api/src/prescription/organization-place/domain/usecases/index.js
@@ -1,11 +1,7 @@
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
 import * as organizationFeatureApi from '../../../../organizational-entities/application/api/organization-features-api.js';
 import * as organizationLearnerRepository from '../../../../prescription/organization-learner/infrastructure/repositories/organization-learner-repository.js';
 import * as organizationRepository from '../../../../shared/infrastructure/repositories/organization-repository.js';
 import { injectDependencies } from '../../../../shared/infrastructure/utils/dependency-injection.js';
-import { importNamedExportsFromDirectory } from '../../../../shared/infrastructure/utils/import-named-exports-from-directory.js';
 import * as organizationPlacesCapacityRepository from '../../infrastructure/repositories/organization-places-capacity-repository.js';
 import * as organizationPlacesLotRepository from '../../infrastructure/repositories/organization-places-lot-repository.js';
 
@@ -17,13 +13,22 @@ const dependencies = {
   organizationFeatureApi,
 };
 
-const path = dirname(fileURLToPath(import.meta.url));
+import { createOrganizationPlacesLot } from './create-organization-places-lot.js';
+import { deleteOrganizationPlacesLot } from './delete-organization-places-lot.js';
+import { findOrganizationPlacesLot } from './find-organization-places-lot.js';
+import { getDataOrganizationsPlacesStatistics } from './get-data-organizations-places-statistics.js';
+import { getOrganizationPlacesCapacity } from './get-organization-places-capacity.js';
+import { getOrganizationPlacesLots } from './get-organization-places-lots.js';
+import { getOrganizationPlacesStatistics } from './get-organization-places-statistics.js';
 
 const usecasesWithoutInjectedDependencies = {
-  ...(await importNamedExportsFromDirectory({
-    path: join(path, './'),
-    ignoredFileNames: ['index.js'],
-  })),
+  createOrganizationPlacesLot,
+  deleteOrganizationPlacesLot,
+  findOrganizationPlacesLot,
+  getDataOrganizationsPlacesStatistics,
+  getOrganizationPlacesCapacity,
+  getOrganizationPlacesLots,
+  getOrganizationPlacesStatistics,
 };
 
 const usecases = injectDependencies(usecasesWithoutInjectedDependencies, dependencies);
