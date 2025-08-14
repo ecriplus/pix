06d02d48e68bb0238b7809b06eb3eb6cf2c69f7a
Alexandre Monney <alexandre.monney@pix.fr>
2025-08-14 19:15:37 +0200

    feat(api): create saveInBatch method on combined-course-repository

--- api/tests/quest/integration/infrastructure/repositories/combined-course-repository_test.js
+++ api/tests/quest/integration/infrastructure/repositories/combined-course-repository_test.js
@@ -1,7 +1,8 @@
 import { CombinedCourse } from '../../../../../src/quest/domain/models/CombinedCourse.js';
+import { Quest } from '../../../../../src/quest/domain/models/Quest.js';
 import * as combinedCourseRepository from '../../../../../src/quest/infrastructure/repositories/combined-course-repository.js';
 import { NotFoundError } from '../../../../../src/shared/domain/errors.js';
-import { catchErr, databaseBuilder, expect } from '../../../../test-helper.js';
+import { catchErr, databaseBuilder, expect, knex } from '../../../../test-helper.js';
 
 describe('Quest | Integration | Repository | combined-course', function () {
   describe('#getByCode', function () {
@@ -30,6 +31,61 @@ describe('Quest | Integration | Repository | combined-course', function () {
       // then
       expect(error).to.be.instanceOf(NotFoundError);
       expect(error.message).to.equal(`Le parcours combiné portant le code ${code} n'existe pas`);
+    });
+  });
+
+  describe('#saveInBatch', function () {
+    it('should save given combined course', async function () {
+      // given
+      const firstOrganizationId = databaseBuilder.factory.buildOrganization().id;
+      const secondOrganizationId = databaseBuilder.factory.buildOrganization().id;
+      const successRequirements = [
+        {
+          requirement_type: 'campaignParticipations',
+          comparison: 'all',
+          data: {
+            targetProfileId: {
+              data: 1,
+              comparison: 'equal',
+            },
+          },
+        },
+      ];
+      await databaseBuilder.commit();
+
+      const quest = new Quest({
+        successRequirements,
+        eligibilityRequirements: [],
+      });
+      const firstCombinedCourse = new CombinedCourse(
+        {
+          name: 'firstCombinedCourse',
+          code: 'firstCode',
+          organizationId: firstOrganizationId,
+        },
+        quest,
+      );
+      const secondCombinedCourse = new CombinedCourse(
+        {
+          name: 'secondCombinedCourse',
+          code: 'secondCode',
+          organizationId: secondOrganizationId,
+        },
+        quest,
+      );
+
+      // when
+      await combinedCourseRepository.saveInBatch({ combinedCourses: [firstCombinedCourse, secondCombinedCourse] });
+
+      // then
+      const firstSavedCombinedCourse = await knex('quests').where('organizationId', firstOrganizationId).first();
+      const secondSavedCombinedCourse = await knex('quests').where('organizationId', secondOrganizationId).first();
+
+      expect(firstSavedCombinedCourse.name).to.equal('firstCombinedCourse');
+      expect(firstSavedCombinedCourse.successRequirements).to.deep.equal(successRequirements);
+
+      expect(secondSavedCombinedCourse.name).to.equal('secondCombinedCourse');
+      expect(secondSavedCombinedCourse.successRequirements).to.deep.equal(successRequirements);
     });
   });
 });
