c0eda00b1e8928c44430b810ec5d03c868490474
Alexandre Monney <alexandre.monney@pix.fr>
2025-08-22 17:24:30 +0200

    fix(api): allow absolute link in trainings

--- api/src/devcomp/domain/usecases/find-recommended-modules-by-target-profile-ids.js
+++ api/src/devcomp/domain/usecases/find-recommended-modules-by-target-profile-ids.js
@@ -9,7 +9,7 @@ const findRecommendedModulesByTargetProfileIds = async function ({
 
   const recommendedModules = await Promise.all(
     recommendedTrainings.map(async ({ id, link, targetProfileIds }) => {
-      const regexp = /^\/modules\/([a-z0-9-]*)/;
+      const regexp = /\/modules\/([a-z0-9-]*)/;
 
       const result = regexp.exec(link);
       const slug = result[1];
