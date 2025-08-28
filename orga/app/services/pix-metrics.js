6b9c1df5f42875303287ded3c0764d71c31ce967
LionelB <160320+lionelB@users.noreply.github.com>
2025-08-28 15:33:58 +0200

    fix(orga): add default param to trackEvent

--- orga/app/services/pix-metrics.js
+++ orga/app/services/pix-metrics.js
@@ -4,7 +4,7 @@ export default class PixMetricsService extends Service {
   @service metrics;
   @service router;
 
-  trackEvent(eventName, { disabled, ...props }) {
+  trackEvent(eventName, { disabled, ...props } = {}) {
     if (disabled) return;
     const url = this.getRedactedPageUrl();
     this.metrics.trackEvent({ eventName, plausibleAttributes: { u: url }, ...props });
