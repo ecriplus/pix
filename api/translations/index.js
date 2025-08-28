c05e510861c5126d0f0fec18c33d2bc2ee2564f4
Benjamin Petetot <benjamin.petetot@pix.fr>
2025-08-28 12:01:45 +0200

    refactor(api): remove translations index file

--- api/translations/index.js
+++
@@ -1,6 +0,0 @@
-import en from './en.json' with { type: 'json' };
-import es from './es.json' with { type: 'json' };
-import fr from './fr.json' with { type: 'json' };
-import nl from './nl.json' with { type: 'json' };
-
-export { en, es, fr, nl };
