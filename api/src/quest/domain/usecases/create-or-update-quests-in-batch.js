7d2381014d26bb5a4ac7d06565ad85cb73bd84a7
alicegoarnisson <alice.goarnisson@pix.fr>
2025-09-10 11:32:42 +0200

    feat(api): ignores create of update quest in batch if quest id corresponds to a combined course

--- api/src/quest/domain/usecases/create-or-update-quests-in-batch.js
+++ api/src/quest/domain/usecases/create-or-update-quests-in-batch.js
@@ -5,6 +5,7 @@ import { createReadStream } from 'node:fs';
 
 import { getDataBuffer } from '../../../prescription/learner-management/infrastructure/utils/bufferize/get-data-buffer.js';
 import { withTransaction } from '../../../shared/domain/DomainTransaction.js';
+import { NotFoundError } from '../../../shared/domain/errors.js';
 import { CsvParser } from '../../../shared/infrastructure/serializers/csv/csv-parser.js';
 import { QUEST_HEADER } from '../constants.js';
 import { Quest } from '../models/Quest.js';
@@ -14,9 +15,10 @@ export const createOrUpdateQuestsInBatch = withTransaction(
    * @param {Object} params - A parameter object.
    * @param {string} params.filePath - path of csv file which contains quests
    * @param {QuestRepository} params.questRepository - questRepository to use.
+   * @param {QuestRepository} params.combinedCourseRepository - combinedCourseRepository to use.
    * @returns {Promise<void>}
    */
-  async ({ filePath, questRepository }) => {
+  async ({ filePath, questRepository, combinedCourseRepository }) => {
     const deleteQuestIds = [];
     const updatedOrNewQuest = [];
 
@@ -26,13 +28,26 @@ export const createOrUpdateQuestsInBatch = withTransaction(
     const csvParser = new CsvParser(buffer, QUEST_HEADER);
     const csvData = csvParser.parse();
 
-    csvData.forEach(({ questId, content, deleteQuest }) => {
+    for (const { questId, content, deleteQuest } of csvData) {
+      if (questId) {
+        try {
+          const isCombinedCourse = await combinedCourseRepository.getById({ id: questId });
+          if (isCombinedCourse) {
+            continue;
+          }
+        } catch (e) {
+          if (!(e instanceof NotFoundError)) {
+            throw Error;
+          }
+        }
+      }
+
       if (deleteQuest?.toLowerCase() === 'oui' && questId) {
         deleteQuestIds.push(questId);
       } else {
         updatedOrNewQuest.push(new Quest({ id: questId || undefined, ...JSON.parse(content) }));
       }
-    });
+    }
 
     if (deleteQuestIds.length > 0) {
       await questRepository.deleteByIds({ questIds: deleteQuestIds });
