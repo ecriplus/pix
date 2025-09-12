cd3965a547a67b1205e81fff689bf1ff102db8df
P-Jeremy <jemyplu@gmail.com>
2025-09-12 15:37:56 +0200

    feat(api): creat new pix-teams table

---
+++ api/db/migrations/20250912133240_create-pix-teams-table.js
@@ -0,0 +1,24 @@
+const TABLE_NAME = 'pix-teams';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  await knex.schema.createTable(TABLE_NAME, function (table) {
+    table.increments('id').primary();
+    table.string('name').notNullable().comment('Team label');
+    table.dateTime('createdAt').notNullable().defaultTo(knex.fn.now());
+    table.dateTime('updatedAt').notNullable().defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  await knex.schema.dropTable(TABLE_NAME);
+};
+
+export { down, up };
