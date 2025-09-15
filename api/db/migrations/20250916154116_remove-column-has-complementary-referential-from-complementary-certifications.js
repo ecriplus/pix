a44cf6007e368f979416cbd4685d7c48756d57e5
Yannick François <yannick.francois@pix.fr>
2025-09-15 16:42:32 +0200

    :card_file_box: remove `hasComplementaryReferential` column

---
+++ api/db/migrations/20250916154116_remove-column-has-complementary-referential-from-complementary-certifications.js
@@ -0,0 +1,26 @@
+const TABLE_NAME = 'complementary-certifications';
+const COLUMN_NAME = 'hasComplementaryReferential';
+
+const up = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.dropColumn(COLUMN_NAME);
+  });
+};
+
+const down = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.boolean(COLUMN_NAME);
+  });
+
+  await knex(TABLE_NAME).update(COLUMN_NAME, true).where({ key: 'DROIT' });
+  await knex(TABLE_NAME).update(COLUMN_NAME, true).where({ key: 'EDU_1ER_DEGRE' });
+  await knex(TABLE_NAME).update(COLUMN_NAME, true).where({ key: 'EDU_2ND_DEGRE' });
+  await knex(TABLE_NAME).update(COLUMN_NAME, true).where({ key: 'PRO_SANTE' });
+  await knex(TABLE_NAME).update(COLUMN_NAME, false).where({ key: 'CLEA' });
+
+  await knex.schema.alterTable(TABLE_NAME, async (table) => {
+    await table.boolean(COLUMN_NAME).notNullable().alter();
+  });
+};
+
+export { down, up };
