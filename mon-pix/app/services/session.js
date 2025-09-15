d0efd2b10936ac36db2ff7389e2c11e91a60864e
Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>
2025-09-15 16:44:40 +0200

    feat(mon-pix): remove unnecessary call to setBestLocale when user authenticates

--- mon-pix/app/services/session.js
+++ mon-pix/app/services/session.js
@@ -11,7 +11,6 @@ export default class CurrentSessionService extends SessionService {
   @service url;
   @service router;
   @service oidcIdentityProviders;
-  @service locale;
 
   routeAfterAuthentication = 'authenticated.user-dashboard';
 
@@ -23,7 +22,7 @@ export default class CurrentSessionService extends SessionService {
   }
 
   async handleAuthentication() {
-    await this._loadCurrentUserAndLocale();
+    await this.currentUser.load();
 
     const nextURL = this.data.nextURL;
     const isFromIdentityProviderLoginPage = this.oidcIdentityProviders.list.some((identityProvider) => {
@@ -109,11 +108,6 @@ export default class CurrentSessionService extends SessionService {
   revokeGarAuthenticationContext() {
     externalUserTokenFromGarStorage.remove();
     userIdForLearnerAssociationStorage.remove();
-  }
-
-  async _loadCurrentUserAndLocale(queryParams) {
-    await this.currentUser.load();
-    this.locale.setBestLocale({ user: this.currentUser.user, queryParams });
   }
 
   _getRouteAfterInvalidation() {
