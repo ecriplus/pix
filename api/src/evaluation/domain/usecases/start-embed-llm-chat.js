5f0ff40b73cb62af803a6abb792328681587cb16
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2025-09-25 16:53:03 +0200

    feat(api): pass challengeId to LLM usecase
    Co-authored-by: Eric Lim <eric.lim@pix.fr>
    Co-authored-by: Laura Bergoens <laura.bergoens@pix.fr>

--- api/src/evaluation/domain/usecases/start-embed-llm-chat.js
+++ api/src/evaluation/domain/usecases/start-embed-llm-chat.js
@@ -1,11 +1,12 @@
 import { DomainError } from '../../../shared/domain/errors.js';
 
 export async function startEmbedLlmChat({ configId, userId, assessmentId, llmApi, assessmentRepository }) {
-  await checkIfAssessmentBelongsToUser(assessmentId, userId, assessmentRepository);
-  return await llmApi.startChat({ configId, userId, assessmentId });
+  const assessment = await checkIfAssessmentBelongsToUser(assessmentId, userId, assessmentRepository);
+  return await llmApi.startChat({ configId, userId, assessmentId, challengeId: assessment.lastChallengeId });
 }
 
 async function checkIfAssessmentBelongsToUser(assessmentId, userId, assessmentRepository) {
   const assessment = await assessmentRepository.get(assessmentId);
   if (assessment.userId !== userId) throw new DomainError(`This assessment does not belong to user`);
+  return assessment;
 }
