9a25565928fbe6eb41766d9272a3299b2f4246fd
Eric Lim <eric.lim@pix.fr>
2025-09-26 17:35:42 +0200

    feat(api): add build chat and build chat message
    in database builder factory directory

---
+++ api/db/database-builder/factory/build-chat-message.js
@@ -0,0 +1,51 @@
+import { databaseBuffer } from '../database-buffer.js';
+import { buildChat } from './build-chat.js';
+
+const TABLE_NAME = 'chat_messages';
+
+const buildChatMessage = function ({
+  id = databaseBuffer.getNextId(),
+  attachmentName = 'attachmentName',
+  attachmentContext = 'attachmentContext',
+  chatId,
+  content = 'Contenu du message',
+  emitter = 'user',
+  hasAttachmentBeenSubmittedAlongWithAPrompt = false,
+  hasErrorOccurred = null,
+  haveVictoryConditionsBeenFulfilled = false,
+  index = 1,
+  shouldBeForwardedToLLM = true,
+  shouldBeRenderedInPreview = false,
+  shouldBeCountedAsPrompt = false,
+  wasModerated = false,
+  createdAt = new Date(),
+} = {}) {
+  if (!chatId) {
+    chatId = buildChat().id;
+  }
+
+  const values = {
+    id,
+    attachmentName,
+    attachmentContext,
+    chatId,
+    content,
+    emitter,
+    hasAttachmentBeenSubmittedAlongWithAPrompt,
+    hasErrorOccurred,
+    haveVictoryConditionsBeenFulfilled,
+    index,
+    shouldBeForwardedToLLM,
+    shouldBeRenderedInPreview,
+    shouldBeCountedAsPrompt,
+    wasModerated,
+    createdAt,
+  };
+
+  return databaseBuffer.pushInsertable({
+    tableName: TABLE_NAME,
+    values,
+  });
+};
+
+export { buildChatMessage };
