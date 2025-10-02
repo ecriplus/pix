890f89b8d59a737a338335c36aedd4f76de7ace5
LionelB <160320+lionelB@users.noreply.github.com>
2025-10-02 15:15:12 +0200

    feat(orga): update model to include combined-course-statistics

--- orga/app/models/combined-course.js
+++ orga/app/models/combined-course.js
@@ -1,8 +1,9 @@
-import Model, { attr, hasMany } from '@ember-data/model';
+import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 
 export default class CombinedCourse extends Model {
   @attr('string') name;
   @attr('string') code;
   @attr({ defaultValue: () => [] }) campaignIds;
   @hasMany('combined-course-participation', { async: true, inverse: null }) combinedCourseParticipations;
+  @belongsTo('combined-course-statistic', { async: true, inverse: null }) combinedCourseStatistics;
 }
