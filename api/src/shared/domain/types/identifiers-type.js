2bfb40c0a40c813f0f2b417e127d3923035d7e04
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2025-10-03 15:45:07 +0200

    tech(api): use combinedCourseId to validate access

--- api/src/shared/domain/types/identifiers-type.js
+++ api/src/shared/domain/types/identifiers-type.js
@@ -56,6 +56,7 @@ const typesPositiveInteger32bits = [
   'certificationCenterMembershipId',
   'certificationCourseId',
   'certificationIssueReportId',
+  'combinedCourseId',
   'complementaryCertificationBadgeId',
   'complementaryCertificationCourseId',
   'complementaryCertificationId',
