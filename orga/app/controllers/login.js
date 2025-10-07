0d5d9c77c1037c1ec58b6835029500293620abe9
Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>
2025-10-07 08:33:28 +0200

    feat(orga): prepare login form

--- orga/app/controllers/login.js
+++ orga/app/controllers/login.js
@@ -3,8 +3,13 @@ import { service } from '@ember/service';
 
 export default class LoginController extends Controller {
   @service currentDomain;
+  @service featureToggles;
 
   get isInternationalDomain() {
     return !this.currentDomain.isFranceDomain;
+  }
+
+  get isNewAuthDesignEnabled() {
+    return this.featureToggles.featureToggles.usePixOrgaNewAuthDesign;
   }
 }
