87d894f77013e84c9255daac53463d0d3d0bfd9b
Thomas Bazin <thomas.bazin@pix.fr>
2025-10-08 09:29:07 +0200

    feat(api): add administration team check before update

--- api/tests/organizational-entities/integration/domain/usecases/update-organization-information.usecase_test.js
+++ api/tests/organizational-entities/integration/domain/usecases/update-organization-information.usecase_test.js
@@ -1,5 +1,8 @@
+import { AdministrationTeamNotFound } from '../../../../../src/organizational-entities/domain/errors.js';
+import { OrganizationForAdmin } from '../../../../../src/organizational-entities/domain/models/OrganizationForAdmin.js';
 import { usecases } from '../../../../../src/organizational-entities/domain/usecases/index.js';
 import {
+  catchErr,
   databaseBuilder,
   domainBuilder,
   expect,
@@ -7,10 +10,12 @@ import {
 } from '../../../../test-helper.js';
 
 describe('Integration | Organizational Entities | Domain | UseCases | update-organization', function () {
+  beforeEach(async function () {
+    await insertMultipleSendingFeatureForNewOrganization();
+  });
+
   it('updates organization information', async function () {
     // given
-    await insertMultipleSendingFeatureForNewOrganization();
-
     const organizationId = databaseBuilder.factory.buildOrganization().id;
 
     const newAdministrationTeamId = databaseBuilder.factory.buildAdministrationTeam().id;
@@ -29,7 +34,30 @@ describe('Integration | Organizational Entities | Domain | UseCases | update-org
     });
 
     // then
+    expect(updatedOrganization).to.be.instanceOf(OrganizationForAdmin);
     expect(updatedOrganization.name).to.equal("Nouveau nom d'organization");
     expect(updatedOrganization.administrationTeamId).to.equal(newAdministrationTeamId);
+  });
+
+  context('when administration team does not exist', function () {
+    it('throws an AdministrationTeamNotFound error', async function () {
+      // given
+      const organizationId = databaseBuilder.factory.buildOrganization().id;
+
+      await databaseBuilder.commit();
+
+      const organizationNewInformations = domainBuilder.buildOrganizationForAdmin({
+        id: organizationId,
+        administrationTeamId: 123,
+      });
+
+      // when
+      const error = await catchErr(usecases.updateOrganizationInformation)({
+        organization: organizationNewInformations,
+      });
+
+      // then
+      expect(error).to.be.instanceOf(AdministrationTeamNotFound);
+    });
   });
 });
--- api/tests/organizational-entities/unit/domain/usecases/update-organization-information.usecase.test.js
+++ api/tests/organizational-entities/unit/domain/usecases/update-organization-information.usecase.test.js
@@ -1,10 +1,11 @@
+import { AdministrationTeamNotFound } from '../../../../../src/organizational-entities/domain/errors.js';
 import { OrganizationForAdmin } from '../../../../../src/organizational-entities/domain/models/OrganizationForAdmin.js';
 import { usecases } from '../../../../../src/organizational-entities/domain/usecases/index.js';
 import { NotFoundError } from '../../../../../src/shared/domain/errors.js';
 import { catchErr, domainBuilder, expect, sinon } from '../../../../test-helper.js';
 
 describe('Unit | Organizational Entities | Domain | UseCase | update-organization-information', function () {
-  let organizationForAdminRepository, tagRepository;
+  let organizationForAdminRepository, tagRepository, administrationTeamRepository;
 
   beforeEach(function () {
     organizationForAdminRepository = {
@@ -14,30 +15,49 @@ describe('Unit | Organizational Entities | Domain | UseCase | update-organizatio
     tagRepository = {
       findByIds: sinon.stub(),
     };
+    administrationTeamRepository = {
+      getById: sinon.stub(),
+    };
   });
 
   it('should update organization information', async function () {
     // given
-    const givenOrganization = domainBuilder.buildOrganizationForAdmin();
+    const givenOrganization = domainBuilder.buildOrganizationForAdmin({
+      administrationTeamId: Symbol('administrationTeamId'),
+    });
 
     const existingOrganizationForAdmin = domainBuilder.buildOrganizationForAdmin({
       organizationId: givenOrganization.id,
     });
-    sinon.stub(existingOrganizationForAdmin, 'updateWithDataProtectionOfficerAndTags');
     organizationForAdminRepository.get.onCall(0).returns(existingOrganizationForAdmin);
+
+    sinon.stub(existingOrganizationForAdmin, 'updateWithDataProtectionOfficerAndTags');
+
     const updatedOrganization = domainBuilder.buildOrganizationForAdmin({ organizationId: givenOrganization.id });
     organizationForAdminRepository.get.onCall(1).returns(updatedOrganization);
+
     const tagsToUpdate = Symbol('tagsToUpdate');
     tagRepository.findByIds.withArgs(givenOrganization.tagIds).resolves(tagsToUpdate);
+
+    const existingAdministrationTeam = Symbol('existingAdministrationTeam');
+
+    administrationTeamRepository.getById
+      .withArgs(givenOrganization.administrationTeamId)
+      .resolves(existingAdministrationTeam);
+
     // when
     const result = await usecases.updateOrganizationInformation({
       organization: givenOrganization,
       organizationForAdminRepository,
       tagRepository,
+      administrationTeamRepository,
     });
 
     // then
     expect(organizationForAdminRepository.get).to.have.been.calledWithExactly({ organizationId: givenOrganization.id });
+
+    expect(administrationTeamRepository.getById).to.have.been.calledWithExactly(givenOrganization.administrationTeamId);
+
     expect(existingOrganizationForAdmin.updateWithDataProtectionOfficerAndTags).to.have.been.calledWithExactly(
       givenOrganization,
       givenOrganization.dataProtectionOfficer,
@@ -63,6 +83,37 @@ describe('Unit | Organizational Entities | Domain | UseCase | update-organizatio
 
       // then
       expect(error).to.be.instanceOf(NotFoundError);
+    });
+
+    it('should reject with an AdministrationTeamNotFound (DomainError) when the administrationTeam does not exist', async function () {
+      // given
+      const givenOrganization = domainBuilder.buildOrganizationForAdmin({ administrationTeamId: 123 });
+
+      const existingOrganizationForAdmin = domainBuilder.buildOrganizationForAdmin({
+        organizationId: givenOrganization.id,
+      });
+      sinon.stub(existingOrganizationForAdmin, 'updateWithDataProtectionOfficerAndTags');
+
+      organizationForAdminRepository.get.onCall(0).returns(existingOrganizationForAdmin);
+
+      const updatedOrganization = domainBuilder.buildOrganizationForAdmin({ organizationId: givenOrganization.id });
+      organizationForAdminRepository.get.onCall(1).returns(updatedOrganization);
+
+      const tagsToUpdate = Symbol('tagsToUpdate');
+      tagRepository.findByIds.withArgs(givenOrganization.tagIds).resolves(tagsToUpdate);
+
+      administrationTeamRepository.getById.withArgs(givenOrganization.administrationTeamId).resolves(null);
+
+      // when
+      const error = await catchErr(usecases.updateOrganizationInformation)({
+        organization: givenOrganization,
+        organizationForAdminRepository,
+        tagRepository,
+        administrationTeamRepository,
+      });
+
+      // then
+      expect(error).to.be.instanceOf(AdministrationTeamNotFound);
     });
   });
 });
