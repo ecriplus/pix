1e18c3b0794dc1f2c970644678aa1832d98bd7e7
Baptiste H <39197212+baptiste-h@users.noreply.github.com>
2025-10-14 18:24:03 +0200

    feat(api): Add new properties to serializer

--- api/tests/quest/unit/infrastructure/serializers/combined-course-participation-serializer_test.js
+++ api/tests/quest/unit/infrastructure/serializers/combined-course-participation-serializer_test.js
@@ -1,18 +1,22 @@
-import { CombinedCourseParticipation } from '../../../../../src/quest/domain/models/CombinedCourseParticipation.js';
+import { CombinedCourseParticipationStatuses } from '../../../../../src/prescription/shared/domain/constants.js';
+import { CombinedCourseParticipationDetails } from '../../../../../src/quest/domain/models/CombinedCourseParticipationDetails.js';
 import { serialize } from '../../../../../src/quest/infrastructure/serializers/combined-course-participation-serializer.js';
 import { expect } from '../../../../test-helper.js';
 
 describe('CombinedCourseParticipationSerializer', function () {
-  it('should serialize a CombinedCourseParticipation', function () {
-    const combinedCourseParticipation = new CombinedCourseParticipation({
+  it('should serialize a CombinedCourseParticipationDetails', function () {
+    const combinedCourseParticipation = new CombinedCourseParticipationDetails({
       id: 1,
       firstName: 'John',
       lastName: 'Doe',
-      questId: 123,
-      organizationLearnerId: 456,
-      status: 'COMPLETED',
+      status: CombinedCourseParticipationStatuses.COMPLETED,
       createdAt: new Date('2023-01-01T00:00:00Z'),
       updatedAt: new Date('2023-01-02T00:00:00Z'),
+      hasFormationItem: true,
+      nbModules: 1,
+      nbCampaigns: 1,
+      nbModulesCompleted: 0,
+      nbCampaignsCompleted: 1,
     });
 
     const serialized = serialize(combinedCourseParticipation);
@@ -24,9 +28,14 @@ describe('CombinedCourseParticipationSerializer', function () {
         attributes: {
           'first-name': 'John',
           'last-name': 'Doe',
-          status: 'COMPLETED',
+          status: CombinedCourseParticipationStatuses.COMPLETED,
           'created-at': new Date('2023-01-01T00:00:00Z'),
           'updated-at': new Date('2023-01-02T00:00:00Z'),
+          'has-formation-item': true,
+          'nb-modules': 1,
+          'nb-campaigns': 1,
+          'nb-modules-completed': 0,
+          'nb-campaigns-completed': 1,
         },
       },
     });
