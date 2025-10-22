469129d2dcde8c35858de80a05b483f50506362e
AndreiaPena <andreia.pena@pix.fr>
2025-10-22 15:39:57 +0200

    🐛 (orga): fix ember inspector

--- orga/app/app.js
+++ orga/app/app.js
@@ -1,28 +1,16 @@
-import { RSVP } from '@ember/-internals/runtime';
 import Application from '@ember/application';
-import * as runtime from '@glimmer/runtime';
-import * as tracking from '@glimmer/tracking';
-import * as validator from '@glimmer/validator';
-import Ember from 'ember';
+import setupInspector from '@embroider/legacy-inspector-support/ember-source-4.12';
 import loadInitializers from 'ember-load-initializers';
 import Resolver from 'ember-resolver';
 
 import config from './config/environment';
 
-// This is a temporary solution, see https://github.com/emberjs/ember-inspector/issues/2612
-window.define('@glimmer/tracking', () => tracking);
-window.define('@glimmer/runtime', () => runtime);
-window.define('@glimmer/validator', () => validator);
-window.define('rsvp', () => RSVP);
-window.define('ember', () => ({ default: Ember }));
-window.define('<my-app>/config/environment', () => ({
-  default: config,
-}));
-
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
+
+  inspector = setupInspector(this);
 }
 
 loadInitializers(App, config.modulePrefix);
