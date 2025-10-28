e882879f31e2053202cdbef671e4ccbc8b20c515
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2025-10-28 10:59:48 +0100

    feat(api): add migration for 1 new column (applicationTld) to oidc-providers table

---
+++ api/db/migrations/20251028094520_update-oidc-providers-for-application-tld.js
@@ -0,0 +1,27 @@
+const TABLE_NAME = 'oidc-providers';
+const COLUMN_NAME = 'applicationTld';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table
+      .string(COLUMN_NAME)
+      .defaultTo(null)
+      .comment('TLD of the application for which this client configuration is for');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.dropColumn(COLUMN_NAME);
+  });
+};
+
+export { down, up };
