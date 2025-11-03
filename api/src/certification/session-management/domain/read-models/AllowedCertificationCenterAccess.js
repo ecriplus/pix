42d0291ec9d20091adc46cc29b6f85617bcbbc50
Yannick François <yannick.francois@pix.fr>
2025-11-03 11:24:46 +0100

    :wastebasket: remove unused constructor

--- api/src/certification/session-management/domain/read-models/AllowedCertificationCenterAccess.js
+++ api/src/certification/session-management/domain/read-models/AllowedCertificationCenterAccess.js
@@ -1,11 +1,4 @@
 /**
  * @class AllowedCertificationCenterAccess
  */
-export class AllowedCertificationCenterAccess {
-  /**
-   * @param {Object} params
-   */
-  constructor(params = {}) {
-    Object.assign(this, params);
-  }
-}
+export class AllowedCertificationCenterAccess {}
