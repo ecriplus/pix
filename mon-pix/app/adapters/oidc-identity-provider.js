01ce5d9053e135638500d2d834ca1471c55d8fcc
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2025-11-13 18:05:09 +0100

    feat(mon-pix): call modified route /api/oidc/identity-providers/{.fr,.org} to not share cache
    between application on .fr and application on .org (cache buster).

--- mon-pix/app/adapters/oidc-identity-provider.js
+++ mon-pix/app/adapters/oidc-identity-provider.js
@@ -1,7 +1,11 @@
+import { service } from '@ember/service';
+
 import ApplicationAdapter from './application';
 
 export default class OidcIdentityProviderAdapter extends ApplicationAdapter {
+  @service currentDomain;
+
   urlForFindAll() {
-    return `${this.host}/${this.namespace}/oidc/identity-providers`;
+    return `${this.host}/${this.namespace}/oidc/identity-providers/${this.currentDomain.getExtension()}`;
   }
 }
