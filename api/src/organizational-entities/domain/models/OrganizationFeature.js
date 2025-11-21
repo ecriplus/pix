26b0f603cc35e27f481993b0286551bf1cb453b0
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2025-11-21 14:42:34 +0100

    feat(api): update organization feature model

--- api/src/organizational-entities/domain/models/OrganizationFeature.js
+++ api/src/organizational-entities/domain/models/OrganizationFeature.js
@@ -1,15 +1,97 @@
+import Joi from 'joi';
+
+import { ORGANIZATION_FEATURE } from '../../../shared/domain/constants.js';
+import { EntityValidationError } from '../../../shared/domain/errors.js';
+
 class OrganizationFeature {
+  #schema = Joi.object({
+    featureName: Joi.string()
+      .valid(
+        ORGANIZATION_FEATURE.CAMPAIGN_WITHOUT_USER_PROFILE.key,
+        ORGANIZATION_FEATURE.COVER_RATE.key,
+        ORGANIZATION_FEATURE.MISSIONS_MANAGEMENT.key,
+        ORGANIZATION_FEATURE.ORALIZATION_MANAGED_BY_PRESCRIBER.key,
+        ORGANIZATION_FEATURE.CAMPAIGN_WITHOUT_USER_PROFILE.key,
+        ORGANIZATION_FEATURE.COVER_RATE.key,
+        ORGANIZATION_FEATURE.COMPUTE_ORGANIZATION_LEARNER_CERTIFICABILITY.key,
+        ORGANIZATION_FEATURE.ATTESTATIONS_MANAGEMENT.key,
+        ORGANIZATION_FEATURE.MULTIPLE_SENDING_ASSESSMENT.key,
+        ORGANIZATION_FEATURE.PLACES_MANAGEMENT.key,
+        ORGANIZATION_FEATURE.LEARNER_IMPORT.key,
+      )
+      .required(),
+    params: Joi.when('featureName', {
+      switch: [
+        {
+          is: Joi.valid(ORGANIZATION_FEATURE.ATTESTATIONS_MANAGEMENT.key),
+          then: Joi.array().required(),
+        },
+        {
+          is: Joi.valid(
+            ORGANIZATION_FEATURE.PLACES_MANAGEMENT.key,
+            ORGANIZATION_FEATURE.ORALIZATION_MANAGED_BY_PRESCRIBER.key,
+          ),
+          then: Joi.object().allow(null).optional(),
+        },
+        {
+          is: Joi.valid(ORGANIZATION_FEATURE.LEARNER_IMPORT.key),
+          then: Joi.object().required(),
+        },
+        {
+          is: Joi.valid(
+            ORGANIZATION_FEATURE.CAMPAIGN_WITHOUT_USER_PROFILE.key,
+            ORGANIZATION_FEATURE.COVER_RATE.key,
+            ORGANIZATION_FEATURE.MISSIONS_MANAGEMENT.key,
+            ORGANIZATION_FEATURE.CAMPAIGN_WITHOUT_USER_PROFILE.key,
+            ORGANIZATION_FEATURE.COVER_RATE.key,
+            ORGANIZATION_FEATURE.COMPUTE_ORGANIZATION_LEARNER_CERTIFICABILITY.key,
+            ORGANIZATION_FEATURE.MULTIPLE_SENDING_ASSESSMENT.key,
+          ),
+          then: Joi.valid(null, '').optional(),
+        },
+      ],
+    }),
+    deleteLearner: Joi.when('featureName', {
+      is: Joi.string().required().valid(ORGANIZATION_FEATURE.LEARNER_IMPORT.key),
+      then: Joi.valid('Y', 'N').allow(null).optional(),
+      otherwise: Joi.valid(null, '').optional(),
+    }),
+    organizationId: Joi.number().integer().required(),
+  });
+
   #deleteLearner;
-  constructor({ featureId, organizationId, params, deleteLearner }) {
-    this.featureId = parseInt(featureId, 10);
-    this.organizationId = parseInt(organizationId, 10);
-    this.params = params ? JSON.parse(params) : null;
+  constructor({ featureName, organizationId, params, deleteLearner, features }) {
+    const parsedParams = params ? JSON.parse(params) : null;
+    const parsedOrganizationId = parseInt(organizationId, 10);
+    const selectedFeature = features.find(({ key }) => key === featureName);
+
+    if (!selectedFeature) {
+      throw new EntityValidationError(
+        {
+          invalidAttributes: [{ attribute: 'featureName', message: 'feature not found' }],
+        },
+        'UNKNOWN_FEATURE',
+      );
+    }
+
+    this.#validate({ featureName, params: parsedParams, deleteLearner, organizationId });
+
+    this.featureId = selectedFeature.id;
+    this.organizationId = parsedOrganizationId;
+    this.params = parsedParams;
 
     this.#deleteLearner = deleteLearner === 'Y';
   }
 
   get deleteLearner() {
     return this.#deleteLearner;
+  }
+
+  #validate(data) {
+    const { error } = this.#schema.validate(data, { allowUnknown: false });
+    if (error) {
+      throw EntityValidationError.fromJoiErrors(error.details);
+    }
   }
 }
 
--- api/tests/organizational-entities/unit/domain/models/OrganizationFeature_test.js
+++ api/tests/organizational-entities/unit/domain/models/OrganizationFeature_test.js
@@ -1,20 +1,95 @@
 import { OrganizationFeature } from '../../../../../src/organizational-entities/domain/models/OrganizationFeature.js';
-import { expect } from '../../../../test-helper.js';
+import { ORGANIZATION_FEATURE } from '../../../../../src/shared/domain/constants.js';
+import { EntityValidationError } from '../../../../../src/shared/domain/errors.js';
+import { catchErrSync, expect } from '../../../../test-helper.js';
 
 describe('Unit | Organizational Entities | Domain | Model | OrganizationFeature', function () {
-  let organizationFeature, featureId, organizationId, params;
+  let organizationFeature, featureName, organizationId, params, features;
 
-  beforeEach(function () {
-    // given
-    featureId = '1';
-    organizationId = '2';
-    params = `{"id": 3}`;
+  describe('#DEFAULT', function () {
+    beforeEach(function () {
+      // given
+      featureName = 'TOTO';
+      organizationId = '2';
+      params = `{"id": 3}`;
+      features = [
+        {
+          key: 'TATA',
+          id: 1,
+        },
+      ];
+    });
+
+    it('should throw EntityErrorValidation given unknown feature', function () {
+      //when
+      const error = catchErrSync(() => new OrganizationFeature({ featureName, organizationId, params, features }))();
+
+      expect(error).instanceOf(EntityValidationError);
+      expect(error.code).equal('UNKNOWN_FEATURE');
+    });
   });
 
-  describe('#constructor', function () {
+  describe('#ATTESTATIONS_MANAGEMENT', function () {
+    beforeEach(function () {
+      // given
+      featureName = ORGANIZATION_FEATURE.ATTESTATIONS_MANAGEMENT.key;
+      organizationId = '2';
+      params = `["RIRI","FIFI","LOULOU"]`;
+      features = [
+        {
+          key: ORGANIZATION_FEATURE.ATTESTATIONS_MANAGEMENT.key,
+          id: 1,
+        },
+      ];
+    });
+
     it('should initialize an instance with given params', function () {
       //when
-      organizationFeature = new OrganizationFeature({ featureId, organizationId, params });
+      organizationFeature = new OrganizationFeature({ featureName, organizationId, params, features });
+      // then
+      expect(organizationFeature).to.deep.equal({
+        featureId: 1,
+        organizationId: 2,
+        params: ['RIRI', 'FIFI', 'LOULOU'],
+      });
+    });
+
+    it('should throw EntityErrorValidation given empty params', function () {
+      //when
+      const error = catchErrSync(() => new OrganizationFeature({ featureName, organizationId, features }))();
+
+      // then
+      expect(error).instanceOf(EntityValidationError);
+    });
+
+    it('should throw EntityErrorValidation on deleteLearner', function () {
+      //when
+      const error = catchErrSync(
+        () => new OrganizationFeature({ featureName, organizationId, deleteLearner: 'Y', features }),
+      )();
+
+      // then
+      expect(error).instanceOf(EntityValidationError);
+    });
+  });
+
+  describe('#ORALIZATION_MANAGED_BY_PRESCRIBER', function () {
+    beforeEach(function () {
+      // given
+      featureName = ORGANIZATION_FEATURE.ORALIZATION_MANAGED_BY_PRESCRIBER.key;
+      organizationId = '2';
+      params = `{"id": 3}`;
+      features = [
+        {
+          key: ORGANIZATION_FEATURE.ORALIZATION_MANAGED_BY_PRESCRIBER.key,
+          id: 1,
+        },
+      ];
+    });
+
+    it('should initialize an instance with given params', function () {
+      //when
+      organizationFeature = new OrganizationFeature({ featureName, organizationId, params, features });
       // then
       expect(organizationFeature).to.deep.equal({
         featureId: 1,
@@ -22,21 +97,217 @@ describe('Unit | Organizational Entities | Domain | Model | OrganizationFeature'
         params: { id: 3 },
       });
     });
-  });
 
-  describe('#deleteLearner', function () {
-    it('should activate learner deletion given params', function () {
+    it('should initialize an instance with given empty params', function () {
       //when
-      organizationFeature = new OrganizationFeature({ featureId, organizationId, params, deleteLearner: 'Y' });
+      organizationFeature = new OrganizationFeature({ featureName, organizationId, features });
+
       // then
-      expect(organizationFeature.deleteLearner).to.be.true;
+      expect(organizationFeature).to.deep.equal({
+        featureId: 1,
+        organizationId: 2,
+        params: null,
+      });
     });
 
-    it('should deactivate learner deletion given params', function () {
+    it('should throw EntityErrorValidation on deleteLearner', function () {
       //when
-      organizationFeature = new OrganizationFeature({ featureId, organizationId, params });
+      const error = catchErrSync(
+        () => new OrganizationFeature({ featureName, organizationId, deleteLearner: 'Y', features }),
+      )();
+
       // then
-      expect(organizationFeature.deleteLearner).to.be.false;
+      expect(error).instanceOf(EntityValidationError);
+    });
+  });
+
+  describe('#PLACES_MANAGEMENT', function () {
+    beforeEach(function () {
+      // given
+      featureName = ORGANIZATION_FEATURE.PLACES_MANAGEMENT.key;
+      organizationId = '2';
+      params = `{"id": 3}`;
+      features = [
+        {
+          key: ORGANIZATION_FEATURE.PLACES_MANAGEMENT.key,
+          id: 1,
+        },
+      ];
+    });
+
+    it('should initialize an instance with given params', function () {
+      //when
+      organizationFeature = new OrganizationFeature({ featureName, organizationId, params, features });
+      // then
+      expect(organizationFeature).to.deep.equal({
+        featureId: 1,
+        organizationId: 2,
+        params: { id: 3 },
+      });
+    });
+
+    it('should initialize an instance with given empty params', function () {
+      //when
+      organizationFeature = new OrganizationFeature({ featureName, organizationId, features });
+
+      // then
+      expect(organizationFeature).to.deep.equal({
+        featureId: 1,
+        organizationId: 2,
+        params: null,
+      });
+    });
+
+    it('should throw EntityErrorValidation on deleteLearner', function () {
+      //when
+      const error = catchErrSync(
+        () => new OrganizationFeature({ featureName, organizationId, deleteLearner: 'Y', features }),
+      )();
+
+      // then
+      expect(error).instanceOf(EntityValidationError);
+    });
+  });
+
+  describe('#LEARNER_IMPORT', function () {
+    beforeEach(function () {
+      // given
+      featureName = ORGANIZATION_FEATURE.LEARNER_IMPORT.key;
+      organizationId = '2';
+      params = `{"id": 3}`;
+      features = [
+        {
+          key: ORGANIZATION_FEATURE.LEARNER_IMPORT.key,
+          id: 1,
+        },
+      ];
+    });
+
+    it('should initialize an instance with given params', function () {
+      //when
+      organizationFeature = new OrganizationFeature({ featureName, organizationId, params, features });
+      // then
+      expect(organizationFeature).to.deep.equal({
+        featureId: 1,
+        organizationId: 2,
+        params: { id: 3 },
+      });
+    });
+
+    it('should throw EntityErrorValidation on empty params', function () {
+      //when
+      const error = catchErrSync(() => new OrganizationFeature({ featureName, organizationId, features }))();
+
+      // then
+      expect(error).instanceOf(EntityValidationError);
+    });
+
+    it('should throw EntityErrorValidation on non object params', function () {
+      //when
+      const error = catchErrSync(
+        () => new OrganizationFeature({ featureName, organizationId, features, params: '[]' }),
+      )();
+
+      // then
+      expect(error).instanceOf(EntityValidationError);
+    });
+
+    describe('#deleteLearner', function () {
+      it('should activate learner deletion given yes params', function () {
+        //when
+        organizationFeature = new OrganizationFeature({
+          featureName,
+          organizationId,
+          params,
+          deleteLearner: 'Y',
+          features,
+        });
+        // then
+        expect(organizationFeature.deleteLearner).to.be.true;
+      });
+
+      it('should deactivate learner deletion given no params', function () {
+        //when
+        organizationFeature = new OrganizationFeature({
+          featureName,
+          organizationId,
+          params,
+          deleteLearner: 'N',
+          features,
+        });
+        // then
+        expect(organizationFeature.deleteLearner).to.be.false;
+      });
+
+      it('should deactivate learner deletion given empty params', function () {
+        //when
+        organizationFeature = new OrganizationFeature({ featureName, organizationId, params, features });
+        // then
+        expect(organizationFeature.deleteLearner).to.be.false;
+      });
+
+      it('should throw EntityErrorValidation on invalid value deleteLearner', function () {
+        //when
+        const error = catchErrSync(
+          () => new OrganizationFeature({ featureName, organizationId, deleteLearner: 'Oui oui', features }),
+        )();
+
+        // then
+        expect(error).instanceOf(EntityValidationError);
+      });
+    });
+  });
+
+  [
+    ORGANIZATION_FEATURE.CAMPAIGN_WITHOUT_USER_PROFILE.key,
+    ORGANIZATION_FEATURE.COVER_RATE.key,
+    ORGANIZATION_FEATURE.MISSIONS_MANAGEMENT.key,
+    ORGANIZATION_FEATURE.CAMPAIGN_WITHOUT_USER_PROFILE.key,
+    ORGANIZATION_FEATURE.COVER_RATE.key,
+    ORGANIZATION_FEATURE.COMPUTE_ORGANIZATION_LEARNER_CERTIFICABILITY.key,
+    ORGANIZATION_FEATURE.MULTIPLE_SENDING_ASSESSMENT.key,
+  ].forEach((featureName) => {
+    describe(`#${featureName}`, function () {
+      beforeEach(function () {
+        // given
+        organizationId = '2';
+        params = `{"id": 3}`;
+        features = [
+          {
+            key: featureName,
+            id: 1,
+          },
+        ];
+      });
+
+      it('should initialize an instance with given params', function () {
+        //when
+        organizationFeature = new OrganizationFeature({ featureName, organizationId, features });
+        // then
+        expect(organizationFeature).to.deep.equal({
+          featureId: 1,
+          organizationId: 2,
+          params: null,
+        });
+      });
+
+      it('should throw EntityErrorValidation given params', function () {
+        //when
+        const error = catchErrSync(() => new OrganizationFeature({ featureName, organizationId, features, params }))();
+
+        // then
+        expect(error).instanceOf(EntityValidationError);
+      });
+
+      it('should throw EntityErrorValidation on deleteLearner', function () {
+        //when
+        const error = catchErrSync(
+          () => new OrganizationFeature({ featureName, organizationId, deleteLearner: 'Y', features }),
+        )();
+
+        // then
+        expect(error).instanceOf(EntityValidationError);
+      });
     });
   });
 });
