12cb7d6967b20204f5626e44d942fef5aef21fc0
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2025-11-21 11:10:09 +0100

    feat(api): add connectionMethodCode support for reconcile-oidc-user-for-admin usecase

--- api/tests/identity-access-management/acceptance/application/oidc-provider.admin.route.test.js
+++ api/tests/identity-access-management/acceptance/application/oidc-provider.admin.route.test.js
@@ -15,14 +15,11 @@ import { createMockedTestOidcProvider } from '../../../tooling/openid-client/ope
 describe('Acceptance | Identity Access Management | Route | Admin | oidc-provider', function () {
   let server;
 
-  beforeEach(async function () {
-    await createMockedTestOidcProvider({ application: 'admin', applicationTld: '.fr' });
-    server = await createServer();
-  });
-
   describe('POST /api/admin/oidc-providers/import', function () {
     it('imports oidc providers and returns an HTTP status code 204', async function () {
       // given
+      server = await createServer();
+
       const superAdmin = await insertUserWithRoleSuperAdmin();
       const payload = [
         {
@@ -81,6 +78,11 @@ describe('Acceptance | Identity Access Management | Route | Admin | oidc-provide
   });
 
   describe('GET /api/admin/oidc/identity-providers', function () {
+    beforeEach(async function () {
+      await createMockedTestOidcProvider({ application: 'admin', applicationTld: '.fr' });
+      server = await createServer();
+    });
+
     it('returns the list of all oidc providers with an HTTP status code 200', async function () {
       // given
       const superAdmin = await insertUserWithRoleSuperAdmin();
@@ -113,6 +115,11 @@ describe('Acceptance | Identity Access Management | Route | Admin | oidc-provide
   });
 
   describe('POST /api/admin/oidc/user/reconcile', function () {
+    beforeEach(async function () {
+      await createMockedTestOidcProvider({ application: 'admin', applicationTld: '.fr' });
+      server = await createServer();
+    });
+
     it('returns an accessToken with a 200 HTTP status code', async function () {
       // given
       const start = new Date();
@@ -181,6 +188,95 @@ describe('Acceptance | Identity Access Management | Route | Admin | oidc-provide
       const lastUserApplicationConnection = await knex('last-user-application-connections').first();
       expect(lastUserApplicationConnection.application).to.equal('admin');
       expect(lastUserApplicationConnection.lastLoggedAt).to.be.greaterThanOrEqual(start);
+    });
+  });
+
+  context('when the OIDC provider has a connectionMethodCode', function () {
+    beforeEach(async function () {
+      await createMockedTestOidcProvider({ application: 'admin', applicationTld: '.fr' });
+      server = await createServer();
+    });
+
+    describe('POST /api/admin/oidc/user/reconcile', function () {
+      beforeEach(async function () {
+        await createMockedTestOidcProvider({
+          application: 'admin',
+          applicationTld: '.fr',
+          identityProvider: 'OIDC_EXAMPLE_NET-ADMIN',
+          connectionMethodCode: 'OIDC_EXAMPLE_NET',
+        });
+        server = await createServer();
+      });
+
+      it('returns an accessToken with a 200 HTTP status code', async function () {
+        // given
+        const start = new Date();
+
+        const user = databaseBuilder.factory.buildUser.withRawPassword({
+          email: 'eva.poree@example.net',
+          rawPassword: 'pix123',
+        });
+        await databaseBuilder.commit();
+
+        const idToken = jsonwebtoken.sign(
+          {
+            given_name: 'Brice',
+            family_name: 'Glace',
+            sub: 'some-user-unique-id',
+          },
+          'secret',
+        );
+        const userAuthenticationKey = await authenticationSessionService.save({
+          sessionContent: { idToken },
+          userInfo: {
+            firstName: 'Brice',
+            lastName: 'Glace',
+            externalIdentityId: 'some-user-unique-id',
+          },
+        });
+
+        // when
+        const response = await server.inject({
+          method: 'POST',
+          url: `/api/admin/oidc/user/reconcile`,
+          headers: {
+            'x-forwarded-proto': 'https',
+            'x-forwarded-host': 'admin.pix.fr',
+          },
+          payload: {
+            data: {
+              attributes: {
+                identity_provider: 'OIDC_EXAMPLE_NET-ADMIN',
+                authentication_key: userAuthenticationKey,
+                email: user.email,
+              },
+            },
+          },
+        });
+
+        // then
+        expect(response.statusCode).to.equal(200);
+
+        const result = response.result;
+        expect(result).to.have.property('access_token');
+
+        const decodedAccessToken = tokenService.getDecodedToken(result.access_token);
+        expect(decodedAccessToken).to.include({ aud: 'https://admin.pix.fr' });
+
+        const authenticationMethods = await knex('authentication-methods');
+        const createdAuthenticationMethod = authenticationMethods.find(
+          (authenticationMethod) => authenticationMethod.identityProvider == 'OIDC_EXAMPLE_NET',
+        );
+        expect(createdAuthenticationMethod).to.exist;
+        expect(createdAuthenticationMethod.externalIdentifier).to.equal('some-user-unique-id');
+
+        const userLogin = await knex('user-logins').first();
+        expect(userLogin.lastLoggedAt).to.be.greaterThanOrEqual(start);
+
+        const lastUserApplicationConnection = await knex('last-user-application-connections').first();
+        expect(lastUserApplicationConnection.application).to.equal('admin');
+        expect(lastUserApplicationConnection.lastLoggedAt).to.be.greaterThanOrEqual(start);
+      });
     });
   });
 });
