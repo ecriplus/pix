c6ec57c553cf4b8cd51ea7590595689a2432ce96
Clément Latzarus <clement.latzarus@pix.fr>
2025-11-24 17:42:06 +0100

    tests(api): add section in module service tests

--- api/tests/devcomp/unit/domain/services/module-service_test.js
+++ api/tests/devcomp/unit/domain/services/module-service_test.js
@@ -13,34 +13,39 @@ describe('#getModuleByLink', function () {
     expect(error).to.be.instanceOf(ModuleDoesNotExistError);
     expect(error.message).to.contain('bidule');
   });
-  it('should throw an error if link slug does not match any module', async function () {
-    const moduleRepository = { getBySlug: sinon.stub() };
-    moduleRepository.getBySlug.withArgs({ slug: 'wrong-slug' }).rejects(new NotFoundError());
-    const error = await catchErr(getModuleByLink)({ link: '/modules/wrong-slug', moduleRepository });
-    expect(error).to.be.instanceOf(ModuleDoesNotExistError);
-  });
-  it('should return module if link slug matches a module', async function () {
-    //given
-    const moduleRepository = { getBySlug: sinon.stub() };
-    const expectedModule = Symbol('module');
-    moduleRepository.getBySlug.withArgs({ slug: 'good-slug' }).resolves(expectedModule);
 
-    //when
-    const module = await getModuleByLink({ link: '/modules/good-slug', moduleRepository });
+  describe('with slug', function () {
+    it('should throw an error if link slug does not match any module', async function () {
+      const moduleRepository = { getBySlug: sinon.stub() };
+      moduleRepository.getBySlug.withArgs({ slug: 'wrong-slug' }).rejects(new NotFoundError());
+      const error = await catchErr(getModuleByLink)({ link: '/modules/wrong-slug', moduleRepository });
+      expect(error).to.be.instanceOf(ModuleDoesNotExistError);
+    });
 
-    //then
-    expect(module).to.equal(expectedModule);
-  });
-  it('should return module if absolute link slug matches a module', async function () {
-    //given
-    const moduleRepository = { getBySlug: sinon.stub() };
-    const expectedModule = Symbol('module');
-    moduleRepository.getBySlug.withArgs({ slug: 'good-slug' }).resolves(expectedModule);
+    it('should return module if link slug matches a module', async function () {
+      // given
+      const moduleRepository = { getBySlug: sinon.stub() };
+      const expectedModule = Symbol('module');
+      moduleRepository.getBySlug.withArgs({ slug: 'good-slug' }).resolves(expectedModule);
 
-    //when
-    const module = await getModuleByLink({ link: 'https://app.pix.fr/modules/good-slug', moduleRepository });
+      // when
+      const module = await getModuleByLink({ link: '/modules/good-slug', moduleRepository });
 
-    //then
-    expect(module).to.equal(expectedModule);
+      // then
+      expect(module).to.equal(expectedModule);
+    });
+
+    it('should return module if absolute link slug matches a module', async function () {
+      // given
+      const moduleRepository = { getBySlug: sinon.stub() };
+      const expectedModule = Symbol('module');
+      moduleRepository.getBySlug.withArgs({ slug: 'good-slug' }).resolves(expectedModule);
+
+      // when
+      const module = await getModuleByLink({ link: 'https://app.pix.fr/modules/good-slug', moduleRepository });
+
+      // then
+      expect(module).to.equal(expectedModule);
+    });
   });
 });
