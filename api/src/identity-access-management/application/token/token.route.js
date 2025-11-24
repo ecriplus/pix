3ffd21081d529f7e491bea7c4eadcbac1f9f3b6e
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2025-11-24 17:32:00 +0100

    refactor(api): remove first obsolete notion of scope now replaced by aud

--- api/src/identity-access-management/application/token/token.route.js
+++ api/src/identity-access-management/application/token/token.route.js
@@ -65,7 +65,6 @@ export const tokenRoutes = [
               grant_type: Joi.string().valid('password').required(),
               username: Joi.string().required(),
               password: Joi.string().required(),
-              scope: Joi.string().optional(), // Now useless, will soon be removed
             }),
           Joi.object()
             .required()
