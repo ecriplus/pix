8d7abe4a541ed74811ecf38aab4528b730bc0f0f
Baptiste H <39197212+baptiste-h@users.noreply.github.com>
2025-11-25 16:03:54 +0100

    feat(orga): add divisions request and division / group filters on route

--- orga/app/routes/authenticated/combined-course/participations.js
+++ orga/app/routes/authenticated/combined-course/participations.js
@@ -4,16 +4,22 @@ import { service } from '@ember/service';
 export default class CombinedCourseRoute extends Route {
   @service store;
   @service router;
+  @service currentUser;
 
   queryParams = {
     pageNumber: { refreshModel: true },
     pageSize: { refreshModel: true },
     fullName: { refreshModel: true },
     statuses: { refreshModel: true },
+    divisions: { refreshModel: true },
+    groups: { refreshModel: true },
   };
 
   async model(params) {
     const combinedCourse = this.modelFor('authenticated.combined-course');
+
+    const divisions = await this.currentUser.loadDivisions();
+
     const combinedCourseParticipations = await this.store
       .query('combined-course-participation', {
         combinedCourseId: combinedCourse.id,
@@ -24,6 +30,8 @@ export default class CombinedCourseRoute extends Route {
         filters: {
           fullName: params.fullName,
           statuses: params.statuses,
+          divisions: params.divisions,
+          groups: params.groups,
         },
       })
       .catch((error) => {
@@ -31,6 +39,6 @@ export default class CombinedCourseRoute extends Route {
         this.router.replaceWith('not-found');
       });
 
-    return { combinedCourse, combinedCourseParticipations };
+    return { combinedCourse, combinedCourseParticipations, divisions };
   }
 }
