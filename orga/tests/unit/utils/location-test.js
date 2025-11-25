136218ce158a0714e75b0d91f556b936d1e00769
Quentin Lebouc <quentin.lebouc@pix.fr>
2025-11-25 17:06:16 +0100

    feat(orga): add location util

--- orga/tests/unit/utils/location-test.js
+++ orga/tests/unit/utils/location-test.js
@@ -20,4 +20,48 @@ module('Unit | Utility | location', function (hooks) {
       assert.strictEqual(url, expectedUrl);
     });
   });
+
+  module('#replace', function () {
+    test('should replace the current URL', function (assert) {
+      // given
+      const newUrl = 'https://example.com/new-path';
+      const replaceStub = sinon.stub(Location, 'replace');
+
+      // when
+      Location.replace(newUrl);
+
+      // then
+      sinon.assert.calledWith(replaceStub, newUrl);
+      assert.ok(true);
+    });
+  });
+
+  module('#reload', function () {
+    test('should reload the current page', function (assert) {
+      // given
+      const reloadStub = sinon.stub(Location, 'reload');
+
+      // when
+      Location.reload();
+
+      // then
+      sinon.assert.calledOnce(reloadStub);
+      assert.ok(true);
+    });
+  });
+
+  module('#assign', function () {
+    test('should assign a new URL', function (assert) {
+      // given
+      const newUrl = 'https://example.com/new-path';
+      const assignStub = sinon.stub(Location, 'assign');
+
+      // when
+      Location.assign(newUrl);
+
+      // then
+      sinon.assert.calledWith(assignStub, newUrl);
+      assert.ok(true);
+    });
+  });
 });
