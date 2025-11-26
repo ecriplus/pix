94a329d37c944be47034957c7998b5a075c35398
Baptiste H <39197212+baptiste-h@users.noreply.github.com>
2025-11-26 16:50:40 +0100

    feat(api): update module repository to retrieve modules only without userId

--- api/src/quest/infrastructure/repositories/module-repository.js
+++ api/src/quest/infrastructure/repositories/module-repository.js
@@ -1,7 +1,7 @@
 import { Module } from '../../domain/models/Module.js';
 
-export const getByUserIdAndModuleIds = async ({ userId, moduleIds, modulesApi }) => {
-  const modules = await modulesApi.getUserModuleStatuses({ userId, moduleIds });
+export const getByIds = async ({ moduleIds, modulesApi }) => {
+  const modules = await modulesApi.getModulesByIds({ moduleIds });
 
   return modules.map((module) => new Module(module));
 };
