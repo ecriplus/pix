de183c0aa8ad1938a6a4d27b37b6ddc1a162464d
Benjamin Petetot <benjamin.petetot@pix.fr>
2025-11-28 16:16:26 +0100

    fix(admin): model relations must be singular

--- admin/app/models/certification-consolidated-framework.js
+++ admin/app/models/certification-consolidated-framework.js
@@ -3,5 +3,5 @@ import Model, { belongsTo, hasMany } from '@ember-data/model';
 export default class CertificationConsolidatedFramework extends Model {
   @belongsTo('complementary-certification', { async: false, inverse: null }) complementaryCertification;
   @hasMany('area', { async: true, inverse: null }) areas;
-  @hasMany('tubes', { async: false, inverse: null }) tubes;
+  @hasMany('tube', { async: false, inverse: null }) tubes;
 }
