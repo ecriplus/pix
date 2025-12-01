ed320b173687f95ebf93f6382708cece984e057a
Lou Goubin <103123143+lgoubin@users.noreply.github.com>
2025-12-01 14:50:40 +0100

    :seedling: api: add seed for new certification_sco_blocked_access_dates table

--- api/db/seeds/data/team-certification/shared/setup-configuration.js
+++ api/db/seeds/data/team-certification/shared/setup-configuration.js
@@ -1,9 +1,11 @@
+import { buildDefaultScoBlockedAccessDates } from '../../../../database-builder/factory/build-sco-blocked-access-dates.js';
 import { createCertificationConfiguration } from '../tools/algorithm-configuration/create-certification-configuration.js';
 import { createIssueReportCategories } from '../tools/algorithm-configuration/create-issue-report-categories.js';
 
 export async function setupConfigurations({ databaseBuilder }) {
   await createCertificationConfiguration({ databaseBuilder });
   await createIssueReportCategories({ databaseBuilder });
+  buildDefaultScoBlockedAccessDates();
 
   await databaseBuilder.commit();
 }
