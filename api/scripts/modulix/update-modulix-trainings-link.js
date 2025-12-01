e8f8933ed28aa3a8693696d03e9dfb93442f097b
Eric Lim <eric.lim@pix.fr>
2025-12-01 16:51:07 +0100

    :hammer:(api): add update modulix training link
    with the super regex
    Co-authored-by: Diane Cordier <diane.cordier@pix.fr>
    Co-authored-by: Matthias Ferraina <matthias.ferraina@pix.fr>
    Co-authored-by: Andreia Pena <andreia.pena@pix.fr>

---
+++ api/scripts/modulix/update-modulix-trainings-link.js
@@ -0,0 +1,60 @@
+import { knex } from '../../db/knex-database-connection.js';
+import moduleService from '../../src/devcomp/domain/services/module-service.js';
+import { repositories } from '../../src/devcomp/infrastructure/repositories/index.js';
+import { Script } from '../../src/shared/application/scripts/script.js';
+import { ScriptRunner } from '../../src/shared/application/scripts/script-runner.js';
+
+export class UpdateModulixTrainingsLink extends Script {
+  constructor() {
+    super({
+      description: 'Update Modulix training link',
+      permanent: false,
+      options: {
+        dryRun: {
+          type: 'boolean',
+          describe: 'Run the script without making any database changes',
+          default: false,
+        },
+      },
+    });
+  }
+
+  async handle({ logger, options }) {
+    const { dryRun } = options;
+    logger.info('Script execution started');
+
+    const trx = await knex.transaction();
+    try {
+      const trainingsToBeUpdated = await trx('trainings').whereLike('link', `%/modules/%`).andWhere('type', 'modulix');
+
+      for (const training of trainingsToBeUpdated) {
+        const splittedLink = training.link.split('modules');
+        const module = await moduleService.getModuleByLink({
+          link: training.link,
+          moduleRepository: repositories.moduleRepository,
+        });
+        const newLink = `${splittedLink[0]}modules/${module.shortId}${splittedLink[1]}`;
+        await trx('trainings').update({ link: newLink, updatedAt: new Date() }).where({ id: training.id });
+      }
+
+      if (dryRun) {
+        const training = await trx('trainings').whereLike('link', `%/modules/%`).andWhere('type', 'modulix').first();
+
+        logger.info(`Training link after update : ${training.link}`);
+        await trx.rollback();
+
+        logger.info(`${trainingsToBeUpdated.length} training(s) should have been updated`);
+        return;
+      }
+
+      await trx.commit();
+      logger.info(`${trainingsToBeUpdated.length} trainings have been successfully updated`);
+      return;
+    } catch (error) {
+      await trx.rollback();
+      throw error;
+    }
+  }
+}
+
+await ScriptRunner.execute(import.meta.url, UpdateModulixTrainingsLink);
