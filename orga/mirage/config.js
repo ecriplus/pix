81a76b58c6fcb89221323de5eb4ddb744afadb15
Quentin Lebouc <quentin.lebouc@pix.fr>
2025-12-02 16:39:12 +0100

    feat(orga): add OIDC authentication flow

--- orga/mirage/config.js
+++ orga/mirage/config.js
@@ -716,4 +716,105 @@ function routes() {
       ],
     };
   });
+
+  this.post('/oidc/token', (schema, request) => {
+    if (!request.requestHeaders.Authorization) {
+      return new Response(
+        401,
+        {},
+        {
+          errors: [
+            {
+              code: 'SHOULD_VALIDATE_CGU',
+              meta: { authenticationKey: 'key', userClaims: { lastName: 'PIX', firstName: 'test' } },
+            },
+          ],
+        },
+      );
+    }
+
+    const createdUser = schema.users.create({
+      firstName: 'Lloyd',
+      lastName: 'Cé',
+      lang: 'fr',
+    });
+
+    return {
+      access_token:
+        'aaa.' +
+        btoa(
+          `{"user_id":${createdUser.id},"source":"oidc-externe","iat":1545321469,"exp":4702193958,"identity_provider":"OIDC_PARTNER"}`,
+        ) +
+        '.bbb',
+      logout_url_uuid: '1f3dbb71-f399-4c1c-85ae-0a863c78aeea',
+      user_id: createdUser.id,
+    };
+  });
+
+  this.get('/oidc/identity-providers/:cache_buster', () => {
+    return {
+      data: [
+        {
+          type: 'oidc-identity-providers',
+          id: 'oidc-partner',
+          attributes: {
+            code: 'OIDC_PARTNER',
+            'organization-name': 'Partenaire OIDC',
+            slug: 'oidc-partner',
+            'should-close-session': false,
+            source: 'oidc-externe',
+            'is-visible': true,
+          },
+        },
+      ],
+    };
+  });
+
+  this.get('/oidc/authorization-url', () => {
+    return {
+      redirectTarget: `https://oidc/connexion/oauth2/authorize`,
+    };
+  });
+
+  this.post('/oidc/users', (schema, request) => {
+    const payload = JSON.parse(request.requestBody);
+    const identityProvider = payload.data.attributes.identity_provider;
+    const profile = schema.profiles.create({ pixScore: 1 });
+    const createdUser = schema.users.create({
+      firstName: 'Lloyd',
+      lastName: 'Cé',
+      lang: 'fr',
+      profile,
+    });
+
+    return {
+      access_token:
+        'aaa.' +
+        btoa(
+          `{"user_id":${createdUser.id},"source":"oidc-externe","identity_provider":"${identityProvider}","iat":1545321469,"exp":4702193958}`,
+        ) +
+        '.bbb',
+      user_id: createdUser.id,
+    };
+  });
+
+  this.get('/oidc/redirect-logout-url', () => {
+    return {
+      redirectLogoutUrl:
+        'http://identity_provider_base_url/deconnexion?id_token_hint=ID_TOKEN&redirect_uri=http%3A%2F%2Flocalhost.fr%3A4200%2Fconnexion',
+    };
+  });
+
+  this.post('/oidc/user/check-reconciliation', () => {
+    return {
+      data: {
+        attributes: {
+          'full-name-from-pix': 'LLoyd Cé',
+          'full-name-from-external-identity-provider': 'LLoyd Idp',
+          email: 'lloyd.ce@example.net',
+          'authentication-methods': [{ identityProvider: 'PIX' }],
+        },
+      },
+    };
+  });
 }
---
+++ orga/tests/acceptance/oidc/oidc-authentication-flow-test.js
@@ -0,0 +1,119 @@
+import { visit } from '@1024pix/ember-testing-library';
+import { click, currentURL, fillIn, settled } from '@ember/test-helpers';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+import { t } from 'ember-intl/test-support';
+import { setupApplicationTest } from 'ember-qunit';
+import Location from 'pix-orga/utils/location';
+import { module, test } from 'qunit';
+import sinon from 'sinon';
+
+import setupIntl from '../../helpers/setup-intl';
+import { unabortedVisit } from '../../helpers/unaborted-visit';
+
+module('Acceptance | OIDC | authentication flow', function (hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+  setupIntl(hooks);
+
+  hooks.beforeEach(function () {
+    sinon.stub(Location, 'assign');
+  });
+
+  hooks.afterEach(function () {
+    Location.assign.restore();
+  });
+
+  module('when the user has not performed an authentication to the OIDC Provider', function () {
+    module('when the wanted OIDC Provider is not enabled/ready/existing', function () {
+      test('it redirects the user to the login page', async function (assert) {
+        // when
+        await unabortedVisit('/connexion/some-oidc-provider-not-enabled-ready-existing');
+
+        // then
+        assert.strictEqual(currentURL(), '/connexion');
+      });
+    });
+
+    module('when the wanted OIDC Provider is enabled and ready', function () {
+      test('it redirects the user to the OIDC provider authentication page', async function (assert) {
+        // when
+        await unabortedVisit('/connexion/oidc-partner');
+
+        // then
+        assert.ok(Location.assign.calledWith('https://oidc/connexion/oauth2/authorize'));
+      });
+    });
+  });
+
+  module('when the user has performed an authentication to the OIDC Provider', function () {
+    module('when the wanted OIDC Provider is not enabled/ready/existing', function () {
+      test('it redirects the user to the login page', async function (assert) {
+        // when
+        await unabortedVisit('/connexion/some-oidc-provider-not-enabled-ready-existing?code=code&state=state');
+
+        // then
+        assert.strictEqual(currentURL(), '/connexion');
+      });
+    });
+
+    module('when the wanted OIDC Provider is enabled and ready', function () {
+      test('it redirects the user to the SSO login page', async function (assert) {
+        // when
+        await unabortedVisit('/connexion/oidc-partner?code=code&state=state');
+
+        // then
+        assert.strictEqual(currentURL(), '/connexion/oidc-partner/login');
+      });
+
+      module('when the user has a Pix account', function () {
+        test('the user can log in their account', async function (assert) {
+          // given
+          server.create('user', {
+            email: 'lloyd.ce@example.net',
+            password: 'pix123',
+            cgu: true,
+            mustValidateTermsOfService: false,
+            lastTermsOfServiceValidatedAt: new Date(),
+          });
+          const screen = await visit('/connexion/oidc-partner?code=code&state=state');
+
+          // when
+          await fillIn(
+            screen.getByRole('textbox', { name: t('pages.login-form.email.label') }),
+            'lloyd.ce@example.net',
+          );
+          await fillIn(screen.getByLabelText(t('pages.login-form.password')), 'pix123');
+
+          await click(screen.getByRole('button', { name: 'Je me connecte' }));
+          // eslint-disable-next-line ember/no-settled-after-test-helper
+          await settled();
+
+          // then
+          assert.ok(screen.getByRole('button', { name: t('pages.login-form.login') }));
+        });
+      });
+
+      // TODO: Uncomment and make those tests work when the signup template is done
+      // module('when the user creates an account', function () {
+      //   module('when the user logs out', function () {
+      //     toRenameToTest('it redirects the user to the logout URL', async function (assert) {
+      //       // given
+      //       const screen = await visit('/connexion/oidc-partner?code=code&state=state');
+      //       await click(screen.getByLabelText(t('common.cgu.label')));
+      //       await click(screen.getByRole('button', { name: 'Je crée mon compte' }));
+      //       // eslint-disable-next-line ember/no-settled-after-test-helper
+      //       await settled();
+
+      //       await click(screen.getByRole('button', { name: 'Lloyd Consulter mes informations' }));
+
+      //       // when
+      //       await click(screen.getByRole('link', { name: 'Se déconnecter' }));
+
+      //       // then
+      //       assert.strictEqual(currentURL(), '/deconnexion');
+      //     });
+      //   });
+      // });
+    });
+  });
+});
