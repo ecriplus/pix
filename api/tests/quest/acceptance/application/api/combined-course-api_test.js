079b1038cb8323e8e28c9d8a4beb0f7fea7778be
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2025-12-08 14:28:03 +0100

    tech(api): update combined course api test definition

--- api/tests/quest/acceptance/application/api/combined-course-api_test.js
+++ api/tests/quest/acceptance/application/api/combined-course-api_test.js
@@ -1,12 +1,6 @@
-import { CampaignParticipationStatuses } from '../../../../../src/prescription/shared/domain/constants.js';
 import * as combinedCourseApi from '../../../../../src/quest/application/api/combined-course-api.js';
 import { CombinedCourse } from '../../../../../src/quest/application/api/CombinedCourse.model.js';
 import { MultipleQuestFoundError } from '../../../../../src/quest/application/api/errors.js';
-import {
-  CRITERION_COMPARISONS,
-  REQUIREMENT_COMPARISONS,
-  REQUIREMENT_TYPES,
-} from '../../../../../src/quest/domain/models/Quest.js';
 import { catchErr, databaseBuilder, expect } from '../../../../test-helper.js';
 
 describe('Acceptance | Quest | Application | combined-course-api', function () {
@@ -20,22 +14,7 @@ describe('Acceptance | Quest | Application | combined-course-api', function () {
       code: 'ABCDE1234',
       name: combinedCourseName,
       organizationId,
-      successRequirements: [
-        {
-          requirement_type: REQUIREMENT_TYPES.OBJECT.CAMPAIGN_PARTICIPATIONS,
-          comparison: REQUIREMENT_COMPARISONS.ALL,
-          data: {
-            campaignId: {
-              data: campaignId,
-              comparison: CRITERION_COMPARISONS.EQUAL,
-            },
-            status: {
-              data: CampaignParticipationStatuses.SHARED,
-              comparison: CRITERION_COMPARISONS.EQUAL,
-            },
-          },
-        },
-      ],
+      combinedCourseContents: [{ campaignId }],
     }).id;
     await databaseBuilder.commit();
   });
@@ -70,24 +49,7 @@ describe('Acceptance | Quest | Application | combined-course-api', function () {
       code: 'QWERTY123',
       name: combinedCourseName,
       organizationId,
-      successRequirements: [
-        {
-          requirement_type: REQUIREMENT_TYPES.OBJECT.CAMPAIGN_PARTICIPATIONS,
-          comparison: REQUIREMENT_COMPARISONS.ALL,
-          data: {
-            campaignId: {
-              data: campaignId,
-              comparison: CRITERION_COMPARISONS.EQUAL,
-            },
-            status: {
-              data: CampaignParticipationStatuses.SHARED,
-              comparison: CRITERION_COMPARISONS.EQUAL,
-            },
-          },
-        },
-      ],
-      rewardId: null,
-      rewardType: null,
+      combinedCourseContents: [{ campaignId }],
     });
 
     await databaseBuilder.commit();
