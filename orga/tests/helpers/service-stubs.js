5680190bbd63d19ab6010462fa991c46fd519e62
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2025-12-09 13:04:36 +0100

    test(orga): introduce service-stubs test helpers for Pix Orga

---
+++ orga/tests/helpers/service-stubs.js
@@ -0,0 +1,49 @@
+import Service from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import sinon from 'sinon';
+
+/**
+ * Stubs the oidcIdentityProviders service.
+ *
+ * @param {Object} owner - The owner object.
+ * @returns {Service} The stubbed oidcIdentityProviders service.
+ */
+export function stubOidcIdentityProvidersService(owner, { oidcIdentityProviders, featuredIdentityProviderCode } = {}) {
+  class OidcProvidersServiceStub extends Service {
+    @tracked isOidcProviderAuthenticationInProgress = false;
+
+    constructor() {
+      super();
+      this.oidcIdentityProviders = oidcIdentityProviders || [];
+
+      this.oidcIdentityProviders.forEach((oidcIdentityProvider) => {
+        this[oidcIdentityProvider.id] = oidcIdentityProvider;
+      });
+
+      this.featuredIdentityProviderCode = featuredIdentityProviderCode;
+
+      this.shouldDisplayAccountRecoveryBanner = sinon.stub();
+    }
+
+    load() {
+      return Promise.resolve();
+    }
+
+    get list() {
+      return this.oidcIdentityProviders;
+    }
+
+    get hasIdentityProviders() {
+      return this.list.length > 0;
+    }
+
+    findBySlug(providerSlug) {
+      if (!this.hasIdentityProviders) return;
+      return this.list.find((oidcProvider) => oidcProvider.slug === providerSlug);
+    }
+  }
+
+  owner.unregister('service:oidcIdentityProviders');
+  owner.register('service:oidcIdentityProviders', OidcProvidersServiceStub);
+  return owner.lookup('service:oidcIdentityProviders');
+}
