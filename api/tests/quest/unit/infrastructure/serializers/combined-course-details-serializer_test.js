fa1fb95f6032bf1dfccfb04f6d1f9c359d6d729c
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2025-12-09 18:59:08 +0100

    tech(api): optimize combined course model
    Co-authored-by: Baptiste Hetet <baptiste.hetet@pix.fr>

--- api/tests/quest/unit/infrastructure/serializers/combined-course-details-serializer_test.js
+++ api/tests/quest/unit/infrastructure/serializers/combined-course-details-serializer_test.js
@@ -1,55 +1,13 @@
-import { CampaignParticipationStatuses } from '../../../../../src/prescription/shared/domain/constants.js';
-import {
-  CRITERION_COMPARISONS,
-  Quest,
-  REQUIREMENT_COMPARISONS,
-  REQUIREMENT_TYPES,
-} from '../../../../../src/quest/domain/models/Quest.js';
 import * as combinedCourseDetailsSerializer from '../../../../../src/quest/infrastructure/serializers/combined-course-details-serializer.js';
 import { domainBuilder, expect } from '../../../../test-helper.js';
 
 describe('Quest | Unit | Infrastructure | Serializers | combined-course-details', function () {
-  it('#serialize', function () {
+  it('#serialize', async function () {
     // given
     const combinedCourseDetails = domainBuilder.buildCombinedCourseDetails({
       name: 'Mon parcours',
       code: 'COMBINIX1',
-      quest: new Quest({
-        id: 1,
-        rewardId: null,
-        rewardType: null,
-        eligibilityRequirements: [],
-        successRequirements: [
-          {
-            requirement_type: REQUIREMENT_TYPES.OBJECT.CAMPAIGN_PARTICIPATIONS,
-            comparison: REQUIREMENT_COMPARISONS.ALL,
-            data: {
-              campaignId: {
-                data: 1,
-                comparison: CRITERION_COMPARISONS.EQUAL,
-              },
-              status: {
-                data: CampaignParticipationStatuses.SHARED,
-                comparison: CRITERION_COMPARISONS.EQUAL,
-              },
-            },
-          },
-          {
-            requirement_type: REQUIREMENT_TYPES.OBJECT.PASSAGES,
-            comparison: REQUIREMENT_COMPARISONS.ALL,
-            data: {
-              moduleId: {
-                data: 7,
-                comparison: CRITERION_COMPARISONS.EQUAL,
-              },
-              isTerminated: {
-                data: true,
-                comparison: CRITERION_COMPARISONS.EQUAL,
-              },
-            },
-          },
-        ],
-      }),
+      combinedCourseItems: [{ campaignId: 1 }, { moduleId: 7 }],
     });
 
     // when
