c592252a1512393051a0b93027f18d359fa109a7
alicegoarnisson <alice.goarnisson@pix.fr>
2025-12-19 15:57:18 +0100

    feat(api): checks if target profile exists before creating combined course blueprint

--- api/src/quest/domain/usecases/create-combined-course-blueprint.js
+++ api/src/quest/domain/usecases/create-combined-course-blueprint.js
@@ -1,3 +1,13 @@
-export const createCombinedCourseBlueprint = async ({ combinedCourseBlueprint, combinedCourseBlueprintRepository }) => {
+import { COMBINED_COURSE_BLUEPRINT_ITEMS } from '../models/CombinedCourseBlueprint.js';
+
+export const createCombinedCourseBlueprint = async ({
+  combinedCourseBlueprint,
+  combinedCourseBlueprintRepository,
+  targetProfileRepository,
+}) => {
+  const targetProfileIds = combinedCourseBlueprint.content
+    .filter((item) => item.type === COMBINED_COURSE_BLUEPRINT_ITEMS.EVALUATION)
+    .map(({ value }) => value);
+  await targetProfileRepository.findByIds({ ids: targetProfileIds });
   return combinedCourseBlueprintRepository.save(combinedCourseBlueprint);
 };
--- api/tests/quest/integration/domain/usecases/create-combined-course-blueprint_test.js
+++ api/tests/quest/integration/domain/usecases/create-combined-course-blueprint_test.js
@@ -1,16 +1,20 @@
 import { CombinedCourseBlueprint } from '../../../../../src/quest/domain/models/CombinedCourseBlueprint.js';
 import { usecases } from '../../../../../src/quest/domain/usecases/index.js';
-import { expect, knex } from '../../../../test-helper.js';
+import { NotFoundError } from '../../../../../src/shared/domain/errors.js';
+import { catchErr, databaseBuilder, expect, knex } from '../../../../test-helper.js';
 
 describe('Integration | Combined course | Domain | UseCases | create-combined-course-blueprint', function () {
   it('should create a combined course blueprint', async function () {
     // given
+    const targetProfileId = databaseBuilder.factory.buildTargetProfile().id;
+    await databaseBuilder.commit();
+
     const combinedCourseBlueprint = {
       name: 'Mon épure',
       internalName: 'Une épure pour tel niveau',
       illustration: 'illustrations/mon-epure.png',
       description: 'Description',
-      content: CombinedCourseBlueprint.buildContentItems([{ moduleId: 'abc-123' }, { targetProfileId: 123 }]),
+      content: CombinedCourseBlueprint.buildContentItems([{ moduleId: 'abc-123' }, { targetProfileId }]),
     };
 
     await usecases.createCombinedCourseBlueprint({ combinedCourseBlueprint });
@@ -24,5 +28,18 @@ describe('Integration | Combined course | Domain | UseCases | create-combined-co
     expect(result[0].content).to.deep.equal(combinedCourseBlueprint.content);
     expect(result[0].updatedAt).to.be.instanceOf(Date);
     expect(result[0].createdAt).to.be.instanceOf(Date);
+  });
+  it('should return error if a targetProfileId in content is not found', async function () {
+    // given
+    const combinedCourseBlueprint = {
+      name: 'Mon épure',
+      internalName: 'Une épure pour tel niveau',
+      illustration: 'illustrations/mon-epure.png',
+      description: 'Description',
+      content: CombinedCourseBlueprint.buildContentItems([{ moduleId: 'abc-123' }, { targetProfileId: 123 }]),
+    };
+
+    const error = await catchErr(usecases.createCombinedCourseBlueprint)({ combinedCourseBlueprint });
+    expect(error).to.be.instanceOf(NotFoundError);
   });
 });
