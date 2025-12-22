ff969ea69c31e6b3c4d108af610ae0918079f6e1
Quentin Lebouc <quentin.lebouc@pix.fr>
2025-12-22 15:51:25 +0100

    feat(orga): create OIDC signup page

---
+++ orga/app/controllers/authentication/oidc/signup.js
@@ -0,0 +1,15 @@
+import Controller from '@ember/controller';
+import { SessionStorageEntry } from 'pix-orga/utils/session-storage-entry';
+
+const invitationStorage = new SessionStorageEntry('joinInvitationData');
+const oidcUserAuthenticationStorage = new SessionStorageEntry('oidcUserAuthentication');
+
+export default class OidcSignupController extends Controller {
+  get currentInvitation() {
+    return invitationStorage.get();
+  }
+
+  get userClaims() {
+    return oidcUserAuthenticationStorage.get()?.userClaims;
+  }
+}
