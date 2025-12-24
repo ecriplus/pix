b4a270b664b09f9f72430e7ebd53df853bafdfa4
Alexandre COIN <alexandre.pc.coin@gmail.com>
2025-12-24 15:34:29 +0100

    tech(api): update JSDoc for complementary-certification-repository.js

--- api/src/certification/enrolment/infrastructure/repositories/complementary-certification-repository.js
+++ api/src/certification/enrolment/infrastructure/repositories/complementary-certification-repository.js
@@ -1,9 +1,14 @@
+// @ts-check
 /**
- * @typedef {import ('./index.js').ComplementaryCertificationApi} ComplementaryCertificationApi
+ * @typedef {import ('../../../shared/domain/models/ComplementaryCertificationKeys.js').ComplementaryCertificationKeys} ComplementaryCertificationKeys
  */
 import { knex } from '../../../../../db/knex-database-connection.js';
 import { ComplementaryCertification } from '../../../shared/domain/models/ComplementaryCertification.js';
 
+/**
+ * @function
+ * @returns {Promise<Array<ComplementaryCertification>>}
+ */
 const findAll = async function () {
   const result = await knex.from('complementary-certifications').select('id', 'label', 'key').orderBy('id', 'asc');
 
@@ -12,6 +17,18 @@ const findAll = async function () {
 
 export { findAll };
 
+/**
+ * @typedef {Object} ComplementaryCertificationDTO
+ * @property {number} id
+ * @property {string} label
+ * @property {ComplementaryCertificationKeys} key
+ */
+
+/**
+ * @function
+ * @param {ComplementaryCertificationDTO} result
+ * @returns {ComplementaryCertification}
+ */
 function _toDomain(result) {
   return new ComplementaryCertification(result);
 }
