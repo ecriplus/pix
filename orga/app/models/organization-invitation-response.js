f13cfdef00b86b6141016ce38a0b9fe85dfdebef
Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>
2026-01-05 08:35:35 +0100

    feat(orga): adapt front model to allow userId instead of email in invitation response

--- orga/app/models/organization-invitation-response.js
+++ orga/app/models/organization-invitation-response.js
@@ -3,4 +3,5 @@ import Model, { attr } from '@ember-data/model';
 export default class OrganizationInvitationResponse extends Model {
   @attr('string') code;
   @attr('string') email;
+  @attr('number') userId;
 }
