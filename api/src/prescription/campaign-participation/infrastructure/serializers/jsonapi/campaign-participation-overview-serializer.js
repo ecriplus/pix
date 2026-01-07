12f6f9730e4e36ca103ea5c0ba35a097fae5a441
Guillaume Olejniczak <la.toile.cosmique@gmail.com>
2026-01-07 13:14:39 +0100

    fix(api): update campaign participation status handling and tests

--- api/src/prescription/campaign-participation/infrastructure/serializers/jsonapi/campaign-participation-overview-serializer.js
+++ api/src/prescription/campaign-participation/infrastructure/serializers/jsonapi/campaign-participation-overview-serializer.js
@@ -1,6 +1,9 @@
 import jsonapiSerializer from 'jsonapi-serializer';
 
+import { CampaignParticipationStatuses } from '../../../../shared/domain/constants.js';
+
 const { Serializer } = jsonapiSerializer;
+const { TO_SHARE, STARTED } = CampaignParticipationStatuses;
 
 const serialize = function (campaignParticipationOverviews) {
   return new Serializer('campaign-participation-overview', {
@@ -19,6 +22,14 @@ const serialize = function (campaignParticipationOverviews) {
       'canRetry',
       'campaignType',
     ],
+    transform(record) {
+      const transformed = { ...record };
+      // TODO: remove this mapping once the status is migrated
+      if (transformed.status === TO_SHARE) {
+        transformed.status = STARTED;
+      }
+      return transformed;
+    },
   }).serialize(campaignParticipationOverviews);
 };
 
