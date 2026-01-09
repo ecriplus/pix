2dbfe0fbbd6a1bcf0588bc362216e210eee4166c
Benjamin Petetot <benjamin.petetot@pix.fr>
2026-01-09 12:48:54 +0100

    refactor: use joinInvitation service when joining in oidc/signup page

--- orga/app/controllers/authentication/oidc/signup.js
+++ orga/app/controllers/authentication/oidc/signup.js
@@ -1,27 +1,48 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
 import { SessionStorageEntry } from 'pix-orga/utils/session-storage-entry';
 
-const invitationStorage = new SessionStorageEntry('joinInvitationData');
 const oidcUserAuthenticationStorage = new SessionStorageEntry('oidcUserAuthentication');
 
 export default class OidcSignupController extends Controller {
+  @service session;
+  @service oidcIdentityProviders;
+  @service joinInvitation;
+
   get currentInvitation() {
-    return invitationStorage.get();
+    return this.joinInvitation.invitation;
+  }
+
+  get identityProviderName() {
+    const { identity_provider_slug } = this.model;
+    return this.oidcIdentityProviders.findBySlug(identity_provider_slug)?.organizationName;
   }
 
   get userClaims() {
     return oidcUserAuthenticationStorage.get()?.userClaims;
   }
 
-  get authenticationKey() {
-    return oidcUserAuthenticationStorage.get()?.authenticationKey;
-  }
+  @action
+  async joinAndSignup() {
+    const { identity_provider_slug } = this.model;
+    const { authenticationKey } = oidcUserAuthenticationStorage.get() || {};
 
-  get invitationId() {
-    return invitationStorage.get()?.invitationId;
-  }
+    try {
+      this.session.set('skipAuthentication', true);
+      await this.session.authenticate('authenticator:oidc', {
+        authenticationKey,
+        identityProviderSlug: identity_provider_slug,
+        hostSlug: 'users',
+      });
+      const createdUserId = this.session.data.authenticated.user_id;
 
-  get invitationCode() {
-    return invitationStorage.get()?.code;
+      await this.joinInvitation.acceptInvitationByUserId(createdUserId);
+
+      this.session.set('skipAuthentication', false);
+      await this.session.handleAuthentication();
+    } finally {
+      this.session.set('skipAuthentication', false);
+    }
   }
 }
