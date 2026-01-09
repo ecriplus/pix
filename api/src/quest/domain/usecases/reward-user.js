43db52441b5d13c9878a9310c756f1e85ebcb29b
Baptiste H <baptiste.hetet@gmail.com>
2026-01-09 16:41:29 +0100

    tech(api): add debug logs to reward user usecase
    Co-authored-by: Xavier Carron <33637571+xav-car@users.noreply.github.com>

--- api/src/quest/domain/usecases/reward-user.js
+++ api/src/quest/domain/usecases/reward-user.js
@@ -18,12 +18,16 @@ export const rewardUser = async ({
       return;
     }
 
+    logger.debug(`Found ${quests.length} quests with reward for user ${userId}`);
+
     const eligibilities = await eligibilityRepository.find({ userId });
     const rewards = await rewardRepository.getByUserId({ userId });
 
     const rewardIds = rewards.map((reward) => reward.rewardId);
 
     for (const quest of quests) {
+      logger.debug(`Parsing quest ${quest.id} with reward ${quest.rewardId} for user ${userId}`);
+
       if (rewardIds.includes(quest.rewardId)) {
         continue;
       }
@@ -37,14 +41,22 @@ export const rewardUser = async ({
       }
 
       for (const dataForQuest of dataForQuests) {
+        logger.debug(`Eligibilities found on quest ${quest.id} for user ${userId}`);
+        logger.debug({ dataForQuest }, `Eligibilities for quest ${quest.id}`);
+
         const campaignParticipationIds = quest.findCampaignParticipationIdsContributingToQuest(dataForQuest);
         const targetProfileIds =
           quest.findTargetProfileIdsWithoutCampaignParticipationContributingToQuest(dataForQuest);
+
         const success = await successRepository.find({ userId, campaignParticipationIds, targetProfileIds });
+        logger.debug({ success }, `Success for quest ${quest.id}`);
         dataForQuest.success = success;
+
         const userHasSucceedQuest = quest.isSuccessful(dataForQuest);
 
         if (userHasSucceedQuest) {
+          logger.debug(`Success found on quest ${quest.id} for user ${userId}. Pushing reward ${quest.rewardId}`);
+
           await rewardRepository.reward({ userId, rewardId: quest.rewardId });
           rewardIds.push(quest.rewardId);
           break;
