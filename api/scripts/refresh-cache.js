07b6bd49c31b99869e4546378ba8e5f417ab1396
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2026-01-13 16:57:17 +0100

    tech(redis): fix refresh cache exit

--- api/scripts/refresh-cache.js
+++ api/scripts/refresh-cache.js
@@ -1,6 +1,16 @@
 import { usecases } from '../src/learning-content/domain/usecases/index.js';
 import { Script } from '../src/shared/application/scripts/script.js';
 import { ScriptRunner } from '../src/shared/application/scripts/script-runner.js';
+import { config } from '../src/shared/config.js';
+
+if (config.environment === 'development' || config.environment === 'test') {
+  process.on('uncaughtException', (error) => {
+    if (error?.message === 'Connection is closed.') {
+      return;
+    }
+    throw error;
+  });
+}
 
 export class RefreshCache extends Script {
   constructor() {
