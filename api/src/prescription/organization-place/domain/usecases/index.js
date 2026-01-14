48eddf91ed5a7b43a942fef62a4e8fb31c1cc93e
Alexandre COIN <alexandre.pc.coin@gmail.com>
2026-01-14 15:22:52 +0100

    refactor(admin): remove unnecessarry capacity route

--- api/src/prescription/organization-place/domain/usecases/index.js
+++ api/src/prescription/organization-place/domain/usecases/index.js
@@ -2,14 +2,12 @@ import * as organizationFeatureApi from '../../../../organizational-entities/app
 import * as organizationLearnerRepository from '../../../../prescription/organization-learner/infrastructure/repositories/organization-learner-repository.js';
 import * as organizationRepository from '../../../../shared/infrastructure/repositories/organization-repository.js';
 import { injectDependencies } from '../../../../shared/infrastructure/utils/dependency-injection.js';
-import * as organizationPlacesCapacityRepository from '../../infrastructure/repositories/organization-places-capacity-repository.js';
 import * as organizationPlacesLotRepository from '../../infrastructure/repositories/organization-places-lot-repository.js';
 
 const dependencies = {
   organizationLearnerRepository,
   organizationPlacesLotRepository,
   organizationRepository,
-  organizationPlacesCapacityRepository,
   organizationFeatureApi,
 };
 
@@ -17,7 +15,6 @@ import { createOrganizationPlacesLot } from './create-organization-places-lot.js
 import { deleteOrganizationPlacesLot } from './delete-organization-places-lot.js';
 import { findOrganizationPlacesLot } from './find-organization-places-lot.js';
 import { getDataOrganizationsPlacesStatistics } from './get-data-organizations-places-statistics.js';
-import { getOrganizationPlacesCapacity } from './get-organization-places-capacity.js';
 import { getOrganizationPlacesLots } from './get-organization-places-lots.js';
 import { getOrganizationPlacesStatistics } from './get-organization-places-statistics.js';
 
@@ -26,7 +23,6 @@ const usecasesWithoutInjectedDependencies = {
   deleteOrganizationPlacesLot,
   findOrganizationPlacesLot,
   getDataOrganizationsPlacesStatistics,
-  getOrganizationPlacesCapacity,
   getOrganizationPlacesLots,
   getOrganizationPlacesStatistics,
 };
