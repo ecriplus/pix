60cef1e3527df83509a85f1af436dfc47616e16f
Alexandre COIN <alexandre.pc.coin@gmail.com>
2026-01-22 11:56:38 +0100

    feat(api): prevent orga archiving if active places lot

--- api/src/organizational-entities/domain/usecases/archive-organization.usecase.js
+++ api/src/organizational-entities/domain/usecases/archive-organization.usecase.js
@@ -4,9 +4,22 @@ const archiveOrganization = withTransaction(async function ({
   organizationId,
   userId,
   organizationForAdminRepository,
+  organizationPlacesLotRepository,
 }) {
+  const organizationPlacesLots = await organizationPlacesLotRepository.findAllByOrganizationIds({
+    organizationIds: [organizationId],
+  });
+
+  if (_arePlacesLotsActive(organizationPlacesLots)) {
+    throw new Error();
+  }
+
   await organizationForAdminRepository.archive({ id: organizationId, archivedBy: userId });
   return await organizationForAdminRepository.get({ organizationId });
 });
+
+const _arePlacesLotsActive = (organizationPlacesLots) => {
+  return Boolean(organizationPlacesLots.find((organizationPlacesLot) => organizationPlacesLot.isActive()));
+};
 
 export { archiveOrganization };
--- api/tests/organizational-entities/integration/domain/usecases/archive-organization.usecase.test.js
+++ api/tests/organizational-entities/integration/domain/usecases/archive-organization.usecase.test.js
@@ -1,29 +1,58 @@
+import dayjs from 'dayjs';
+
 import { usecases } from '../../../../../src/organizational-entities/domain/usecases/index.js';
 import { NotFoundError } from '../../../../../src/shared/domain/errors.js';
 import { catchErr, databaseBuilder, expect, sinon } from '../../../../test-helper.js';
 
 describe('Integration | Organizational Entities | Domain | UseCase | archive-organization', function () {
   context('when the organization does exist', function () {
-    it('archives the organization', async function () {
-      // given
-      const now = new Date('2022-02-22');
-      const clock = sinon.useFakeTimers({ now, toFake: ['Date'] });
-      const superAdminUser = databaseBuilder.factory.buildUser.withRole();
-      const organization = databaseBuilder.factory.buildOrganization();
-      await databaseBuilder.commit();
+    context('when there is no active places lot', function () {
+      it('archives the organization', async function () {
+        // given
+        const now = new Date('2022-02-22');
+        const clock = sinon.useFakeTimers({ now, toFake: ['Date'] });
+        const superAdminUser = databaseBuilder.factory.buildUser.withRole();
+        const organization = databaseBuilder.factory.buildOrganization();
+        await databaseBuilder.commit();
 
-      // when
-      const archivedOrganization = await usecases.archiveOrganization({
-        organizationId: organization.id,
-        userId: superAdminUser.id,
+        // when
+        const archivedOrganization = await usecases.archiveOrganization({
+          organizationId: organization.id,
+          userId: superAdminUser.id,
+        });
+
+        // then
+        expect(archivedOrganization.archivedAt).to.deep.equal(now);
+        expect(archivedOrganization.archivistFirstName).to.deep.equal(superAdminUser.firstName);
+        expect(archivedOrganization.archivistLastName).to.deep.equal(superAdminUser.lastName);
+
+        clock.restore();
       });
+    });
 
-      // then
-      expect(archivedOrganization.archivedAt).to.deep.equal(now);
-      expect(archivedOrganization.archivistFirstName).to.deep.equal(superAdminUser.firstName);
-      expect(archivedOrganization.archivistLastName).to.deep.equal(superAdminUser.lastName);
+    context('when there are active places lots', function () {
+      it('throws an error', async function () {
+        // given
+        const superAdminUser = databaseBuilder.factory.buildUser.withRole();
+        const organization = databaseBuilder.factory.buildOrganization();
+        databaseBuilder.factory.buildOrganizationPlace({
+          organizationId: organization.id,
+          activationDate: new Date('2022-02-22'),
+          expirationDate: dayjs(new Date()).add(1, 'day').toDate(),
+          count: 1,
+        });
+        await databaseBuilder.commit();
 
-      clock.restore();
+        // when
+        const error = await catchErr(await usecases.archiveOrganization)({
+          organizationId: organization.id,
+          userId: superAdminUser.id,
+        });
+
+        // then
+        expect(error).to.be.instanceOf(Error);
+        clock.restore();
+      });
     });
   });
 
