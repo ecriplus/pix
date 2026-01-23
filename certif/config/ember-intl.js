2124f6e8757d62d2f5b8f6c78dc8d7a777c6e38c
Benjamin Petetot <benjamin.petetot@pix.fr>
2026-01-23 14:01:46 +0100

    fix(certif): apply the ember-intl v8 migration

--- certif/config/ember-intl.js
+++ certif/config/ember-intl.js
@@ -3,20 +3,6 @@
 module.exports = function (/* environment */) {
   return {
     /**
-     * Merges the fallback locale's translations into all other locales as a
-     * build-time fallback strategy.
-     *
-     * This will **not** prevent missing translation warnings or errors from occurring.
-     * It's meant as safety net when warnings are enabled.
-     * When enabled along with `errorOnMissingTranslations` any fallback attempts will result in an error.
-     *
-     * @property fallbackLocale
-     * @type {String?}
-     * @default "null"
-     */
-    fallbackLocale: null,
-
-    /**
      * Path where translations are stored.  This is relative to the project root.
      * For example, if your translations are an npm dependency, set this to:
      *`'./node_modules/path/to/translations'`
@@ -48,48 +34,5 @@ module.exports = function (/* environment */) {
      * @default "false"
      */
     wrapTranslationsWithNamespace: false,
-
-    /**
-     * Cause a build error if ICU argument mismatches are detected between translations
-     * with the same key across all locales.
-     *
-     * @property errorOnNamedArgumentMismatch
-     * @type {Boolean}
-     * @default "false"
-     */
-    errorOnNamedArgumentMismatch: false,
-
-    /**
-     * Cause a build error if missing translations are detected.
-     *
-     * See https://ember-intl.github.io/ember-intl/docs/guide/missing-translations#throwing-a-build-error-on-missing-required-translation
-     *
-     * @property errorOnMissingTranslations
-     * @type {Boolean}
-     * @default "false"
-     */
-    errorOnMissingTranslations: true,
-
-    /**
-     * Removes empty translations from the build output.
-     *
-     * @property stripEmptyTranslations
-     * @type {Boolean}
-     * @default "false"
-     */
-    stripEmptyTranslations: false,
-
-    /**
-     * A function that is called whenever any translation key, from any locale, is missing at build time.
-     *
-     * See https://ember-intl.github.io/ember-intl/docs/guide/missing-translations#requiring-translations
-     *
-     * @property requiresTranslation
-     * @type {Function}
-     * @default "function(key,locale) { return true }"
-     */
-    requiresTranslation(/* key, locale */) {
-      return true;
-    },
   };
 };
