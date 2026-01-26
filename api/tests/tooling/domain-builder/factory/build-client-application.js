155ec3173316a60a2a175a3a560e9adbbd272958
LionelB <160320+lionelB@users.noreply.github.com>
2026-01-26 12:13:19 +0100

    fix(api): fix clientApplication domain builder

--- api/tests/tooling/domain-builder/factory/build-client-application.js
+++ api/tests/tooling/domain-builder/factory/build-client-application.js
@@ -6,12 +6,14 @@ export function buildClientApplication({
   clientId = 'client-id',
   clientSecret = 'super-secret',
   scopes = ['scope1', 'scope2'],
-  jurisdiction = [
-    {
-      rule: 'tags',
-      value: ['MEDNUM'],
-    },
-  ],
+  jurisdiction = {
+    rules: [
+      {
+        name: 'tags',
+        value: ['MEDNUM'],
+      },
+    ],
+  },
 } = {}) {
   return new ClientApplication({ id, name, clientId, clientSecret, scopes, jurisdiction });
 }
