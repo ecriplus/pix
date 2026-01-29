6ff710c9ca6bc6006c33b7c172c7c6ce64f0d8f0
Fael BASSETTI <fael.bassetti@pix.fr>
2026-01-29 15:57:45 +0100

    tech(llm): move databaseBuilders into their own directory for autocompletion

--- api/tests/evaluation/acceptance/application/assessments/assessment-controller_test.js
+++ api/tests/evaluation/acceptance/application/assessments/assessment-controller_test.js
@@ -888,7 +888,7 @@ describe('Acceptance | Controller | assessment-controller', function () {
             configId: chatDTO.configurationId,
             configContent: chatDTO.configuration,
           };
-          await databaseBuilder.factory.buildChat(databaseChat);
+          await databaseBuilder.factory.llm.buildChat(databaseChat);
           await databaseBuilder.commit();
 
           const promptLlmScope = nock('https://llm-test.pix.fr/api')
