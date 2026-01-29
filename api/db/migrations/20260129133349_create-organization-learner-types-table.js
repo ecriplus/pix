c0ee2b98f5cfb5974bfbfec580549c8ee3a7d9a4
Thomas Bazin <thomas.bazin@pix.fr>
2026-01-29 15:14:17 +0100

    feat(db): create migration for creation of organization_learner_types table

---
+++ api/db/migrations/20260129133349_create-organization-learner-types-table.js
@@ -0,0 +1,22 @@
+const TABLE_NAME = 'organization_learner_types';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  await knex.schema.createTable(TABLE_NAME, function (table) {
+    table.increments('id').primary();
+    table.string('name').notNullable().comment("Name of the organization learner's type");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  await knex.schema.dropTable(TABLE_NAME);
+};
+
+export { down, up };
