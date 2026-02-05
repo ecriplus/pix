f9d9d5f7a9bbd47e92dd4f83e4a5df7ad25c16e4
Eric Lim <eric.lim@pix.fr>
2026-02-05 14:57:12 +0100

    :zap: (api): add index on passages table
    on userId and moduleId columns

---
+++ api/db/migrations/20260205134706_add-user-id-module-id-index-in-passages-table.js
@@ -0,0 +1,24 @@
+const TABLE_NAME = 'passages';
+const COLUMN_NAMES = ['userId', 'moduleId'];
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.index(COLUMN_NAMES);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.dropColumn(COLUMN_NAMES);
+  });
+};
+
+export { down, up };
