33076dd1feec3f777dd6d2b7855e00e96f783a2d
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2026-02-05 15:46:05 +0100

    tech(api): handle empty scoring configurations from certification_versions

--- api/src/certification/shared/domain/models/V3CertificationScoring.js
+++ api/src/certification/shared/domain/models/V3CertificationScoring.js
@@ -28,16 +28,17 @@ export class V3CertificationScoring {
     allAreas,
     competenceList,
   }) {
-    const competencesForScoring = competenceForScoringConfiguration.map(({ competence: competenceCode, values }) => {
-      const competence = competenceList.find(({ index: code }) => code === competenceCode);
-      const area = allAreas.find((area) => area.id === competence.areaId);
-      return new CompetenceForScoring({
-        competenceId: competence.id,
-        areaCode: area.code,
-        competenceCode,
-        intervals: values,
-      });
-    });
+    const competencesForScoring =
+      competenceForScoringConfiguration?.map(({ competence: competenceCode, values }) => {
+        const competence = competenceList.find(({ index: code }) => code === competenceCode);
+        const area = allAreas.find((area) => area.id === competence.areaId);
+        return new CompetenceForScoring({
+          competenceId: competence.id,
+          areaCode: area.code,
+          competenceCode,
+          intervals: values,
+        });
+      }) || [];
 
     return new V3CertificationScoring({
       competencesForScoring,
