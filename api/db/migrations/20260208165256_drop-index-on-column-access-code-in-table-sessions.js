36bb5b9a04cfa24a474de7005a3af613a47380f1
laura bergoens <laura.bergoens@pix.fr>
2026-02-08 18:00:05 +0100

    tech(api): drop unused index on column accessCode in sessions

---
+++ api/db/migrations/20260208165256_drop-index-on-column-access-code-in-table-sessions.js
@@ -0,0 +1,23 @@
+const TABLE_NAME = 'sessions';
+const INDEX_NAME = 'sessions_accesscode_index';
+const COLUMN_NAME = 'accessCode';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  await knex.raw(`DROP INDEX IF EXISTS ??;`, [INDEX_NAME]);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  await knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.index(COLUMN_NAME, INDEX_NAME);
+  });
+};
+
+export { down, up };
