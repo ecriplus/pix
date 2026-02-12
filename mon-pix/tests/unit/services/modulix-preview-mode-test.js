8f6c268ec25e1caf84927d02b2f4ff9fde8d0edf
AndreiaPena <andreia.pena@pix.fr>
2026-02-12 17:14:10 +0100

    ⚡️ (mon-pix): create methods to display element ids button on preview service

--- mon-pix/tests/unit/services/modulix-preview-mode-test.js
+++ mon-pix/tests/unit/services/modulix-preview-mode-test.js
@@ -22,4 +22,67 @@ module('Unit | Service | modulix-preview-mode', function (hooks) {
     // then
     assert.true(previewMode.isEnabled);
   });
+
+  module('isElementsIdButtonEnabled', function () {
+    test('it should be disabled by default', function (assert) {
+      // when
+      const previewMode = this.owner.lookup('service:modulix-preview-mode');
+
+      // then
+      assert.false(previewMode.isElementsIdButtonEnabled);
+    });
+
+    test('it enables elements id button', function (assert) {
+      // given
+      const previewMode = this.owner.lookup('service:modulix-preview-mode');
+
+      // when
+      previewMode.enableElementsIdButton();
+
+      // then
+      assert.true(previewMode.isElementsIdButtonEnabled);
+    });
+
+    module('get isPreviewAndElementsIdButtonEnabled', function () {
+      module('when preview mode is not enabled', function () {
+        test('it returns false', function (assert) {
+          // given
+          const previewMode = this.owner.lookup('service:modulix-preview-mode');
+
+          // when
+          previewMode.enableElementsIdButton();
+
+          // then
+          assert.false(previewMode.isPreviewAndElementsIdButtonEnabled);
+        });
+      });
+
+      module('when elements id button is not enabled', function () {
+        test('it returns false', function (assert) {
+          // given
+          const previewMode = this.owner.lookup('service:modulix-preview-mode');
+
+          // when
+          previewMode.enable();
+
+          // then
+          assert.false(previewMode.isPreviewAndElementsIdButtonEnabled);
+        });
+      });
+
+      module('when both are enabled', function () {
+        test('it returns true', function (assert) {
+          // given
+          const previewMode = this.owner.lookup('service:modulix-preview-mode');
+
+          // when
+          previewMode.enable();
+          previewMode.enableElementsIdButton();
+
+          // then
+          assert.true(previewMode.isPreviewAndElementsIdButtonEnabled);
+        });
+      });
+    });
+  });
 });
