a0e47228d6b63c450f4ad10ff468b79616483fa7
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2026-02-16 11:33:24 +0100

    remove unused mapper error

--- api/src/prescription/target-profile/domain/errors.js
+++
@@ -1,9 +0,0 @@
-import { DomainError } from '../../../shared/domain/errors.js';
-
-class TargetProfileCannotBeCreated extends DomainError {
-  constructor(message = 'Erreur lors de la création du profil cible.') {
-    super(message);
-  }
-}
-
-export { TargetProfileCannotBeCreated };
