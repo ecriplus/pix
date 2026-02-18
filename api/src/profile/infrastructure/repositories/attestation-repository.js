8a090b1292d97a1c82875a89fdb9fd54f2e91b9e
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2026-02-18 11:22:05 +0100

    feat(api): add getDataByKey in attestation repository

--- api/src/profile/infrastructure/repositories/attestation-repository.js
+++ api/src/profile/infrastructure/repositories/attestation-repository.js
@@ -2,10 +2,20 @@ import { DomainTransaction } from '../../../shared/domain/DomainTransaction.js';
 import { Attestation } from '../../domain/models/Attestation.js';
 
 export const getByKey = async ({ attestationKey }) => {
-  const knexConnection = await DomainTransaction.getConnection();
+  const knexConnection = DomainTransaction.getConnection();
   const attestation = await knexConnection('attestations').where({ key: attestationKey }).first();
 
   if (!attestation) return null;
 
   return new Attestation(attestation);
+};
+
+export const getDataByKey = async ({ key, attestationStorage }) => {
+  const attestation = await getByKey({ attestationKey: key });
+
+  if (!attestation) return null;
+
+  const templateName = attestation.templateName + '.pdf';
+  const { Body } = await attestationStorage.readFile({ key: templateName });
+  return Body;
 };
