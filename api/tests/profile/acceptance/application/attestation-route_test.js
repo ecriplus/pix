9a14977329e713d906fae8066106b64c466b3939
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2026-02-18 14:28:25 +0100

    test(api): adapt attestation test to use s3 bucket

--- api/tests/profile/acceptance/application/attestation-route_test.js
+++ api/tests/profile/acceptance/application/attestation-route_test.js
@@ -1,9 +1,16 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import * as url from 'node:url';
+
 import {
   createServer,
   databaseBuilder,
   expect,
   generateAuthenticatedUserRequestHeaders,
+  nock,
 } from '../../../test-helper.js';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 describe('Profile | Acceptance | Application | Attestation Route ', function () {
   let server;
@@ -16,13 +23,17 @@ describe('Profile | Acceptance | Application | Attestation Route ', function () 
     it('should be ok', async function () {
       // given
       const userId = databaseBuilder.factory.buildUser().id;
-      const attestationKey = databaseBuilder.factory.buildAttestation().key;
-
+      const attestation = databaseBuilder.factory.buildAttestation();
       await databaseBuilder.commit();
+
+      nock('http://attestations.fake.endpoint.example.net:80')
+        .get(`/attestations.bucket/${attestation.templateName}?x-id=GetObject`)
+        .reply(200, () => fs.createReadStream(path.join(__dirname, 'template.pdf')));
+
       const options = {
         method: 'GET',
         headers: generateAuthenticatedUserRequestHeaders({ userId }),
-        url: `/api/users/${userId}/attestations/${attestationKey}`,
+        url: `/api/users/${userId}/attestations/${attestation.key}`,
       };
 
       // when
