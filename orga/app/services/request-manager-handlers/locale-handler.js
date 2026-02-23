c179fc0913b0f6ae5d01790fec28cbefb5313269
Quentin Lebouc <quentin.lebouc@pix.fr>
2026-02-23 11:29:33 +0100

    feat(orga): add request manager service
    Co-authored-by: LEGO Technix <109212476+lego-technix@users.noreply.github.com>

---
+++ orga/app/services/request-manager-handlers/locale-handler.js
@@ -0,0 +1,16 @@
+import { service } from '@ember/service';
+
+/**
+ * Request manager handler adding user locale in request header.
+ * See: https://github.com/emberjs/data/blob/main/guides/requests/examples/1-auth.md
+ */
+export default class LocaleHandler {
+  @service locale;
+
+  request(context, next) {
+    const headers = new Headers(context.request.headers);
+    headers.append('Accept-Language', this.locale.acceptLanguageHeader);
+
+    return next(Object.assign({}, context.request, { headers }));
+  }
+}
