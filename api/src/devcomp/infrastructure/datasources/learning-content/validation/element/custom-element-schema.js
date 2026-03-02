7df1742652057ed1665885a7eaf49b922260b224
dianeCdrPix <diane.cordier@pix.fr>
2026-03-02 11:28:19 +0100

    ⚡️(api): add joi description for custom-element

--- api/src/devcomp/infrastructure/datasources/learning-content/validation/element/custom-element-schema.js
+++ api/src/devcomp/infrastructure/datasources/learning-content/validation/element/custom-element-schema.js
@@ -6,9 +6,15 @@ import { htmlSchema, uuidSchema } from '../utils.js';
 const commonProps = {
   id: uuidSchema,
   type: Joi.string().valid('custom').required(),
-  title: Joi.string().allow('').required(),
-  instruction: htmlSchema.allow('').required(),
-  functionalInstruction: htmlSchema.allow('').required(),
+  title: Joi.string().allow('').required().description("Titre de l'élément interactif ou dynamique. Champ facultatif"),
+  instruction: htmlSchema
+    .allow('')
+    .required()
+    .description("Consigne pédagogique de l'élément interactif ou dynamique. Champ facultatif"),
+  functionalInstruction: htmlSchema
+    .allow('')
+    .required()
+    .description("Consigne fonctionnelle de l'élément interactif ou dynamique. Champ facultatif"),
 };
 
 export const customElementSchema = Joi.alternatives().conditional('.tagName', {
