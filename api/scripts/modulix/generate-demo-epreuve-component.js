bbf11f1325bd89805b8885040857699a97679142
rouxxi <gwenvael.laskar@pix.fr>
2026-03-06 10:38:46 +0100

    tech(generate-demo-epreuve-components): pass function into a script class
    Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>
    Co-authored-by: Fael Bassetti <fael.bassetti@pix.fr>
    Co-authored-by: <cecilia.bossard@pix.fr>

--- api/scripts/modulix/generate-demo-epreuve-component.js
+++ api/scripts/modulix/generate-demo-epreuve-component.js
@@ -5,40 +5,65 @@ import { propsExamples } from '@1024pix/epreuves-components/props.examples';
 import { schema as schemas } from '@1024pix/epreuves-components/schema';
 
 import demoEpreuveComponents from '../../src/devcomp/infrastructure/datasources/learning-content/modules/demo-epreuves-components.json' with { type: 'json' };
+import { Script } from '../../src/shared/application/scripts/script.js';
+import { ScriptRunner } from '../../src/shared/application/scripts/script-runner.js';
 
-const module = {
-  id: '235c680e-cbd2-4c56-bef6-80d3ed4d417a',
-  shortId: '0aefd71f',
-  slug: 'demo-epreuves-components',
-  title: 'Démonstration des composants Pix Épreuves',
-  isBeta: true,
-  visibility: 'private',
-  details: {
-    image: 'https://assets.pix.org/modules/placeholder-details.svg',
-    description:
-      '<p>Ce module est dédié à des tests internes à Pix.</p><p>Il contient normalement tous les composants Pix Épreuves.</p>',
-    duration: 2,
-    level: 'novice',
-    objectives: ['<p>Non régression sur les composants Pix Épreuves.</p>'],
-    tabletSupport: 'comfortable',
-  },
-  sections: [
-    {
-      id: 'be0a544c-99bd-4fbc-99b4-a52b5456a008',
-      type: 'blank',
-      grains: Object.entries(schemas)
-        .map(([key, _]) => {
-          const props = propsExamples[key];
-          return generateGrain(key, props);
-        })
-        .sort(byPOIName),
-    },
-  ],
-};
+export class GenerateDemoEpreuveComponentFile extends Script {
+  constructor() {
+    super({
+      description: 'Generate a file demo-epreuve-component.json',
+      permanent: true,
+      options: {},
+    });
+  }
+
+  async handle({ logger }) {
+    logger.info(`import schemas and modules from @1024pix`);
+
+    const module = {
+      id: '235c680e-cbd2-4c56-bef6-80d3ed4d417a',
+      shortId: '0aefd71f',
+      slug: 'demo-epreuves-components',
+      title: 'Démonstration des composants Pix Épreuves',
+      isBeta: true,
+      visibility: 'private',
+      details: {
+        image: 'https://assets.pix.org/modules/placeholder-details.svg',
+        description:
+          '<p>Ce module est dédié à des tests internes à Pix.</p><p>Il contient normalement tous les composants Pix Épreuves.</p>',
+        duration: 2,
+        level: 'novice',
+        objectives: ['<p>Non régression sur les composants Pix Épreuves.</p>'],
+        tabletSupport: 'comfortable',
+      },
+      sections: [
+        {
+          id: 'be0a544c-99bd-4fbc-99b4-a52b5456a008',
+          type: 'blank',
+          grains: Object.entries(schemas)
+            .map(([key, _]) => {
+              const props = propsExamples[key];
+              return generateGrain(key, props);
+            })
+            .sort(byPOIName),
+        },
+      ],
+    };
+
+    logger.info(`START: generate file demo-epreuve-component.json`);
+
+    await fs.writeFile(
+      'api/src/devcomp/infrastructure/datasources/learning-content/modules/demo-epreuves-components.json',
+      JSON.stringify(module, null, 2),
+    );
+
+    logger.info(`END : demo-epreuve-component.json file generated`);
+  }
+}
 
 function generateGrain(poiName, props) {
   const grain = {};
-  const oldGrain = demoEpreuveComponents.sections[0].grains.find((grain) => grain.title === poiName);
+  const oldGrain = demoEpreuveComponents?.sections[0].grains.find((grain) => grain.title === poiName);
 
   grain.id = oldGrain?.id ?? randomUUID();
   grain.type = 'discovery';
@@ -77,7 +102,4 @@ function byPOIName(previousGrain, nextGrain) {
   return previousGrain.title.localeCompare(nextGrain.title);
 }
 
-await fs.writeFile(
-  'api/src/devcomp/infrastructure/datasources/learning-content/modules/demo-epreuves-components.json',
-  JSON.stringify(module, null, 2),
-);
+await ScriptRunner.execute(import.meta.url, GenerateDemoEpreuveComponentFile);
