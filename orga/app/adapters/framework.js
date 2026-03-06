04eaa07b8a0c23144e55500610b768b8bc0d97d8
LionelB <160320+lionelB@users.noreply.github.com>
2026-03-06 19:09:09 +0100

    refactor(orga): remove old route

--- orga/app/adapters/framework.js
+++ orga/app/adapters/framework.js
@@ -1,10 +1,6 @@
 import ApplicationAdapter from './application';
 
 export default class FrameworkAdapter extends ApplicationAdapter {
-  urlForQuery() {
-    return `${this.host}/${this.namespace}/frameworks/for-target-profile-submission`;
-  }
-
   urlForFindAll(modelName, { adapterOptions }) {
     const { organizationId } = adapterOptions;
     return `${this.host}/${this.namespace}/organizations/${organizationId}/frameworks`;
