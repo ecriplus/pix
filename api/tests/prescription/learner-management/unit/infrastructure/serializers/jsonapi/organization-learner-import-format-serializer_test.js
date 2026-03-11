15a932b9ef16a4aa699af932765d4e234f55d590
LionelB <160320+lionelB@users.noreply.github.com>
2026-03-11 14:11:23 +0100

    feat(api): add organization-learner-import-format serializer
    Co-authored-by: Xavier Carron <33637571+xav-car@users.noreply.github.com>

---
+++ api/tests/prescription/learner-management/unit/infrastructure/serializers/jsonapi/organization-learner-import-format-serializer_test.js
@@ -0,0 +1,56 @@
+import { OrganizationLearnerImportFormat } from '../../../../../../../src/prescription/learner-management/domain/models/OrganizationLearnerImportFormat.js';
+import { serialize } from '../../../../../../../src/prescription/learner-management/infrastructure/serializers/jsonapi/organization-learner-import-format-serializer.js';
+import { expect } from '../../../../../../test-helper.js';
+
+describe('Unit | Serializer | JSONAPI | organization-learner-import-format-serializer', function () {
+  describe('#serialize', function () {
+    it('should convert an organizationLearnerImportFormat model object into JSON API data', function () {
+      // given
+      const updatedAt = new Date();
+      const createdAt = new Date();
+      const organizationImport = new OrganizationLearnerImportFormat({
+        id: 123,
+        name: 'SCOIMPORT',
+        fileType: 'csv',
+        config: {
+          acceptedEncoding: ['utf-8'],
+          unicityColumns: ['my_column'],
+          validationRules: { formats: [{ name: 'my_column', type: 'string' }] },
+          headers: [
+            {
+              name: 'Prénom apprenant',
+              config: {
+                property: 'firstName',
+                validate: {
+                  type: 'string',
+                  required: true,
+                },
+                reconcile: {
+                  name: 'COMMON_FIRSTNAME',
+                  fieldId: 'reconcileField2',
+                  position: 2,
+                },
+              },
+              required: true,
+            },
+          ],
+        },
+        createdBy: 1,
+        updatedAt,
+        createdAt,
+      });
+
+      expect(serialize(organizationImport)).deep.equal({
+        data: {
+          type: 'organization-learner-import-formats',
+          id: '123',
+          attributes: {
+            'file-type': 'csv',
+            name: 'SCOIMPORT',
+            config: organizationImport.config,
+          },
+        },
+      });
+    });
+  });
+});
