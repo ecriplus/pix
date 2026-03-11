6d51479d77ccb8dde9a15ec6f85dfacfa5b1272a
LionelB <160320+lionelB@users.noreply.github.com>
2026-03-11 12:55:23 +0100

    feat(api): add usecase to find all orgnaizationLearnerImportFormat

---
+++ api/tests/prescription/learner-management/integration/domain/usecases/find-all-organization-learner-import-format_test.js
@@ -0,0 +1,48 @@
+import { OrganizationLearnerImportFormat } from '../../../../../../src/prescription/learner-management/domain/models/OrganizationLearnerImportFormat.js';
+import { usecases } from '../../../../../../src/prescription/learner-management/domain/usecases/index.js';
+import { ORGANIZATION_FEATURE } from '../../../../../../src/shared/domain/constants.js';
+import { databaseBuilder, expect } from '../../../../../test-helper.js';
+
+describe('Integration | UseCase | Learner Management | find-all-organization-learner-import-format', function () {
+  let scoImportFormat, supImportFormat;
+
+  beforeEach(async function () {
+    const scoOrganizationId = databaseBuilder.factory.buildOrganization().id;
+    const supOrganizationId = databaseBuilder.factory.buildOrganization().id;
+
+    const featureId = databaseBuilder.factory.buildFeature({
+      key: ORGANIZATION_FEATURE.LEARNER_IMPORT.key,
+    }).id;
+
+    scoImportFormat = databaseBuilder.factory.buildOrganizationLearnerImportFormat({ name: 'SCO' });
+    supImportFormat = databaseBuilder.factory.buildOrganizationLearnerImportFormat({ name: 'SUP' });
+
+    databaseBuilder.factory.buildOrganizationFeature({
+      organizationId: scoOrganizationId,
+      featureId,
+      params: {
+        organizationLearnerImportFormatId: scoImportFormat.id,
+        organizationLearnerImportFormatName: scoImportFormat.name,
+      },
+    });
+    databaseBuilder.factory.buildOrganizationFeature({
+      organizationId: supOrganizationId,
+      featureId,
+      params: {
+        organizationLearnerImportFormatId: supImportFormat.id,
+        organizationLearnerImportFormatName: supImportFormat.name,
+      },
+    });
+
+    await databaseBuilder.commit();
+  });
+
+  it('should return all OrganizationLearnerImportFormat', async function () {
+    // when
+    const result = await usecases.findAllOrganizationLearnerImportFormats();
+
+    // then
+    expect(result).lengthOf(2);
+    expect(result[0]).instanceOf(OrganizationLearnerImportFormat);
+  });
+});
