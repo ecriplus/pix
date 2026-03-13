ed8f41eac0c059fdf848efc4fe9db58a1d02f477
Thomas Bazin <thomas.bazin@pix.fr>
2026-03-13 18:20:34 +0100

    feat(admin): create networks/:id route

--- admin/app/models/network.js
+++ admin/app/models/network.js
@@ -3,4 +3,5 @@ import Model, { attr } from '@ember-data/model';
 export default class Network extends Model {
   @attr('string') name;
   @attr('number') organizationId;
+  @attr() headOrganization;
 }
