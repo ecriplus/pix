9e74037e375786213e6bf3d97fdd698c8f2ae685
Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>
2026-03-23 16:01:21 +0100

    feat(api): provide OIDC authentication methods and feature toggle to determine email connection eligibility

--- api/src/identity-access-management/domain/usecases/get-user-account-info.usecase.js
+++ api/src/identity-access-management/domain/usecases/get-user-account-info.usecase.js
@@ -1,15 +1,26 @@
+import { featureToggles } from '../../../shared/infrastructure/feature-toggles/index.js';
 import { UserAccountInfo } from '../models/UserAccountInfo.js';
 
-const getUserAccountInfo = async ({ userId, userRepository, privacyUsersApiRepository }) => {
+const getUserAccountInfo = async ({
+  userId,
+  userRepository,
+  privacyUsersApiRepository,
+  authenticationMethodRepository,
+}) => {
   const user = await userRepository.get(userId);
 
   const canSelfDeleteAccount = await privacyUsersApiRepository.canSelfDeleteAccount({ userId });
-
+  const oidcAuthenticationMethods = await authenticationMethodRepository.findAllOidcAuthenticationMethodsByUser({
+    userId,
+  });
+  const addEmailConnectionMethodEnabled = await featureToggles.get('addEmailConnectionMethodEnabled');
   return new UserAccountInfo({
     id: user.id,
     email: user.email,
     username: user.username,
     canSelfDeleteAccount,
+    oidcAuthenticationMethods,
+    addEmailConnectionMethodEnabled,
   });
 };
 
