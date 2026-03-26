0598165dca09f23636e612112eccc342b533d600
Baptiste H <baptiste.hetet@gmail.com>
2026-03-26 11:53:55 +0100

    tech(api): remove unwanted authorization check on user can manage combined course prehandler
    Co-authored-by: Alice Goarnisson <alice.goarnisson@pix.fr>

--- api/src/quest/application/usecases/check-user-can-manage-combined-course.js
+++ api/src/quest/application/usecases/check-user-can-manage-combined-course.js
@@ -1,6 +1,5 @@
 import * as membershipRepository from '../../../shared/infrastructure/repositories/membership-repository.js';
 import * as combinedCourseRepository from '../../infrastructure/repositories/combined-course-repository.js';
-import * as checkCombinedCourseIsNotDeleted from './check-combined-course-is-not-deleted.js';
 
 const execute = async function ({
   userId,
@@ -8,11 +7,7 @@ const execute = async function ({
   dependencies = { membershipRepository, combinedCourseRepository },
 }) {
   const { organizationId } = await dependencies.combinedCourseRepository.getById({ id: combinedCourseId });
-  const authorized = await checkCombinedCourseIsNotDeleted.execute({ id: combinedCourseId });
 
-  if (!authorized) {
-    return false;
-  }
   const memberships = await dependencies.membershipRepository.findByUserIdAndOrganizationId({
     userId,
     organizationId,
