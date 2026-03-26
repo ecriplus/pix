9ee6c763209a08f16ffd0d4e84a261768ddf02f5
Thomas Bazin <thomas.bazin@pix.fr>
2026-03-26 16:51:25 +0100

    tech(seeds): create seeds with multiple levels for PRO and SCO networks

--- api/db/seeds/data/team-acquisition/build-networks.js
+++ api/db/seeds/data/team-acquisition/build-networks.js
@@ -6,57 +6,265 @@ import {
 } from './constants.js';
 
 function _buildProNetwork(databaseBuilder) {
-  const network = databaseBuilder.factory.buildNetwork({ name: 'Réseau Professionnel' });
+  const { network, structure: headStructure } = databaseBuilder.factory.buildNetworkAndHeadOrganization({
+    name: 'Réseau Professionnel',
+    headOrganization: {
+      name: 'ABC Comptabilité',
+      externalId: 'PRO_FOR_NETWORK',
+      type: 'PRO',
+      provinceCode: '75',
+      credit: null,
+      createdAt: new Date('2026-09-03'),
+      updatedAt: new Date('2026-09-03'),
 
-  const headOrganization = databaseBuilder.factory.buildOrganization({
-    name: 'ABC Comptabilité Loire-Atlantique',
-    externalId: 'PRO_FOR_NETWORK',
-    type: 'PRO',
-    provinceCode: '44',
-    credit: null,
-    createdAt: new Date('2026-09-03'),
-    updatedAt: new Date('2026-09-03'),
-
-    administrationTeamId: ADMINISTRATION_TEAM_SOLO_ID,
-    organizationLearnerTypeId: ORGANIZATION_LEARNER_TYPE_PROFESSIONAL_ID,
+      administrationTeamId: ADMINISTRATION_TEAM_SOLO_ID,
+      organizationLearnerTypeId: ORGANIZATION_LEARNER_TYPE_PROFESSIONAL_ID,
+    },
   });
+  _buildRegionalAgencies();
 
-  const structure = databaseBuilder.factory.buildStructure();
+  function _buildRegionalAgencies() {
+    databaseBuilder.factory.buildOrganizationInNetwork({
+      networkId: network.id,
+      parentStructureId: headStructure.id,
+      organizationData: {
+        name: 'ABC Comptabilité Agence Ile-de-France',
+        externalId: 'PRO_FOR_NETWORK',
+        type: 'PRO',
+        provinceCode: '75',
+        createdAt: new Date('2026-09-03'),
+        updatedAt: new Date('2026-09-03'),
 
-  databaseBuilder.factory.buildFactStructure({
-    structureId: structure.id,
-    networkId: network.id,
-    organizationId: headOrganization.id,
-  });
+        administrationTeamId: ADMINISTRATION_TEAM_SOLO_ID,
+        organizationLearnerTypeId: ORGANIZATION_LEARNER_TYPE_PROFESSIONAL_ID,
+      },
+    });
+
+    databaseBuilder.factory.buildOrganizationInNetwork({
+      networkId: network.id,
+      parentStructureId: headStructure.id,
+      organizationData: {
+        name: 'ABC Comptabilité Agence Hauts de France',
+        externalId: 'PRO_FOR_NETWORK',
+        type: 'PRO',
+        provinceCode: '59',
+        createdAt: new Date('2026-09-03'),
+        updatedAt: new Date('2026-09-03'),
+
+        administrationTeamId: ADMINISTRATION_TEAM_SOLO_ID,
+        organizationLearnerTypeId: ORGANIZATION_LEARNER_TYPE_PROFESSIONAL_ID,
+      },
+    });
+
+    databaseBuilder.factory.buildOrganizationInNetwork({
+      networkId: network.id,
+      parentStructureId: headStructure.id,
+      organizationData: {
+        name: 'ABC Comptabilité Agence Corse',
+        externalId: 'PRO_FOR_NETWORK',
+        type: 'PRO',
+        provinceCode: '20',
+        createdAt: new Date('2026-09-03'),
+        updatedAt: new Date('2026-09-03'),
+
+        administrationTeamId: ADMINISTRATION_TEAM_SOLO_ID,
+        organizationLearnerTypeId: ORGANIZATION_LEARNER_TYPE_PROFESSIONAL_ID,
+      },
+    });
+  }
 }
 
 function _buildScoNetwork(databaseBuilder) {
-  const network = databaseBuilder.factory.buildNetwork({ name: 'Réseau Scolaire' });
+  const { network, structure: ministereStructure } = databaseBuilder.factory.buildNetworkAndHeadOrganization({
+    name: 'Réseau Scolaire',
+    headOrganization: {
+      name: 'Ministère',
+      externalId: 'SCO_FOR_NETWORK',
+      type: 'SCO',
+      provinceCode: '75',
+      isManagingStudents: true,
+      createdAt: new Date('2026-09-03'),
+      updatedAt: new Date('2026-09-03'),
 
-  const headOrganization = databaseBuilder.factory.buildOrganization({
-    name: 'Ministère',
-    externalId: 'SCO_FOR_NETWORK',
-    type: 'SCO',
-    provinceCode: '75',
-    credit: null,
-    isManagingStudents: true,
-    createdAt: new Date('2026-09-03'),
-    updatedAt: new Date('2026-09-03'),
-
-    administrationTeamId: ADMINISTRATION_TEAM_ALPHA_ID,
-    organizationLearnerTypeId: ORGANIZATION_LEARNER_TYPE_STUDENT_ID,
+      administrationTeamId: ADMINISTRATION_TEAM_ALPHA_ID,
+      organizationLearnerTypeId: ORGANIZATION_LEARNER_TYPE_STUDENT_ID,
+    },
   });
 
-  const structure = databaseBuilder.factory.buildStructure();
+  _buildAcademieVersaillesAndChildren();
+  _buildAcademieNantesAndChildren();
+  _buildAcademieLilleAndChildren();
 
-  databaseBuilder.factory.buildFactStructure({
-    structureId: structure.id,
-    networkId: network.id,
-    organizationId: headOrganization.id,
+  function _buildAcademieVersaillesAndChildren() {
+    const { structure: academieVersaillesStructure } = databaseBuilder.factory.buildOrganizationInNetwork({
+      networkId: network.id,
+      parentStructureId: ministereStructure.id,
+      organizationData: {
+        name: 'Académie de Versailles',
+        externalId: 'SCO_FOR_NETWORK',
+        type: 'SCO',
+        provinceCode: '78',
+        isManagingStudents: true,
+        createdAt: new Date('2026-09-03'),
+        updatedAt: new Date('2026-09-03'),
+
+        administrationTeamId: ADMINISTRATION_TEAM_ALPHA_ID,
+        organizationLearnerTypeId: ORGANIZATION_LEARNER_TYPE_STUDENT_ID,
+      },
+    });
+
+    databaseBuilder.factory.buildOrganizationInNetwork({
+      networkId: network.id,
+      parentStructureId: academieVersaillesStructure.id,
+      organizationData: {
+        name: "Lycée 1 de l'Académie de Versailles",
+        externalId: 'SCO_FOR_NETWORK',
+        type: 'SCO',
+        provinceCode: '78',
+        isManagingStudents: true,
+        createdAt: new Date('2026-09-03'),
+        updatedAt: new Date('2026-09-03'),
+
+        administrationTeamId: ADMINISTRATION_TEAM_ALPHA_ID,
+        organizationLearnerTypeId: ORGANIZATION_LEARNER_TYPE_STUDENT_ID,
+      },
+    });
+
+    databaseBuilder.factory.buildOrganizationInNetwork({
+      networkId: network.id,
+      parentStructureId: academieVersaillesStructure.id,
+      organizationData: {
+        name: "Collège 1 de l'Académie de Versailles",
+        externalId: 'SCO_FOR_NETWORK',
+        type: 'SCO',
+        provinceCode: '78',
+        isManagingStudents: true,
+        createdAt: new Date('2026-09-03'),
+        updatedAt: new Date('2026-09-03'),
+
+        administrationTeamId: ADMINISTRATION_TEAM_ALPHA_ID,
+        organizationLearnerTypeId: ORGANIZATION_LEARNER_TYPE_STUDENT_ID,
+      },
+    });
+  }
+
+  function _buildAcademieNantesAndChildren() {
+    const { structure: academieNantesStructure } = databaseBuilder.factory.buildOrganizationInNetwork({
+      networkId: network.id,
+      parentStructureId: ministereStructure.id,
+      organizationData: {
+        name: 'Académie de Nantes',
+        externalId: 'SCO_FOR_NETWORK',
+        type: 'SCO',
+        provinceCode: '44',
+        isManagingStudents: true,
+        createdAt: new Date('2026-09-03'),
+        updatedAt: new Date('2026-09-03'),
+
+        administrationTeamId: ADMINISTRATION_TEAM_ALPHA_ID,
+        organizationLearnerTypeId: ORGANIZATION_LEARNER_TYPE_STUDENT_ID,
+      },
+    });
+
+    databaseBuilder.factory.buildOrganizationInNetwork({
+      networkId: network.id,
+      parentStructureId: academieNantesStructure.id,
+      organizationData: {
+        name: "Lycée 1 de l'Académie de Nantes",
+        externalId: 'SCO_FOR_NETWORK',
+        type: 'SCO',
+        provinceCode: '44',
+        isManagingStudents: true,
+        createdAt: new Date('2026-09-03'),
+        updatedAt: new Date('2026-09-03'),
+
+        administrationTeamId: ADMINISTRATION_TEAM_ALPHA_ID,
+        organizationLearnerTypeId: ORGANIZATION_LEARNER_TYPE_STUDENT_ID,
+      },
+    });
+
+    databaseBuilder.factory.buildOrganizationInNetwork({
+      networkId: network.id,
+      parentStructureId: academieNantesStructure.id,
+      organizationData: {
+        name: "Collège 1 de l'Académie de Nantes",
+        externalId: 'SCO_FOR_NETWORK',
+        type: 'SCO',
+        provinceCode: '44',
+        isManagingStudents: true,
+        createdAt: new Date('2026-09-03'),
+        updatedAt: new Date('2026-09-03'),
+
+        administrationTeamId: ADMINISTRATION_TEAM_ALPHA_ID,
+        organizationLearnerTypeId: ORGANIZATION_LEARNER_TYPE_STUDENT_ID,
+      },
+    });
+  }
+
+  function _buildAcademieLilleAndChildren() {
+    const { structure: academieLilleStructure } = databaseBuilder.factory.buildOrganizationInNetwork({
+      networkId: network.id,
+      parentStructureId: ministereStructure.id,
+      organizationData: {
+        name: 'Académie de Lille',
+        externalId: 'SCO_FOR_NETWORK',
+        type: 'SCO',
+        provinceCode: '59',
+        isManagingStudents: true,
+        createdAt: new Date('2026-09-03'),
+        updatedAt: new Date('2026-09-03'),
+
+        administrationTeamId: ADMINISTRATION_TEAM_ALPHA_ID,
+        organizationLearnerTypeId: ORGANIZATION_LEARNER_TYPE_STUDENT_ID,
+      },
+    });
+
+    databaseBuilder.factory.buildOrganizationInNetwork({
+      networkId: network.id,
+      parentStructureId: academieLilleStructure.id,
+      organizationData: {
+        name: "Lycée 1 de l'Académie de Lille",
+        externalId: 'SCO_FOR_NETWORK',
+        type: 'SCO',
+        provinceCode: '59',
+        isManagingStudents: true,
+        createdAt: new Date('2026-09-03'),
+        updatedAt: new Date('2026-09-03'),
+
+        administrationTeamId: ADMINISTRATION_TEAM_ALPHA_ID,
+        organizationLearnerTypeId: ORGANIZATION_LEARNER_TYPE_STUDENT_ID,
+      },
+    });
+
+    databaseBuilder.factory.buildOrganizationInNetwork({
+      networkId: network.id,
+      parentStructureId: academieLilleStructure.id,
+      organizationData: {
+        name: "Collège 1 de l'Académie de Lille",
+        externalId: 'SCO_FOR_NETWORK',
+        type: 'SCO',
+        provinceCode: '59',
+        isManagingStudents: true,
+        createdAt: new Date('2026-09-03'),
+        updatedAt: new Date('2026-09-03'),
+
+        administrationTeamId: ADMINISTRATION_TEAM_ALPHA_ID,
+        organizationLearnerTypeId: ORGANIZATION_LEARNER_TYPE_STUDENT_ID,
+      },
+    });
+  }
+}
+
+function _buildComplexNetwork(databaseBuilder) {
+  databaseBuilder.factory.buildNetworkWithMultipleLevels({
+    name: 'Réseau Complexe',
+    numberOfLevels: 4,
+    childrenPerNode: 3,
   });
 }
 
 export function buildNetworks(databaseBuilder) {
   _buildProNetwork(databaseBuilder);
   _buildScoNetwork(databaseBuilder);
+  _buildComplexNetwork(databaseBuilder);
 }
