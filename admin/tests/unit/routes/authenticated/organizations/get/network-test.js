b6b9b258f19ae7f979fcef4b8ca399be2c6cc35a
Thomas Bazin <thomas.bazin@pix.fr>
2026-03-30 10:47:10 +0200

    feat(admin): restrict organizations/get/network route to super admin and redirect if organization does not belong to network

---
+++ admin/tests/unit/routes/authenticated/organizations/get/network-test.js
@@ -0,0 +1,48 @@
+import EmberObject from '@ember/object';
+import Service from '@ember/service';
+import { setupTest } from 'ember-qunit';
+import { module, test } from 'qunit';
+import sinon from 'sinon';
+
+module('Unit | Route | authenticated/organizations/get/network', function (hooks) {
+  setupTest(hooks);
+
+  let route, restrictAccessToStub;
+
+  hooks.beforeEach(function () {
+    route = this.owner.lookup('route:authenticated/organizations/get/network');
+    sinon.stub(route.router, 'replaceWith');
+
+    restrictAccessToStub = sinon.stub().returns();
+    class AccessControlStub extends Service {
+      restrictAccessTo = restrictAccessToStub;
+    }
+    this.owner.register('service:access-control', AccessControlStub);
+  });
+
+  module('#beforeModel', function () {
+    test('it should check if current user is super admin', function (assert) {
+      // given
+      const organization = EmberObject.create({ network: { id: 123, name: 'My Network' } });
+      sinon.stub(route, 'modelFor').returns(organization);
+
+      // when
+      route.beforeModel();
+
+      // then
+      assert.ok(restrictAccessToStub.calledWith(['isSuperAdmin'], 'authenticated.organizations.get.details'));
+    });
+
+    test('it should transition to details route when organization does not belong to a network', async function (assert) {
+      // given
+      const organization = EmberObject.create({ network: { id: null, name: null } });
+      sinon.stub(route, 'modelFor').returns(organization);
+
+      // when
+      await route.beforeModel();
+
+      // then
+      assert.ok(route.router.replaceWith.calledWith('authenticated.organizations.get.details'));
+    });
+  });
+});
