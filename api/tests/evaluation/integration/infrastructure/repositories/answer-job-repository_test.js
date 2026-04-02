b2ccb9cabec8d6e82ed23cbb8dbe17309106153e
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2026-04-02 11:10:46 +0200

    refactor(api): move job unit test to integration

--- api/tests/evaluation/integration/infrastructure/repositories/answer-job-repository_test.js
+++ api/tests/evaluation/integration/infrastructure/repositories/answer-job-repository_test.js
@@ -1,10 +1,14 @@
 import { AnswerJobRepository } from '../../../../../src/evaluation/infrastructure/repositories/answer-job-repository.js';
 import { config } from '../../../../../src/shared/config.js';
 import { featureToggles } from '../../../../../src/shared/infrastructure/feature-toggles/index.js';
+import { temporaryStorage } from '../../../../../src/shared/infrastructure/key-value-storages/index.js';
 import { expect, sinon } from '../../../../test-helper.js';
 
-describe('Evaluation | Unit | Infrastructure | Repositories | AnswerJobRepository', function () {
+const profileRewardTemporaryStorage = temporaryStorage.withPrefix('profile-rewards:');
+
+describe('Evaluation | Integration | Infrastructure | Repositories | AnswerJobRepository', function () {
   beforeEach(async function () {
+    await profileRewardTemporaryStorage.flushAll();
     sinon.stub(config, 'featureToggles');
     await featureToggles.set('isQuestEnabled', true);
     await featureToggles.set('isAsyncQuestRewardingCalculationEnabled', true);
@@ -13,49 +17,43 @@ describe('Evaluation | Unit | Infrastructure | Repositories | AnswerJobRepositor
   describe('#performAsync', function () {
     it('should do nothing if quests are disabled', async function () {
       // given
-      const profileRewardTemporaryStorageStub = { increment: sinon.stub() };
       await featureToggles.set('isQuestEnabled', false);
-      const userId = Symbol('userId');
-      const answerJobRepository = new AnswerJobRepository({
-        dependencies: { profileRewardTemporaryStorage: profileRewardTemporaryStorageStub },
-      });
+      const userId = '123';
+      const answerJobRepository = new AnswerJobRepository();
 
       // when
       await answerJobRepository.performAsync({ userId });
 
       // then
-      expect(profileRewardTemporaryStorageStub.increment).not.to.have.been.called;
+      const counter = await profileRewardTemporaryStorage.get(userId);
+      expect(counter).to.be.null;
     });
 
     it('should do nothing if quests are in sync mode', async function () {
       // given
-      const profileRewardTemporaryStorageStub = { increment: sinon.stub() };
       await featureToggles.set('isAsyncQuestRewardingCalculationEnabled', false);
-      const userId = Symbol('userId');
-      const answerJobRepository = new AnswerJobRepository({
-        dependencies: { profileRewardTemporaryStorage: profileRewardTemporaryStorageStub },
-      });
+      const userId = '123';
+      const answerJobRepository = new AnswerJobRepository();
 
       // when
       await answerJobRepository.performAsync({ userId });
 
       // then
-      expect(profileRewardTemporaryStorageStub.increment).not.to.have.been.called;
+      const counter = await profileRewardTemporaryStorage.get(userId);
+      expect(counter).to.be.null;
     });
 
     it("should increment user's jobs count in temporary storage", async function () {
       // given
-      const profileRewardTemporaryStorageStub = { increment: sinon.stub() };
       const userId = '123';
-      const answerJobRepository = new AnswerJobRepository({
-        dependencies: { profileRewardTemporaryStorage: profileRewardTemporaryStorageStub },
-      });
+      const answerJobRepository = new AnswerJobRepository();
 
       // when
       await answerJobRepository.performAsync({ userId });
 
       // then
-      expect(profileRewardTemporaryStorageStub.increment).to.have.been.calledWith(userId);
+      const counter = await profileRewardTemporaryStorage.get(userId);
+      expect(counter).to.equal(1);
     });
   });
 });
