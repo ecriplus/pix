76dad26831dd2ca911a20f473e81bdd94a5c72f7
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2026-04-03 14:51:44 +0200

    feat(api): add organization learner filter repository

--- api/src/prescription/learner-management/infrastructure/repositories/organization-learner-filter-repository.js
+++ api/src/prescription/learner-management/infrastructure/repositories/organization-learner-filter-repository.js
@@ -1,4 +1,6 @@
 import { DomainTransaction } from '../../../../shared/domain/DomainTransaction.js';
+import { UnicityConstraintError } from '../../../../shared/domain/errors.js';
+import * as knexUtils from '../../../../shared/infrastructure/utils/knex-utils.js';
 
 const deleteOrganizationLearnerFiltersFromOrganizationId = async function (organizationId) {
   const knexConn = DomainTransaction.getConnection();
@@ -6,4 +8,20 @@ const deleteOrganizationLearnerFiltersFromOrganizationId = async function (organ
   await knexConn('organization_learner_filters').where('organization_id', organizationId).del();
 };
 
-export { deleteOrganizationLearnerFiltersFromOrganizationId };
+const saveOrganizationLearnerFilters = async function (organizationLearnerFilters) {
+  try {
+    const knexConn = DomainTransaction.getConnection();
+    await knexConn.batchInsert('organization_learner_filters', organizationLearnerFilters).transacting(knexConn);
+  } catch (error) {
+    if (knexUtils.isUniqConstraintViolated(error) && error.code === '23505') {
+      throw new UnicityConstraintError(
+        'Unicity constraint failed on organization learner filter repository',
+        error.detail,
+      );
+    }
+
+    throw error;
+  }
+};
+
+export { deleteOrganizationLearnerFiltersFromOrganizationId, saveOrganizationLearnerFilters };
