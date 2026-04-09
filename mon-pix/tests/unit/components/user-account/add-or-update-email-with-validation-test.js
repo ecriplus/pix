26a1251a8e297f43b56e66c8a8a84bafa44c1cb6
Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>
2026-04-09 15:43:18 +0200

    feat(mon-pix): enable template and parent component to handle email adding and updating

--- mon-pix/tests/unit/components/user-account/add-or-update-email-with-validation-test.js
+++ mon-pix/tests/unit/components/user-account/add-or-update-email-with-validation-test.js
@@ -2,13 +2,13 @@ import { setupTest } from 'ember-qunit';
 import createGlimmerComponent from 'mon-pix/tests/helpers/create-glimmer-component';
 import { module, test } from 'qunit';
 
-module('Unit | Component | user-account | update-email-with-validation', function (hooks) {
+module('Unit | Component | user-account | add-or-update-email-with-validation', function (hooks) {
   setupTest(hooks);
 
   module('#showVerificationCode', function () {
     test('should show verification code page', async function (assert) {
       // given
-      const component = createGlimmerComponent('user-account/update-email-with-validation');
+      const component = createGlimmerComponent('user-account/add-or-update-email-with-validation');
       const newEmail = 'toto@example.net';
       const password = 'pix123';
 
@@ -21,7 +21,7 @@ module('Unit | Component | user-account | update-email-with-validation', functio
 
     test('should save new email trimmed and in lowercase on sendVerificationCode', async function (assert) {
       // given
-      const component = createGlimmerComponent('user-account/update-email-with-validation');
+      const component = createGlimmerComponent('user-account/add-or-update-email-with-validation');
       const newEmail = '   Toto@Example.net    ';
       const password = 'pix123';
 
@@ -34,7 +34,7 @@ module('Unit | Component | user-account | update-email-with-validation', functio
 
     test('should save password on sendVerificationCode', async function (assert) {
       // given
-      const component = createGlimmerComponent('user-account/update-email-with-validation');
+      const component = createGlimmerComponent('user-account/add-or-update-email-with-validation');
       const newEmail = 'toto@example.net';
       const password = 'pix123';
 
@@ -43,6 +43,28 @@ module('Unit | Component | user-account | update-email-with-validation', functio
 
       // then
       assert.strictEqual(component.password, 'pix123');
+    });
+  });
+
+  module('#action', function () {
+    test('returns "add-email" when canAddEmailConnectionMethod is true', async function (assert) {
+      // given
+      const component = createGlimmerComponent('user-account/add-or-update-email-with-validation', {
+        canAddEmailConnectionMethod: true,
+      });
+
+      // then
+      assert.strictEqual(component.action, 'add-email');
+    });
+
+    test('returns "update-email" when canAddEmailConnectionMethod is false', async function (assert) {
+      // given
+      const component = createGlimmerComponent('user-account/add-or-update-email-with-validation', {
+        canAddEmailConnectionMethod: false,
+      });
+
+      // then
+      assert.strictEqual(component.action, 'update-email');
     });
   });
 });
