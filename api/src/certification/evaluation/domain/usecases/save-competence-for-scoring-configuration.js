54c83b4aac961c06f05bde3493fc8a3503cbe885
laura bergoens <laura.bergoens@pix.fr>
2026-04-11 19:43:50 +0200

    tech(api): remove unneccesary transaction in saveCompetenceForScoringConfig usecase

--- api/src/certification/evaluation/domain/usecases/save-competence-for-scoring-configuration.js
+++ api/src/certification/evaluation/domain/usecases/save-competence-for-scoring-configuration.js
@@ -2,15 +2,11 @@
  * @typedef {import('./index.js').ScoringConfigurationRepository} ScoringConfigurationRepository
  */
 
-import { withTransaction } from '../../../../shared/domain/DomainTransaction.js';
-
-export const saveCompetenceForScoringConfiguration = withTransaction(
-  /**
-   * @param {object} params
-   * @param {object} params.configuration
-   * @param {ScoringConfigurationRepository} params.scoringConfigurationRepository
-   */
-  async ({ configuration, scoringConfigurationRepository }) => {
-    return scoringConfigurationRepository.saveCompetenceForScoringConfiguration({ configuration });
-  },
-);
+/**
+ * @param {object} params
+ * @param {object} params.configuration
+ * @param {ScoringConfigurationRepository} params.scoringConfigurationRepository
+ */
+export async function saveCompetenceForScoringConfiguration({ configuration, scoringConfigurationRepository }) {
+  return scoringConfigurationRepository.saveCompetenceForScoringConfiguration({ configuration });
+}
