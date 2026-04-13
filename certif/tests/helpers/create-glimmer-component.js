e67de29cbaaea1b878c6460aa74d2db08f6be64c
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2026-04-13 15:37:29 +0200

    chore(certif): tweak creater-glimmer-component help for @glimmer/component v2

--- certif/tests/helpers/create-glimmer-component.js
+++ certif/tests/helpers/create-glimmer-component.js
@@ -1,9 +1,35 @@
+import { setOwner } from '@ember/owner';
 import { getContext } from '@ember/test-helpers';
-import GlimmerComponentManager from '@glimmer/component/-private/ember-component-manager';
+import GlimmerComponent from '@glimmer/component';
+
+const OriginalBaseClass = Object.getPrototypeOf(GlimmerComponent);
+const OriginalBaseProto = Object.getPrototypeOf(GlimmerComponent.prototype);
+
+let pendingOwner, pendingArgs;
+
+class SafeBase {
+  constructor(owner, args) {
+    setOwner(this, owner ?? pendingOwner);
+    this.args = args ?? pendingArgs;
+  }
+}
+Object.setPrototypeOf(SafeBase.prototype, OriginalBaseProto);
 
 export default function createComponent(lookupPath, named = {}) {
   const { owner } = getContext();
   const { class: componentClass } = owner.factoryFor(lookupPath);
-  const componentManager = new GlimmerComponentManager(owner);
-  return componentManager.createComponent(componentClass, { named });
+
+  pendingOwner = owner;
+  pendingArgs = named;
+  Object.setPrototypeOf(GlimmerComponent, SafeBase);
+  Object.setPrototypeOf(GlimmerComponent.prototype, SafeBase.prototype);
+
+  try {
+    return new componentClass(owner, named);
+  } finally {
+    Object.setPrototypeOf(GlimmerComponent, OriginalBaseClass);
+    Object.setPrototypeOf(GlimmerComponent.prototype, OriginalBaseProto);
+    pendingOwner = null;
+    pendingArgs = null;
+  }
 }
