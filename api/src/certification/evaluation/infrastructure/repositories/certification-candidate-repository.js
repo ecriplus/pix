8333190c0088fd61542c3e7fdd0c4fc65558b28d
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2026-04-15 12:30:47 +0200

    refactor(api): get the right subscriptionScope in evaluation/certification-candidate-repository

--- api/src/certification/evaluation/infrastructure/repositories/certification-candidate-repository.js
+++ api/src/certification/evaluation/infrastructure/repositories/certification-candidate-repository.js
@@ -10,6 +10,7 @@ import { Candidate } from '../../domain/models/Candidate.js';
  * @property {boolean} accessibilityAdjustmentNeeded
  * @property {Date} reconciledAt
  * @property {ComplementaryCertificationKeys | null} complementaryCertificationKey
+ * @property {string | null} subscription
  */
 
 /**
@@ -23,9 +24,14 @@ export const findByAssessmentId = async function ({ assessmentId }) {
   const knexConn = DomainTransaction.getConnection();
 
   const result = await knexConn('certification-candidates')
-    .select('certification-candidates.accessibilityAdjustmentNeeded', 'certification-candidates.reconciledAt', {
-      complementaryCertificationKey: 'complementary-certifications.key',
-    })
+    .select(
+      'certification-candidates.accessibilityAdjustmentNeeded',
+      'certification-candidates.reconciledAt',
+      'certification-candidates.subscription',
+      {
+        complementaryCertificationKey: 'complementary-certifications.key',
+      },
+    )
     .join('certification-courses', 'certification-courses.candidateId', 'certification-candidates.id')
     .join('assessments', 'assessments.certificationCourseId', 'certification-courses.id')
     .leftJoin(
@@ -53,11 +59,12 @@ export const findByAssessmentId = async function ({ assessmentId }) {
  * @param {RawCertificationCandidateResult} params
  * @returns {Candidate}
  */
-const _toDomain = ({ accessibilityAdjustmentNeeded, reconciledAt, complementaryCertificationKey }) => {
+const _toDomain = ({ accessibilityAdjustmentNeeded, reconciledAt, complementaryCertificationKey, subscription }) => {
+  const subscriptionScope = subscription || _determineScope(complementaryCertificationKey);
   return new Candidate({
     accessibilityAdjustmentNeeded,
     reconciledAt,
-    subscriptionScope: _determineScope(complementaryCertificationKey),
+    subscriptionScope,
     hasCleaSubscription: complementaryCertificationKey === ComplementaryCertificationKeys.CLEA,
   });
 };
