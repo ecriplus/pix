a7c78a547da59e1d2e5f35f4d900eaab4c90721d
Matthias FERRAINA <matthias.ferraina@pix.fr>
2026-04-15 09:24:18 +0200

    :bug:(api): rename key column
    Co-authored-by: Diane Cordier  <diane.cordier@pix.fr>
    Co-authored-by: Eric Lim <eric.lim@pix.fr>

---
+++ api/db/migrations/20260414153434_add-update-recommendationEngine-feature-key.js
@@ -0,0 +1,28 @@
+import { CAMPAIGN_FEATURES } from '../../src/shared/domain/constants.js';
+
+const OLD_KEY = JSON.stringify(CAMPAIGN_FEATURES.RECOMMENDATION_ENGINE);
+const OLD_DESCRIPTION = "Permet d'indiquer si la campagne est concernée par le moteur de recommandations";
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  await knex('features').where({ key: OLD_KEY }).update({
+    key: CAMPAIGN_FEATURES.RECOMMENDATION_ENGINE.key,
+    description: CAMPAIGN_FEATURES.RECOMMENDATION_ENGINE.description,
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  await knex('features').where({ key: CAMPAIGN_FEATURES.RECOMMENDATION_ENGINE.key }).update({
+    key: OLD_KEY,
+    description: OLD_DESCRIPTION,
+  });
+};
+
+export { down, up };
