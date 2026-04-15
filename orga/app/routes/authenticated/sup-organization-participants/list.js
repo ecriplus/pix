31f23292d16c61d1203dc92af5f65ae15a452b60
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2026-04-15 09:37:55 +0200

    tech(orga): remove rsvp from sup learner routes

--- orga/app/routes/authenticated/sup-organization-participants/list.js
+++ orga/app/routes/authenticated/sup-organization-participants/list.js
@@ -1,7 +1,6 @@
 import { action } from '@ember/object';
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
-import RSVP from 'rsvp';
 
 import paramsValidator from '../../../utils/params-validator';
 export default class ListRoute extends Route {
@@ -23,30 +22,34 @@ export default class ListRoute extends Route {
     paramsValidator.validateCertificabilityParams(params);
 
     const organizationId = this.currentUser.organization.id;
-    return RSVP.hash({
-      importDetail: this.currentUser.canAccessImportPage
-        ? await this.store.queryRecord('organization-import-detail', {
-            organizationId: this.currentUser.organization.id,
-          })
-        : null,
-      participants: this.store.query('sup-organization-participant', {
-        filter: {
-          organizationId,
-          search: params.search,
-          studentNumber: params.studentNumber,
-          groups: params.groups,
-          certificability: params.certificability,
-        },
-        sort: {
-          participationCount: params.participationCountOrder,
-          lastnameSort: params.lastnameSort,
-        },
-        page: {
-          number: params.pageNumber,
-          size: params.pageSize,
-        },
-      }),
+    const participants = await this.store.query('sup-organization-participant', {
+      filter: {
+        organizationId,
+        search: params.search,
+        studentNumber: params.studentNumber,
+        groups: params.groups,
+        certificability: params.certificability,
+      },
+      sort: {
+        participationCount: params.participationCountOrder,
+        lastnameSort: params.lastnameSort,
+      },
+      page: {
+        number: params.pageNumber,
+        size: params.pageSize,
+      },
     });
+
+    const importDetail = this.currentUser.canAccessImportPage
+      ? await this.store.queryRecord('organization-import-detail', {
+          organizationId: this.currentUser.organization.id,
+        })
+      : null;
+
+    return {
+      importDetail,
+      participants,
+    };
   }
 
   resetController(controller, isExiting) {
