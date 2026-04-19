7044d42a20efb24c573a66dec75fc1499c3b4454
laura bergoens <laura.bergoens@pix.fr>
2026-04-19 23:40:27 +0200

    tech(e2e): recette-certif do not need pixorga + only creates its own data

--- high-level-tests/e2e-playwright/helpers/db.ts
+++ high-level-tests/e2e-playwright/helpers/db.ts
@@ -4,13 +4,19 @@ import Knex from 'knex';
 import { NON_OIDC_IDENTITY_PROVIDERS } from '../../../api/src/identity-access-management/domain/constants/identity-providers.js';
 // @ts-expect-error AuthenticationMethod from API project
 import { AuthenticationMethod } from '../../../api/src/identity-access-management/domain/models/AuthenticationMethod.js';
-import { buildCertificationData } from './certification/db.ts';
-import { PIX_ADMIN_SUPPORT_DATA, PIX_APP_USER_DATA, PIX_ORGA_ADMIN_DATA, PIX_ORGA_MEMBER_DATA } from './db-data.js';
+import { buildCoreVersion, buildCpfData, buildPixCertifUser } from './certification/builders/index.ts';
+import {
+  PIX_ADMIN_SUPPORT_DATA,
+  PIX_APP_USER_DATA,
+  PIX_CERTIF_PRO_DATA,
+  PIX_ORGA_ADMIN_DATA,
+  PIX_ORGA_MEMBER_DATA,
+} from './db-data.js';
 import {
   createCertificationCenterInDB,
   createCertificationCenterMembershipInDB,
   createOrganizationLearnerInDB,
-  createUserInDB,
+  createUserInDB
 } from './db-utils.ts';
 
 export const knex = Knex({ client: 'postgresql', connection: process.env.DATABASE_URL });
@@ -20,7 +26,9 @@ export async function buildStaticData() {
   if (!hasDataAlreadyBeenBuilt) {
     await buildAuthenticatedUsers();
     await buildTargetProfiles();
-    await buildCertificationData();
+    await buildCpfData(knex);
+    await buildCoreVersion(knex);
+    await buildPixCertifUser(knex, PIX_CERTIF_PRO_DATA, null);
   }
 }
 
@@ -481,7 +489,9 @@ export async function createTargetProfileInDB(name: string) {
     targetProfileId: id,
     organizationId,
   }));
-  await knex('target-profile-shares').insert(targetProfileSharesToInsert);
+  if (targetProfileSharesToInsert.length > 0) {
+    await knex('target-profile-shares').insert(targetProfileSharesToInsert);
+  }
   return id;
 }
 
