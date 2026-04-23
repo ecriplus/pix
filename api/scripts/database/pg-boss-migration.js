4cfd7282957b86dd7a419347cd8ee62bbbf87380
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2026-04-23 16:06:08 +0200

    tech(api): adapt migration script to pgboss v12

--- api/scripts/database/pg-boss-migration.js
+++ api/scripts/database/pg-boss-migration.js
@@ -1,12 +1,25 @@
-import { JobClient } from '../../src/shared/infrastructure/jobs/JobClient.js';
-import { logger } from '../../src/shared/infrastructure/utils/logger.js';
+import { spawn } from 'node:child_process';
 
-try {
-  console.log('run pgboss migrations');
-  await JobClient.instance.initialize();
-} catch (error) {
-  logger.error(error);
-  process.exitCode = 1;
-} finally {
-  await JobClient.instance.stop();
+const { NODE_ENV, JOBS_DATABASE_URL, TEST_JOBS_DATABASE_URL } = process.env;
+const CONNECTION_STRING = NODE_ENV === 'test' ? TEST_JOBS_DATABASE_URL : JOBS_DATABASE_URL;
+
+console.log(`Run pg-boss migrations"`);
+execute(`npx pg-boss migrate --connection-string=${CONNECTION_STRING}`);
+
+function execute(scriptCommands = [], options = {}) {
+  return new Promise((resolve, reject) => {
+    const npmExec = spawn(scriptCommands, {
+      cwd: options.cwd || process.cwd(),
+      stdio: 'inherit',
+      shell: true,
+    });
+
+    npmExec.on('close', (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`${scriptCommands} failed with code ${code}`));
+      }
+    });
+  });
 }
