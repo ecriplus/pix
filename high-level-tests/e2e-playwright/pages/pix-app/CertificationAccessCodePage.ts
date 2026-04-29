bbb4817756e721708947999779e79cb598de9818
Alexandre COIN <alexandre.pc.coin@gmail.com>
2026-04-29 17:25:07 +0200

    tests(e2e): check language selection on e2e tests

--- high-level-tests/e2e-playwright/pages/pix-app/CertificationAccessCodePage.ts
+++ high-level-tests/e2e-playwright/pages/pix-app/CertificationAccessCodePage.ts
@@ -5,6 +5,12 @@ export class CertificationAccessCodePage {
   constructor(public readonly page: Page) {}
 
   async fillAccessCodeAndStartCertificationTest(accessCode: string) {
+    const languageConfirmationCheckbox = this.page.getByRole('checkbox', {
+      name: /Je confirme être à l'aise dans la langue sélectionnée/,
+    });
+    if (await languageConfirmationCheckbox.isVisible()) {
+      await languageConfirmationCheckbox.click();
+    }
     await this.page.getByLabel("Code d'accès communiqué").fill(accessCode);
     await this.page.getByRole('button', { name: 'Commencer mon test' }).click();
     await this.page.waitForURL(/\/assessments\/\d+\/challenges\/\d+$/);
