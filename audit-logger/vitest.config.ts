9c4200740a38fba7d5fceaf7d8adb26918e085ad
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2026-04-30 09:15:34 +0200

    tech(audit-logger): configure audit logger test to output a report xml compatible with CircleCI

--- audit-logger/vitest.config.ts
+++ audit-logger/vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    reporters: process.env.CI ? 'junit' : 'default',
+    outputFile: process.env.CI ? './test-results/report.xml' : undefined,
     globals: true,
     clearMocks: true,
     watch: false,
