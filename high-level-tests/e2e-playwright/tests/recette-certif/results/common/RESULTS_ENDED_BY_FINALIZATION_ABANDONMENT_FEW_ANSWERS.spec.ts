9e0d1d62c96092bc73d26ec2eaa144af4a1cdfd7
laura bergoens <laura.bergoens@pix.fr>
2026-05-07 01:16:56 +0200

    tech(e2e): remove redundant testing on certification test ended before the end

---
+++ high-level-tests/e2e-playwright/tests/recette-certif/results/common/RESULTS_ENDED_BY_FINALIZATION_ABANDONMENT_FEW_ANSWERS.spec.ts
@@ -0,0 +1,151 @@
+import { expect, test } from '../../../../fixtures/certification/index.ts';
+import { checkSessionInformationAndExpectSuccess, getTestRef } from '../../../../helpers/certification/utils.ts';
+import { getNowAsDDMMYYYY } from '../../../../helpers/utils.ts';
+import { HomePage as AdminHomePage } from '../../../../pages/pix-admin/index.ts';
+import { HomePage } from '../../../../pages/pix-app/index.ts';
+import { SessionManagementPage } from '../../../../pages/pix-certif/index.ts';
+
+test(
+  `${getTestRef(import.meta.url)}`,
+  {
+    tag: ['@snapshot', '@core', '@results'],
+    annotation: [
+      {
+        type: 'scenario',
+        description: `User takes a certification test. Only one right answer.
+         - Test ended by finalization
+         - Abort reason : abandonment
+         - Results visible in all PixAdmin screens
+         - Certificate visible in PixApp
+         - Checks CSV results file`,
+      },
+    ],
+  },
+  async ({
+    pixAppCertifiableUserPage,
+    pixCertifProPage,
+    enrollCandidateAndPassExam,
+    pixAdminRoleCertifPage,
+    getCertifiableUserData,
+    snapshotHandler,
+    testRef,
+    snapshotPath,
+    csvResultPath,
+  }) => {
+    const certifiableUserData = await getCertifiableUserData('buffy.summers@example.net');
+    const pixAppCertifiablePage = await pixAppCertifiableUserPage(certifiableUserData);
+    const { sessionNumber, certificationNumber, certificationCenterName, invigilatorOverviewPage } =
+      await enrollCandidateAndPassExam({
+        testRef,
+        rightWrongAnswersSequence: [true],
+        pixAppPage: pixAppCertifiablePage,
+        certifiableUserData,
+      });
+    await invigilatorOverviewPage.close();
+
+    await test.step('user stops at second challenge', async () => {
+      await expect(pixAppCertifiablePage.getByLabel('Votre progression')).toContainText('2 / 32');
+    });
+
+    await test.step('Finalization by marking a technical issue and scoring', async () => {
+      const sessionManagementPage = new SessionManagementPage(pixCertifProPage);
+      const sessionFinalizationPage = await sessionManagementPage.goToFinalizeSession();
+      await expect(pixCertifProPage.getByText(certifiableUserData.lastName)).toBeVisible();
+      await sessionFinalizationPage.markAbandonmentFor(certifiableUserData.lastName);
+      await sessionFinalizationPage.finalizeSession();
+      await sessionFinalizationPage.close();
+    });
+
+    const adminHomepage = new AdminHomePage(pixAdminRoleCertifPage);
+
+    await test.step('Check all session data', async () => {
+      const sessionsMainPage = await adminHomepage.goToCertificationSessionsTab();
+      const sessionPage = await sessionsMainPage.goToSessionToPublishInfo(sessionNumber);
+
+      await test.step('Check session information', async () => {
+        await checkSessionInformationAndExpectSuccess(sessionPage, {
+          address: `address ${testRef}`,
+          room: `room ${testRef}`,
+          invigilatorName: `examiner ${testRef}`,
+          status: 'Finalisée',
+          nbStartedCertifications: 0,
+          nbIssueReportsUnsolved: 0,
+          nbIssueReports: 0,
+          nbCertificationsInError: 0,
+        });
+      });
+
+      await pixAdminRoleCertifPage
+        .getByRole('link', { name: 'Liste des certifications de la session', exact: true })
+        .click();
+      await test.step('Check certification information', async () => {
+        const certificationListPage = await sessionPage.goToCertificationListPage();
+        const certificationData = await certificationListPage.getCertificationData();
+        expect(certificationData.length).toBe(1);
+        expect(certificationData[0]).toMatchObject({
+          Prénom: certifiableUserData.firstName,
+          Nom: certifiableUserData.lastName,
+          Statut: 'Rejetée',
+          Résultats: 'Pix',
+          'Signalements impactants non résolus': '',
+          'Certification passée': 'Pix Cœur',
+        });
+
+        const certificationInformationPage = await certificationListPage.goToCertificationInfoPage(
+          certifiableUserData.firstName,
+        );
+        const certificationGeneralInfo = await certificationInformationPage.getGeneralInfo();
+        expect(certificationGeneralInfo.sessionNumber).toBe(sessionNumber);
+        expect(certificationGeneralInfo.status).toBe('Rejetée');
+        expect(certificationGeneralInfo.result).toBe('Pix');
+
+        const certificationDetails = await certificationInformationPage.getDetails();
+        expect(certificationDetails.status).toBe('Rejetée');
+        expect(certificationDetails.result).toBe('Pix');
+        expect(certificationDetails.nbAnsweredQuestionsOverTotal).toBe('1/32');
+        expect(certificationDetails.nbQuestionsOK).toBe(1);
+        expect(certificationDetails.nbQuestionsKO).toBe(0);
+        expect(certificationDetails.nbQuestionsAband).toBe(0);
+        expect(certificationDetails.nbValidatedTechnicalIssues).toBe(0);
+        expect(certificationDetails.testEndedBy).toBe('Finalisation session');
+        expect(certificationDetails.abortReason).toBe('Abandon : Manque de temps ou départ prématuré');
+      });
+    });
+
+    await test.step('Publish session', async () => {
+      const sessionsMainPage = await adminHomepage.goToCertificationSessionsTab();
+      await sessionsMainPage.publishSession(sessionNumber);
+    });
+
+    await test.step('User checks their certification result', async () => {
+      await pixAppCertifiablePage.goto(process.env.PIX_APP_FR_URL as string);
+      const homePage = new HomePage(pixAppCertifiablePage);
+      const certificateListPage = await homePage.goToMyCertificates();
+      const { mainStatus, extraStatus, detailsFramework, certificationCenter, examDate, result, comment } =
+        await certificateListPage.getCertificateData(certificationNumber);
+      expect(mainStatus).toBe('Certification Pix : Non obtenue');
+      expect(extraStatus).toBe(null);
+      expect(detailsFramework).toBe(null);
+      expect(certificationCenter).toBe('Centre de certification : ' + certificationCenterName);
+      expect(examDate).toBe('Date de passage : ' + getNowAsDDMMYYYY());
+      expect(result).toBe('- PIX');
+      expect(comment).toBe(
+        'Commentaire : Le nombre de questions répondues pendant votre test de certification est insuffisant et ne nous permet pas de vous délivrer une certification.',
+      );
+    });
+
+    await test.step('Checking CSV result file content', async () => {
+      const sessionPage = await adminHomepage.goToSession(sessionNumber);
+      const csvBuffer = await sessionPage.downloadCsvResultFile();
+
+      await snapshotHandler.compareCsvOrRecord(csvBuffer, csvResultPath, [
+        'Date de passage de la certification',
+        'Session',
+        'Numéro de certification',
+        'Centre de certification',
+      ]);
+    });
+
+    await snapshotHandler.expectOrRecord(snapshotPath);
+  },
+);
