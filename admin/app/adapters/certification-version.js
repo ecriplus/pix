c8120b544ded7744ceef042517c772b9bdd7b092
Alexandre COIN <alexandre.pc.coin@gmail.com>
2026-05-11 14:51:54 +0200

    refactor(admin): remove unnecessarry adpter findRecord overload

--- admin/app/adapters/certification-version.js
+++ admin/app/adapters/certification-version.js
@@ -3,11 +3,6 @@ import ApplicationAdapter from './application';
 export default class CertificationVersionAdapter extends ApplicationAdapter {
   namespace = 'api/admin';
 
-  findRecord(store, type, id) {
-    const url = `${this.host}/${this.namespace}/certification-versions/${id}`;
-    return this.ajax(url, 'GET');
-  }
-
   updateRecord(store, type, snapshot) {
     const certificationVersionId = snapshot.id;
     const url = `${this.host}/${this.namespace}/certification-versions/${certificationVersionId}`;
--- admin/tests/unit/adapters/certification-version-test.js
+++ admin/tests/unit/adapters/certification-version-test.js
@@ -6,38 +6,6 @@ import sinon from 'sinon';
 module('Unit | Adapters | certification-version', function (hooks) {
   setupTest(hooks);
 
-  module('#findRecord', () => {
-    test('should call certification-versions endpoint with the given id', async function (assert) {
-      // given
-      const adapter = this.owner.lookup('adapter:certification-version');
-      const response = {
-        data: {
-          id: '42',
-          type: 'certification-versions',
-          attributes: {
-            'start-date': '2024-01-01',
-            'expiration-date': null,
-            'assessment-duration': 105,
-            'minimum-answers': 20,
-            'maximum-assessment-length': 32,
-          },
-          relationships: {
-            areas: { data: [] },
-          },
-        },
-      };
-      sinon.stub(adapter, 'ajax').resolves(response);
-
-      // when
-      const result = await adapter.findRecord(null, null, '42');
-
-      // then
-      const expectedUrl = `${ENV.APP.API_HOST}/api/admin/certification-versions/42`;
-      sinon.assert.calledWith(adapter.ajax, expectedUrl, 'GET');
-      assert.deepEqual(result, response);
-    });
-  });
-
   module('#updateRecord', () => {
     test('should call PATCH with certification version id and serialized data', async function (assert) {
       const adapter = this.owner.lookup('adapter:certification-version');
