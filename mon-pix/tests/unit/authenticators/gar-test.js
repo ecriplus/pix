5dd8eacf6e94e268546dc72551f679392e0e2914
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2026-06-05 12:07:46 +0200

    feat(mon-pix): in GarAuthenticator store expiresAt of decodedAccessToken in session store
    All AccessTokens from Pix API have a time-limited validity: they have the "exp"
    claim.

--- mon-pix/tests/unit/authenticators/gar-test.js
+++ mon-pix/tests/unit/authenticators/gar-test.js
@@ -13,22 +13,27 @@ module('Unit | Authenticator | gar', function (hooks) {
     test('should authenticate the user', async function (assert) {
       // given
       const authenticator = this.owner.lookup('authenticator:gar');
-      const token = Symbol('mon super token');
-
-      const decodeToken = sinon.stub().returns({ user_id: 1, source: 'gar' });
+      const token =
+        'aaa.' +
+        btoa(`{
+        "user_id": 1,
+        "source": "gar",
+        "iat": 1545321469,
+        "exp": 4702193958
+      }`) +
+        '.bbb';
 
       // when
-      const expectedResult = await authenticator.authenticate(token, decodeToken);
+      const result = await authenticator.authenticate(token);
 
       // then
-      sinon.assert.calledWith(decodeToken, token);
-      assert.deepEqual(expectedResult, {
+      assert.deepEqual(result, {
         token_type: 'bearer',
         access_token: token,
         user_id: 1,
+        expiresAt: 4702193958000,
         source: 'gar',
       });
-      assert.ok(true);
     });
   });
 });
