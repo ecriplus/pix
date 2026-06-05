33813ed5a6eb20f184a495e73724765edd2699d5
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2026-06-05 16:39:13 +0200

    feat(orga): in OidcAuthenticator don’t try to restore session if AccessToken is expired
    All AccessTokens from Pix API have a time-limited validity: they have the "exp"
    claim.

--- orga/tests/unit/authenticators/oidc-test.js
+++ orga/tests/unit/authenticators/oidc-test.js
@@ -232,4 +232,44 @@ module('Unit | Authenticator | oidc', function (hooks) {
       });
     });
   });
+
+  module('restore', function () {
+    module('when there is no access_token', function () {
+      test('it rejects', async function (assert) {
+        // given
+        const authenticator = this.owner.lookup('authenticator:oidc');
+        const data = {};
+
+        // when & then
+        await assert.rejects(authenticator.restore(data));
+      });
+    });
+
+    module('when there is an access_token', function () {
+      module('when the access_token is expired', function () {
+        test('it rejects', async function (assert) {
+          // given
+          const authenticator = this.owner.lookup('authenticator:oidc');
+          const data = { expiresAt: new Date().getTime(), access_token: 'accessTokenData' };
+
+          // when & then
+          await assert.rejects(authenticator.restore(data));
+        });
+      });
+
+      module('when the access_token is not expired', function () {
+        test('it returns the still valid data', async function (assert) {
+          // given
+          const authenticator = this.owner.lookup('authenticator:oidc');
+          const data = { expiresAt: new Date().getTime() + 60000, access_token: 'accessTokenData' };
+
+          // when
+          const result = await authenticator.restore(data);
+
+          // then
+          assert.strictEqual(result, data);
+        });
+      });
+    });
+  });
 });
