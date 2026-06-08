6237fecbfbc1b5fd9ba3bcc3792d6bd12d007afa
dianeCdrPix <diane.cordier@pix.fr>
2026-06-08 17:49:38 +0200

    refacto(api): delete useless private methods and simplify export

--- api/src/devcomp/domain/models/element/QCU-declarative.js
+++ api/src/devcomp/domain/models/element/QCU-declarative.js
@@ -2,31 +2,21 @@ import { assertNotNullOrUndefined } from '../../../../shared/domain/models/asser
 import { ModuleInstantiationError } from '../../errors.js';
 import { Element } from './Element.js';
 
-class QCUDeclarative extends Element {
+export class QCUDeclarative extends Element {
   constructor({ id, instruction, proposals, hasShortProposals = false } = {}) {
     super({ id, type: 'qcu-declarative' });
 
     assertNotNullOrUndefined(instruction, 'The instruction is required for a QCU declarative');
-    this.#assertProposalsIsAnArray(proposals);
-    this.#assertProposalsAreNotEmpty(proposals);
+    if (proposals.length === 0) {
+      throw new ModuleInstantiationError('The proposals are required for a QCU declarative');
+    }
+    if (!Array.isArray(proposals)) {
+      throw new ModuleInstantiationError('The QCU declarative proposals should be a list');
+    }
 
     this.instruction = instruction;
     this.proposals = proposals;
     this.isAnswerable = true;
     this.hasShortProposals = Boolean(hasShortProposals);
   }
-
-  #assertProposalsAreNotEmpty(proposals) {
-    if (proposals.length === 0) {
-      throw new ModuleInstantiationError('The proposals are required for a QCU declarative');
-    }
-  }
-
-  #assertProposalsIsAnArray(proposals) {
-    if (!Array.isArray(proposals)) {
-      throw new ModuleInstantiationError('The QCU declarative proposals should be a list');
-    }
-  }
 }
-
-export { QCUDeclarative };
